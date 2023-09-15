0676553102dbfaed92571fa51a447b82e1ff93b3
AyushPandey1502 <114850053+AyushPandey1502@users.noreply.github.com>
2023-09-15 14:08:45 +0530

    Time: 154 ms (19.19%), Space: 15.1 MB (23.89%) - LeetHub

--- 0217-contains-duplicate/0217-contains-duplicate.c
+++ 0217-contains-duplicate/0217-contains-duplicate.c
@@ -1,14 +1,8 @@
-int compare(const void* a, const void* b) {
-    return (*(int*)a - *(int*)b);
-}
+int cmpFunc(const void* a, const void* b){ return *(int*)a - *(int*)b; }
 
-bool containsDuplicate(int* nums, int numsSize) {
-    qsort(nums, numsSize, sizeof(int), compare);
+bool containsDuplicate(int* nums, int numsSize){
+    qsort(nums, numsSize, sizeof(int), cmpFunc);
+    for(int i=1; i<numsSize; i++){ if(nums[i]==nums[i-1]){ return true; } }
 
-    for (int i = 0; i < numsSize - 1; i++) {
-        if (nums[i] == nums[i + 1]) {
-            return true; 
-        }
-    }
-    return false; 
+    return false;
 }
