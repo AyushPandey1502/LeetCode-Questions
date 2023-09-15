81f142b6130942c5997760a61eee13e2cf10f640
AyushPandey1502 <114850053+AyushPandey1502@users.noreply.github.com>
2023-09-15 16:39:43 +0530

    Time: 1 ms (94.57%), Space: 44.2 MB (80.19%) - LeetHub

---
+++ 2058-concatenation-of-array/2058-concatenation-of-array.java
@@ -0,0 +1,10 @@
+class Solution {
+    public int[] getConcatenation(int[] nums) {
+        int n = nums.length;
+        int[] result = new int[2*n];
+        for(int i = 0; i < result.length; i++){
+            result[i] = nums[i%n];
+        }
+        return result;
+    }
+}
