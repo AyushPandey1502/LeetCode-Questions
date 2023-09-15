4f2753c5e991037e5885de54580276d1eac92ba4
AyushPandey1502 <114850053+AyushPandey1502@users.noreply.github.com>
2023-09-15 22:47:30 +0530

    Time: 4 ms (64.07%), Space: 20.9 MB (11.32%) - LeetHub

--- 2876-number-of-employees-who-met-the-target/2876-number-of-employees-who-met-the-target.cpp
+++ 2876-number-of-employees-who-met-the-target/2876-number-of-employees-who-met-the-target.cpp
@@ -1,10 +1,8 @@
 class Solution {
 public:
     int numberOfEmployeesWhoMetTarget(vector<int>& hours, int target) {
-        int count = 0;
-        for(int hr: hours){
-            if(hr >= target) count++;
-        }
-        return count;
+        int ans = 0;
+        for(auto h: hours) ans += (h >= target);
+        return ans;
     }
 };
