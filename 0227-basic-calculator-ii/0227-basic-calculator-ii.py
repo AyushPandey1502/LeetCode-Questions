5a196fffe0a3a298a8ec1a85c50c3095e2d1d6e8
AyushPandey1502 <114850053+AyushPandey1502@users.noreply.github.com>
2023-09-21 11:45:46 +0530

    Time: 117 ms (69.26%), Space: 99.3 MB (5.44%) - LeetHub

--- 0227-basic-calculator-ii/0227-basic-calculator-ii.py
+++ 0227-basic-calculator-ii/0227-basic-calculator-ii.py
@@ -2,5 +2,4 @@ from math import floor
 
 class Solution:
     def calculate(self, s: str) -> int:
-        s = s.replace('/', '//')
-        return floor(eval(s))
+        return floor(eval(s.replace('/','//')))
