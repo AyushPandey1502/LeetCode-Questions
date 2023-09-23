5fcfbca0968d8fee3f07a73087c205a9b8afe8a8
AyushPandey1502 <114850053+AyushPandey1502@users.noreply.github.com>
2023-09-23 22:06:07 +0530

    Time: 0 ms (100%), Space: 39.2 MB (50.04%) - LeetHub

--- 0367-valid-perfect-square/0367-valid-perfect-square.java
+++ 0367-valid-perfect-square/0367-valid-perfect-square.java
@@ -1,10 +1,16 @@
-// Newton Method
 class Solution {
     public boolean isPerfectSquare(int num) {
-        long x = num;
-        while (x*x > num){
-            x = (x + num / x) >> 1;
+        int low = 1, high = num;
+        while (low <= high) {
+            long mid = (low + high) >>> 1;
+            if (mid * mid == num) {
+                return true;
+            } else if (mid * mid < num) {
+                low = (int) mid + 1;
+            } else {
+                high = (int) mid - 1;
+            }
         }
-        return x*x == num;
+        return false;
     }
 }
