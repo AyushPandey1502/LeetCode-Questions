21625b89c7f13029f0241926c32b067cfb983d1b
AyushPandey1502 <114850053+AyushPandey1502@users.noreply.github.com>
2023-09-27 20:08:06 +0530

    Time: 9 ms (48.16%), Space: 11.9 MB (10.89%) - LeetHub

--- 0179-largest-number/0179-largest-number.cpp
+++ 0179-largest-number/0179-largest-number.cpp
@@ -1,15 +1,21 @@
 class Solution {
 public:
-    string largestNumber(vector<int> &num) {
-        vector<string> arr;
-        for(auto i:num)
-            arr.push_back(to_string(i));
-        sort(begin(arr), end(arr), [](string &s1, string &s2){ return s1+s2>s2+s1; });
-        string res;
-        for(auto s:arr)
-            res+=s;
-        while(res[0]=='0' && res.length()>1)
-            res.erase(0,1);
-        return  res;
+    string largestNumber(vector<int>& nums) {
+        vector<string> numStrings;
+        
+        for (int num : nums) {
+            numStrings.push_back(to_string(num));
+        }
+        sort(numStrings.begin(), numStrings.end(), [](const string& a, const string& b) {
+            return a + b > b + a;
+        });
+        if (numStrings[0] == "0") {
+            return "0";
+        }
+        string result;
+        for (const string& str : numStrings) {
+            result += str;
+        }
+        return result;
     }
 };
