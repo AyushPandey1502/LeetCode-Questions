21f2044ba11d20890112bbf10bf64fb5e50c8ec1
AyushPandey1502 <114850053+AyushPandey1502@users.noreply.github.com>
2023-09-28 22:54:28 +0530

    Time: 13 ms (17.42%), Space: 16.6 MB (25.63%) - LeetHub

--- 0941-sort-array-by-parity/0941-sort-array-by-parity.cpp
+++ 0941-sort-array-by-parity/0941-sort-array-by-parity.cpp
@@ -1,25 +1,15 @@
 class Solution {
 public:
-    vector<int> sortArrayByParity(vector<int>& nums) {
-        vector<int> even, odd;
-        
-        for (int num : nums) {
-            if (num % 2 == 0) {
-                even.push_back(num);
-            } else {
-                odd.push_back(num);
-            }
-        }
-        
-        vector<int> result(even.size() + odd.size());
-        int index = 0;
-        
-        for (int num : even) {
-            result[index++] = num;
-        }
-        for (int num : odd) {
-            result[index++] = num;
-        }
-        return result;
-    }
+    vector<int> sortArrayByParity(vector<int>& A) {
+    int j=0;
+    for(int i=0;i<A.size();i++)
+	{
+        if(A[i]%2==0)
+		{
+            swap(A[i],A[j]);
+            j++;
+		}
+	}
+    return A;
+}
 };
