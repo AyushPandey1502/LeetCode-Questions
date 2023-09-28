9c5726c2559d9186bfe5de57b5f1bad04429805d
AyushPandey1502 <114850053+AyushPandey1502@users.noreply.github.com>
2023-09-28 23:46:46 +0530

    Time: 0 ms (100%), Space: 6.4 MB (10.83%) - LeetHub

---
+++ 0007-reverse-integer/0007-reverse-integer.c
@@ -0,0 +1,20 @@
+int reverse(int x) {
+    int reversed = 0;
+    
+    while (x != 0) {
+        int lastDigit = x % 10;
+        
+        // Check for overflow before updating the reversed integer
+        if (reversed > INT_MAX / 10 || (reversed == INT_MAX / 10 && lastDigit > 7)) {
+            return 0;
+        }
+        if (reversed < INT_MIN / 10 || (reversed == INT_MIN / 10 && lastDigit < -8)) {
+            return 0;
+        }
+        
+        reversed = reversed * 10 + lastDigit;
+        x /= 10;
+    }
+    
+    return reversed;
+}
