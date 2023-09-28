08c5b95e25e6e731efd1ddc7ffdea6f303a6f41e
AyushPandey1502 <114850053+AyushPandey1502@users.noreply.github.com>
2023-09-28 23:42:20 +0530

    Time: 8 ms (84.11%), Space: 8.4 MB (18.17%) - LeetHub

--- 0002-add-two-numbers/0002-add-two-numbers.c
+++ 0002-add-two-numbers/0002-add-two-numbers.c
@@ -1,33 +1,28 @@
 struct ListNode* addTwoNumbers(struct ListNode* l1, struct ListNode* l2) {
-    struct ListNode* result = NULL;
-    struct ListNode* current = NULL;
+    struct ListNode* dummy = (struct ListNode*)malloc(sizeof(struct ListNode)); 
+    struct ListNode* current = dummy;
     int carry = 0;
-    
+
     while (l1 || l2 || carry) {
         int sum = carry;
-        
+
         if (l1) {
             sum += l1->val;
             l1 = l1->next;
         }
-        
         if (l2) {
             sum += l2->val;
             l2 = l2->next;
         }
-        
         carry = sum / 10;
-        
-        if (!current) {
-            result = current = (struct ListNode*)malloc(sizeof(struct ListNode));
-        } else {
-            current->next = (struct ListNode*)malloc(sizeof(struct ListNode));
-            current = current->next;
-        }
-        
+
+        current->next = (struct ListNode*)malloc(sizeof(struct ListNode));
+        current = current->next;
         current->val = sum % 10;
         current->next = NULL;
     }
     
+    struct ListNode* result = dummy->next; 
+    free(dummy); 
     return result;
 }
