c55e35e89e9fcb021609715d0c3101a460287675
AyushPandey1502 <114850053+AyushPandey1502@users.noreply.github.com>
2023-09-28 23:03:56 +0530

    Time: 11 ms (24.58%), Space: 16.5 MB (36.74%) - LeetHub

--- 0941-sort-array-by-parity/0941-sort-array-by-parity.cpp
+++ 0941-sort-array-by-parity/0941-sort-array-by-parity.cpp
@@ -1,13 +1,6 @@
 class Solution {
 public:
     vector<int> sortArrayByParity(vector<int>& nums) {
-        deque<int> q;
-        for(int x: nums){
-            if((x&1)==1) 
-                q.push_back(x);
-            else
-                q.push_front(x);     
-        }
-        return vector<int>(q.begin(), q.end());
+        sort(nums.begin(), nums.end(), [&](int x, int y){return (x&1)<(y&1);}); return nums;
     }
 };
