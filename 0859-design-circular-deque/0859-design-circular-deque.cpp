3fd42223af790b43b2d575765e29194ebce3c960
AyushPandey1502 <114850053+AyushPandey1502@users.noreply.github.com>
2023-09-29 12:27:47 +0530

    Time: 23 ms (56.29%), Space: 16.8 MB (25.88%) - LeetHub

--- 0859-design-circular-deque/0859-design-circular-deque.cpp
+++ 0859-design-circular-deque/0859-design-circular-deque.cpp
@@ -74,16 +74,3 @@ public:
         return ((front == 0 && rear == size-1) || (front != 0 && rear ==(front - 1)%(size -1)));
     }
 };
-
-/**
- * Your MyCircularDeque object will be instantiated and called as such:
- * MyCircularDeque* obj = new MyCircularDeque(k);
- * bool param_1 = obj->insertFront(value);
- * bool param_2 = obj->insertLast(value);
- * bool param_3 = obj->deleteFront();
- * bool param_4 = obj->deleteLast();
- * int param_5 = obj->getFront();
- * int param_6 = obj->getRear();
- * bool param_7 = obj->isEmpty();
- * bool param_8 = obj->isFull();
- */
