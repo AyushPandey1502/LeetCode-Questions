f57abb8cfc633f181fc09450666e1632f37e881c
AyushPandey1502 <114850053+AyushPandey1502@users.noreply.github.com>
2023-10-02 21:45:36 +0530

    Time: 0 ms (100%), Space: 6.9 MB (22.53%) - LeetHub

--- 1944-truncate-sentence/1944-truncate-sentence.cpp
+++ 1944-truncate-sentence/1944-truncate-sentence.cpp
@@ -1,12 +1,13 @@
 class Solution {
-public:
-    string truncateSentence(string s, int k) {
-        int i = 0;
-        for (; i < s.length(); i++) {
-            if (s[i] == ' ' && --k == 0) {
-                break;
-            }
-        }
-        return s.substr(0, i);
+ public:
+  string truncateSentence(string s, int k) {
+    size_t i(0);
+    int count(0);
+    for (; i < s.size(); ++i) {
+      if (s[i] == ' ') ++count;
+      if (count == k) break;
     }
+    if (i == s.size() - 1) return s;
+    return s.substr(0, i);
+  }
 };
