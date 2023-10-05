bd9bdb502578dfcb8737cad7269a49d7ec1af17f
AyushPandey1502 <114850053+AyushPandey1502@users.noreply.github.com>
2023-10-05 18:52:50 +0530

    Time: 53 ms (85.48%), Space: 38.4 MB (72.23%) - LeetHub

---
+++ 2486-most-frequent-even-element/2486-most-frequent-even-element.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    int mostFrequentEven(std::vector<int>& nums) {
+        if (nums.empty()) return -1;
+        
+        std::unordered_map<int, int> count_map;
+        int max_count = 0;
+        int most_frequent_even = -1;
+        
+        for (int num : nums) {
+            if (num % 2 == 0) {
+                count_map[num]++;
+                if (count_map[num] > max_count || (count_map[num] == max_count && num < most_frequent_even)) {
+                    max_count = count_map[num];
+                    most_frequent_even = num;
+                }
+            }
+        }
+        
+        return most_frequent_even;
+    }
+};
