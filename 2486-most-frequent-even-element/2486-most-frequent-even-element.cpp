4a5911b097ddaee7b358e1b794577e558f8c6fcd
AyushPandey1502 <114850053+AyushPandey1502@users.noreply.github.com>
2023-10-05 18:55:54 +0530

    Time: 55 ms (81.67%), Space: 38.4 MB (70.13%) - LeetHub

--- 2486-most-frequent-even-element/2486-most-frequent-even-element.cpp
+++ 2486-most-frequent-even-element/2486-most-frequent-even-element.cpp
@@ -1,21 +1,24 @@
 class Solution {
 public:
     int mostFrequentEven(vector<int>& nums) {
-        if (nums.empty()) return -1;
-        
-        unordered_map<int, int> count_map;
-        int max_count = 0;
-        int most_frequent_even = -1;
-        
-        for (int num : nums) {
-            if (num % 2 == 0) {
-                count_map[num]++;
-                if (count_map[num] > max_count || (count_map[num] == max_count && num < most_frequent_even)) {
-                    max_count = count_map[num];
-                    most_frequent_even = num;
-                }
+        unordered_map<int, int> map;
+        for(auto num : nums){
+            if(num%2 == 0){
+                map[num]++;
             }
         }
-        return most_frequent_even;
+        unordered_map<int, int>::iterator it;
+        int maxFreqEvenNumber = -1;
+        int maxFreq = 0;
+        for(it = map.begin(); it != map.end(); it++){
+            if(it->second > maxFreq){
+                maxFreq = it->second;
+                maxFreqEvenNumber = it->first;
+            }
+            else if(it->second == maxFreq){
+                maxFreqEvenNumber = min(maxFreqEvenNumber, it->first);
+            }
+        }
+        return maxFreqEvenNumber;
     }
 };
