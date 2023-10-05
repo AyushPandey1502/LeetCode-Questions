9d62e48773355795cb97e4f1e41c10858400c271
AyushPandey1502 <114850053+AyushPandey1502@users.noreply.github.com>
2023-10-05 18:35:41 +0530

    Time: 9 ms (61.98%), Space: 16.3 MB (18.73%) - LeetHub

--- 0229-majority-element-ii/0229-majority-element-ii.cpp
+++ 0229-majority-element-ii/0229-majority-element-ii.cpp
@@ -1,11 +1,50 @@
 class Solution {
 public:
-    vector<int> majorityElement(vector<int>& nums) {
-        int n = nums.size();
+    vector<int> majorityElement(std::vector<int>& nums) {
         vector<int> result;
-        set<int> set(nums.begin(), nums.end());
-        for(auto x : set)
-            if(count(nums.begin(), nums.end(), x) > (n/3)) result.push_back(x);
+        
+        if (nums.empty()) {
+            return result;
+        }
+        
+        int count1 = 0, count2 = 0, candidate1 = 0, candidate2 = 1;
+        
+        for (int n : nums) {
+            if (n == candidate1) {
+                count1++;
+            } else if (n == candidate2) {
+                count2++;
+            } else if (count1 == 0) {
+                candidate1 = n;
+                count1 = 1;
+            } else if (count2 == 0) {
+                candidate2 = n;
+                count2 = 1;
+            } else {
+                count1--;
+                count2--;
+            }
+        }
+        
+        count1 = 0;
+        count2 = 0;
+        
+        // Recount to confirm the majority elements
+        for (int n : nums) {
+            if (n == candidate1) {
+                count1++;
+            } else if (n == candidate2) {
+                count2++;
+            }
+        }
+        
+        if (count1 > nums.size() / 3) {
+            result.push_back(candidate1);
+        }
+        if (count2 > nums.size() / 3) {
+            result.push_back(candidate2);
+        }
+        
         return result;
     }
 };
