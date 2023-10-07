66c144fec008e26c9723651e4818a0f8b7569257
AyushPandey1502 <114850053+AyushPandey1502@users.noreply.github.com>
2023-10-07 12:03:12 +0530

    Time: 3 ms (61.09%), Space: 13.8 MB (13.3%) - LeetHub

--- 0404-sum-of-left-leaves/0404-sum-of-left-leaves.cpp
+++ 0404-sum-of-left-leaves/0404-sum-of-left-leaves.cpp
@@ -1,15 +1,12 @@
 class Solution {
 public:
     int sumOfLeftLeaves(TreeNode* root) {
-        if (!root) return 0;
-
+        if(!root) return 0;
         int leftSum = 0;
-        if (root->left && !root->left->left && !root->left->right) {
+        if(root->left && !root->left->left && !root->left->right){
             leftSum += root->left->val;
         }
-        leftSum += sumOfLeftLeaves(root->left);
-        leftSum += sumOfLeftLeaves(root->right);
-
+        leftSum += sumOfLeftLeaves(root->left) + sumOfLeftLeaves(root->right);
         return leftSum;
     }
 };
