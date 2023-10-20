71c2ac94c796a08c5b624de9853281c359df2903
AyushPandey1502 <114850053+AyushPandey1502@users.noreply.github.com>
2023-10-20 12:35:23 +0530

    Time: 0 ms (100%), Space: 6.2 MB (61.89%) - LeetHub

--- 0029-divide-two-integers/0029-divide-two-integers.cpp
+++ 0029-divide-two-integers/0029-divide-two-integers.cpp
@@ -1,20 +1,30 @@
-class Solution
-{ // There is Reason why this questioh has so low accuracy
+class Solution {
 public:
-    int divide(int dividend, int divisor)
-    {
-        int sgn = ((dividend < 0) ^ (divisor < 0));
-        long long int d = abs(dividend), dv = abs(divisor), ans = 0;
-        if (dv == 1)
-            ans = d;
-        else
-            while (d >= dv) 
-            {
-                ++ans;
-                d -= dv;
+    int divide(int dividend, int divisor) {
+        if (dividend == INT_MIN && divisor == -1) {
+            return INT_MAX;  // Overflow case
+        }
+
+        int sign = (dividend < 0) ^ (divisor < 0) ? -1 : 1;
+        long long ldividend = llabs(static_cast<long long>(dividend));
+        long long ldivisor = llabs(static_cast<long long>(divisor));
+
+        long long quotient = 0;
+        long long temp = 0;
+
+        for (int i = 31; i >= 0; i--) {
+            if ((temp + (ldivisor << i)) <= ldividend) {
+                temp += ldivisor << i;
+                quotient |= 1LL << i;
             }
-        if (sgn == 1)
-            ans = -ans;
-        return ((ans < INT_MIN) ? INT_MIN : ((ans > INT_MAX) ? INT_MAX : ans ));
+        }
+
+        if (sign == -1 && quotient > static_cast<long long>(INT_MAX)) {
+            return INT_MIN;
+        } else if (sign == 1 && quotient > static_cast<long long>(INT_MAX) - 1) {
+            return INT_MAX;
+        }
+
+        return static_cast<int>(sign * quotient);
     }
 };
