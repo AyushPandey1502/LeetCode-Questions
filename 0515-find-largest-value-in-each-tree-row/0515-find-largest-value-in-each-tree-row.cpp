1ce5d6a80523464bc62bc6c776920843db1a78b3
AyushPandey1502 <114850053+AyushPandey1502@users.noreply.github.com>
2023-10-24 23:09:23 +0530

    Time: 16 ms (9.36%), Space: 22.5 MB (50.91%) - LeetHub

--- 0515-find-largest-value-in-each-tree-row/0515-find-largest-value-in-each-tree-row.cpp
+++ 0515-find-largest-value-in-each-tree-row/0515-find-largest-value-in-each-tree-row.cpp
@@ -1,26 +1,27 @@
 class Solution {
 public:
     vector<int> largestValues(TreeNode* root) {
-        queue<TreeNode*>q;
-        vector<int>temp;
-       vector<vector<int>>ans;
-        if(root==NULL) return temp;
-        q.push(root);
-        while(q.empty()!=true)
-        {   vector<int>level;
-            int n=q.size();
-            for(int i=0;i<n;i++)
-            {   TreeNode* t=q.front();q.pop();
-                level.push_back(t->val);
-                if(t->left) q.push(t->left);
-                if(t->right) q.push(t->right);
+        if(root==NULL) 
+			return {};
+        vector<int> res;
+        queue<TreeNode*> q;
+        q.push(root); 
+        while (!q.empty())
+        {
+            int size = q.size(); 
+            int maxi=INT_MIN;
+            for(int i=0;i<size;i++){
+                TreeNode* tmp=q.front();
+                if(tmp->left)
+                    q.push(tmp->left);
+                if(tmp->right)
+                    q.push(tmp->right);
+                if(maxi<tmp->val)
+                    maxi=tmp->val;
+                q.pop();
             }
-            ans.push_back(level);
-         int maximum=level[0];
-         for(int i=1;i<level.size();i++)
-             maximum=max(maximum,level[i]);
-         temp.push_back(maximum);
+            res.push_back(maxi);
         }
-        return temp;
+        return res;
     }
 };
