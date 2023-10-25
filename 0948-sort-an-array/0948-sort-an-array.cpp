ce34bed81811c4cd2384a566500a22482b392f39
AyushPandey1502 <114850053+AyushPandey1502@users.noreply.github.com>
2023-10-26 00:24:14 +0530

    Time: 128 ms (79%), Space: 76 MB (49.86%) - LeetHub

--- 0948-sort-an-array/0948-sort-an-array.cpp
+++ 0948-sort-an-array/0948-sort-an-array.cpp
@@ -1,16 +1,13 @@
 class Solution {
 public:
     void radixSort(vector<int>& nums, int n) {
-        int max_num = *max_element(nums.begin(), nums.end());
         int min_num = *min_element(nums.begin(), nums.end());
-
         if (min_num < 0) {
             for (int& num : nums) {
                 num += abs(min_num);
             }
-            max_num += abs(min_num); 
         }
-
+        int max_num = *max_element(nums.begin(), nums.end());
         for (int pos = 1; max_num / pos > 0; pos *= 10) {
             countSort(nums, n, pos);
         }
