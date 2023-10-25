30e4609a7b0d5bbd91573ea1cc42dfd572e3e532
AyushPandey1502 <114850053+AyushPandey1502@users.noreply.github.com>
2023-10-26 00:38:32 +0530

    Time: 212 ms (22.23%), Space: 101.9 MB (13.81%) - LeetHub

---
+++ 0164-maximum-gap/0164-maximum-gap.cpp
@@ -0,0 +1,59 @@
+class Solution {
+public:
+    int maximumGap(vector<int>& nums) {
+        int n = nums.size();
+        if(n < 2) return 0;
+        radixSort(nums, n);
+        int maxDiff = nums[1] - nums[0];
+        for(int i = 2; i < n; i++){
+            int diff = nums[i] - nums[i-1];
+            if(diff > maxDiff) maxDiff = diff;
+        }
+        return maxDiff;
+    }
+    void radixSort(vector<int>& nums, int n) {
+        int min_num = *min_element(nums.begin(), nums.end());
+        if (min_num < 0) {
+            for (int& num : nums) {
+                num += abs(min_num);
+            }
+        }
+        int max_num = *max_element(nums.begin(), nums.end());
+        for (int pos = 1; max_num / pos > 0; pos *= 10) {
+            countSort(nums, n, pos);
+        }
+
+        if (min_num < 0) {
+            for (int& num : nums) {
+                num -= abs(min_num);
+            }
+        }
+    }
+
+    void countSort(vector<int>& nums, int n, int pos) {
+        vector<int> count(10, 0);
+        vector<int> newArray(n);
+
+        for (int i = 0; i < n; i++) {
+            count[(nums[i] / pos) % 10]++;
+        }
+
+        for (int i = 1; i < 10; i++) {
+            count[i] += count[i - 1];
+        }
+
+        for (int i = n - 1; i >= 0; i--) {
+            newArray[--count[(nums[i] / pos) % 10]] = nums[i];
+        }
+
+        for (int i = 0; i < n; i++) {
+            nums[i] = newArray[i];
+        }
+    }
+
+    vector<int> sortArray(vector<int>& nums) {
+        int size = nums.size();
+        radixSort(nums, size);
+        return nums;
+    }
+};
