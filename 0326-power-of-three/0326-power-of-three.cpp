58e6091b051baeb0b3c8594860b50b381448be92
Ayush Pandey <114850053+AyushPandey1502@users.noreply.github.com>
2024-05-31 13:04:26 +0530

    Time: 11 ms (71.71%), Space: 8.2 MB (38.79%) - LeetHub

--- 0326-power-of-three/0326-power-of-three.cpp
+++ 0326-power-of-three/0326-power-of-three.cpp
@@ -4,7 +4,7 @@ public:
         if(n <= 0) return false;
         while(n > 1){
             int remainder = n % 3;
-            if(remainder > 0 && remainder < 3) return false;
+            if(remainder != 0) return false;
             n /= 3;
         }
         return true;
