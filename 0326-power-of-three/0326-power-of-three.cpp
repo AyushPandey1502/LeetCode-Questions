70b42b5417db21144ce61843d91a27419bd67728
Ayush Pandey <114850053+AyushPandey1502@users.noreply.github.com>
2024-05-31 13:06:26 +0530

    Time: 3 ms (98.05%), Space: 8.1 MB (76.09%) - LeetHub

--- 0326-power-of-three/0326-power-of-three.cpp
+++ 0326-power-of-three/0326-power-of-three.cpp
@@ -2,11 +2,6 @@ class Solution {
 public:
     bool isPowerOfThree(int n) {
         if(n <= 0) return false;
-        while(n > 1){
-            int remainder = n % 3;
-            if(remainder != 0) return false;
-            n /= 3;
-        }
-        return true;
+        return ((n == 1) || (n % 3 == 0 && isPowerOfThree(n / 3)));
     }
 };
