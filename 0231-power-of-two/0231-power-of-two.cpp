9ce443eccbade18838c9c2a4d0bfe74b49f5d863
Ayush Pandey <114850053+AyushPandey1502@users.noreply.github.com>
2024-05-31 12:52:52 +0530

    Time: 0 ms (100%), Space: 7.2 MB (77.01%) - LeetHub

--- 0231-power-of-two/0231-power-of-two.cpp
+++ 0231-power-of-two/0231-power-of-two.cpp
@@ -1,7 +1,8 @@
 class Solution {
 public:
     bool isPowerOfTwo(int n) {
-        if(n == 0) return false;
-        return ceil(log2(n)) == floor(log2(n));
+        if(n <= 0) return false;
+        int setBits = __builtin_popcount(n);
+        return (setBits == 1);
     }
 };
