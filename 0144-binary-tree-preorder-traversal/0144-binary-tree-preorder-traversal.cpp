15ad9fcbd1c504ab616cfc8e8b6e6539863a1022
Ayush Pandey <114850053+AyushPandey1502@users.noreply.github.com>
2024-06-01 23:35:14 +0530

    Time: 0 ms (100%), Space: 9.9 MB (48%) - LeetHub

--- 0144-binary-tree-preorder-traversal/0144-binary-tree-preorder-traversal.cpp
+++ 0144-binary-tree-preorder-traversal/0144-binary-tree-preorder-traversal.cpp
@@ -1,17 +1,15 @@
-// Time Complexity : O(n)
-// Space Complexity : O(n)
 class Solution {
 public:
-    void storePreorder(TreeNode* root, vector<int> &result){
+    vector<int> result;
+    void storePreorder(TreeNode* root){
         if(root == nullptr) return;
         result.push_back(root -> val);
-        storePreorder(root -> left, result);
-        storePreorder(root -> right, result);
+        storePreorder(root -> left);
+        storePreorder(root -> right);
     }
 
     vector<int> preorderTraversal(TreeNode* root) {
-        vector<int> result;
-        storePreorder(root, result);
+        storePreorder(root);
         return result;
     }
 };
