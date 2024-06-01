d54575ea1234991b624e2bc4d9b797460f0dd0ce
Ayush Pandey <114850053+AyushPandey1502@users.noreply.github.com>
2024-06-01 13:05:22 +0530

    Time: 3 ms (18.97%), Space: 7.2 MB (7.62%) - LeetHub

--- 1441-minimum-flips-to-make-a-or-b-equal-to-c/1441-minimum-flips-to-make-a-or-b-equal-to-c.cpp
+++ 1441-minimum-flips-to-make-a-or-b-equal-to-c/1441-minimum-flips-to-make-a-or-b-equal-to-c.cpp
@@ -2,17 +2,18 @@ class Solution {
 public:
     int minFlips(int a, int b, int c) {
         int count = 0;
-        for(int i = 0; i < 32; i++){
-            int bitA = (a & (1 << i)) ? 1 : 0;
-            int bitB = (b & (1 << i)) ? 1 : 0;
-            int bitC = (c & (1 << i)) ? 1 : 0;
-            if((bitA | bitB) != bitC){
-                if(bitC == 0){
-                    if(bitA == 1 && bitB == 1) count += 2;
-                    else count += 1;
+        for (int i = 0; i < 32; ++i) {
+            int bitC = (c >> i) & 1;
+            int bitA = (a >> i) & 1;
+            int bitB = (b >> i) & 1;
+
+            if (bitC == 0) {
+                count += bitA + bitB;
+            } else {
+                if (bitA == 0 && bitB == 0) {
+                    count += 1;
                 }
-                else count += 1;
-            } 
+            }
         }
         return count;
     }
