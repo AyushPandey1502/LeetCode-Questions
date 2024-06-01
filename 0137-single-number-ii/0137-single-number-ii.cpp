1b61bb13e205ad0a88bc21ebb19a57cf607c2e6e
Ayush Pandey <114850053+AyushPandey1502@users.noreply.github.com>
2024-06-01 11:19:36 +0530

    Time: 4 ms (79.68%), Space: 12.1 MB (29.99%) - LeetHub

--- 0137-single-number-ii/0137-single-number-ii.cpp
+++ 0137-single-number-ii/0137-single-number-ii.cpp
@@ -1,25 +1,10 @@
 class Solution {
 public:
     int singleNumber(vector<int>& nums) {
-        if (nums.empty()) {
-            return -1; 
-        }
-
         sort(nums.begin(), nums.end());
-
-        if (nums.size() == 1 || nums[0] != nums[1]) {
-            return nums[0];
+        for (int i = 1; i < nums.size(); i += 3) {
+            if(nums[i] != nums[i-1]) return nums[i-1];
         }
-
-        for (int i = 1; i < nums.size(); ++i) {
-            if (i == nums.size() - 1) {
-                if (nums[i] != nums[i - 1]) {
-                    return nums[i];
-                }
-            } else if (nums[i - 1] != nums[i] && nums[i] != nums[i + 1]) {
-                return nums[i];
-            }
-        }
-        return -1;
+        return nums[nums.size()-1];
     }
 };
