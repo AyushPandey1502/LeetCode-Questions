769351c1efbbf2942396f759c4eced5fd0c52bc9
Ayush Pandey <114850053+AyushPandey1502@users.noreply.github.com>
2024-06-02 13:26:18 +0530

    Time: 79 ms (64.44%), Space: 56.1 MB (54.82%) - LeetHub

---
+++ 2574-count-subarrays-with-median-k/2574-count-subarrays-with-median-k.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    int countSubarrays(vector<int>& nums, int k) {
+        unordered_map<int, int> map;
+        int count = 0;
+        map[0]++;
+        int diff = 0;
+        bool found = false;
+        for(int i = 0; i < nums.size(); i++){
+            if(nums[i] > k) diff++;
+            else if(nums[i] < k) diff--;
+            else found = true;
+
+            if(found) count += map[diff] + map[diff-1];
+            else map[diff]++;
+        }
+        return count;
+    }
+};
