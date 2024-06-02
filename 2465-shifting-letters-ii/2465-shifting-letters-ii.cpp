5326793aea8f1efa0bd0abaff0587849215260be
Ayush Pandey <114850053+AyushPandey1502@users.noreply.github.com>
2024-06-02 14:50:53 +0530

    Time: 237 ms (59.53%), Space: 96.6 MB (96.77%) - LeetHub

--- 2465-shifting-letters-ii/2465-shifting-letters-ii.cpp
+++ 2465-shifting-letters-ii/2465-shifting-letters-ii.cpp
@@ -1,20 +1,22 @@
 class Solution {
 public:
     string shiftingLetters(string s, vector<vector<int>>& shifts) {
-        vector<int> shiftAmount(s.size() + 1, 0);
+        vector<int> shiftEffect(s.size() + 1, 0);
         for (auto& shift : shifts) {
-            int start = shift[0], end = shift[1], direction = shift[2] == 1 ? 1 : -1;
-            shiftAmount[start] += direction;
-            if (end + 1 < s.size()) {
-                shiftAmount[end + 1] -= direction;
-            }
+            int start = shift[0];
+            int end = shift[1];
+            int direction = shift[2] == 1 ? 1 : -1;
+
+            shiftEffect[start] += direction;
+            shiftEffect[end + 1] -= direction;
         }
         int netShift = 0;
         for (int i = 0; i < s.size(); ++i) {
-            netShift += shiftAmount[i];
-            int shiftValue = ((netShift % 26) + 26) % 26;
-            s[i] = (s[i] - 'a' + shiftValue) % 26 + 'a';
+            netShift += shiftEffect[i];
+            int newChar = ((s[i] - 'a' + netShift) % 26 + 26) % 26;
+            s[i] = 'a' + newChar;
         }
+
         return s;
     }
 };
