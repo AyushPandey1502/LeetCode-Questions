a3ef16be5cde2e6f3c60235cffc93b9a0c622324
Ayush Pandey <114850053+AyushPandey1502@users.noreply.github.com>
2024-06-04 09:25:22 +0530

    Time: 9 ms (16.33%), Space: 9.8 MB (6.93%) - LeetHub

--- 0409-longest-palindrome/0409-longest-palindrome.cpp
+++ 0409-longest-palindrome/0409-longest-palindrome.cpp
@@ -1,13 +1,16 @@
 class Solution {
 public:
     int longestPalindrome(string s) {
-        unordered_map<char, int> map;
-        int n = s.length();
-        for(auto ch : s) map[ch]++;
-        int oddFreq = 0;
-        for(auto it : map){
-            if(it.second % 2 == 1) oddFreq++;
+        unordered_set<int> charSet;
+        int count = 0;
+        for(char ch : s){
+            if(charSet.find(ch) != charSet.end()){
+                charSet.erase(ch);
+                count += 2;
+            }else{
+                charSet.insert(ch);
+            }
         }
-        return oddFreq > 0 ? n - oddFreq + 1 : n;
+        return (charSet.empty()) ? count : count + 1;
     }
 };
