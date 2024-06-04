01324bbed04a3c403de9ac1aee04d8caa0f9331d
Ayush Pandey <114850053+AyushPandey1502@users.noreply.github.com>
2024-06-04 10:04:29 +0530

    Time: 282 ms (63.22%), Space: 171.6 MB (33.65%) - LeetHub

--- 2237-longest-palindrome-by-concatenating-two-letter-words/2237-longest-palindrome-by-concatenating-two-letter-words.cpp
+++ 2237-longest-palindrome-by-concatenating-two-letter-words/2237-longest-palindrome-by-concatenating-two-letter-words.cpp
@@ -3,23 +3,19 @@ public:
     int longestPalindrome(vector<string>& words) {
         unordered_map<string, int> map;
         int result = 0;
-        
-        for (auto word : words) {
+        for(auto word : words){
             string wordRev = word;
             reverse(wordRev.begin(), wordRev.end());
-            if (map[wordRev] > 0) {
+            if(map[wordRev] > 0){
                 map[wordRev]--;
                 result += 4;
-            } else {
+            }else{
                 map[word]++;
             }
         }
-        for (auto it : map) {
-            if (it.first[0] == it.first[1] && it.second > 0) {
-                return result + 2;
-            }
+        for(auto it : map){
+            if(it.first[0] == it.first[1] && it.second > 0) return result + 2;
         }
-
         return result;
     }
 };
