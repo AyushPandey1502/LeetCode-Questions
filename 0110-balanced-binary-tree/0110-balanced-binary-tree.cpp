9837f4f150f594b5b4d99b4b84b577c59514ef9a
Ayush Pandey <114850053+AyushPandey1502@users.noreply.github.com>
2024-06-05 16:11:33 +0530

    Time: 5 ms (84.74%), Space: 21.7 MB (14.07%) - LeetHub

--- 0110-balanced-binary-tree/0110-balanced-binary-tree.cpp
+++ 0110-balanced-binary-tree/0110-balanced-binary-tree.cpp
@@ -10,9 +10,8 @@ public:
         if(root == NULL) return 0;
 
         int lh = dfsHeight(root -> left);
-        if(lh == -1) return -1;
         int rh = dfsHeight(root -> right);
-        if(rh == -1) return -1;
+        if(lh == -1 || rh == -1) return -1;
 
         if(abs(lh - rh) > 1) return -1;
         return 1 + max(lh, rh);
