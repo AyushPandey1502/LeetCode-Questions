ed47bb3a2b0d583e0ce7d694575f17f93fab182f
Ayush Pandey <114850053+AyushPandey1502@users.noreply.github.com>
2024-06-05 15:32:49 +0530

    Time: 2 ms (42.97%), Space: 9.9 MB (63.53%) - LeetHub

--- 0145-binary-tree-postorder-traversal/0145-binary-tree-postorder-traversal.cpp
+++ 0145-binary-tree-postorder-traversal/0145-binary-tree-postorder-traversal.cpp
@@ -1,31 +1,37 @@
-// ONE STACK APPROACH
-// TIME COMPLEXITY : O(2N)
-// SPACE COMPLEXITY : O(N)
+// PREORDER, INORDER AND POSTORDER USING SINGLE STACK
+// TIME COMPLEXITY : O(3N)
+// SPACE COMPLEXITY : O(3N)
 class Solution {
 public:
     vector<int> postorderTraversal(TreeNode* root) {
-        vector<int> postorder;
-        if(root == nullptr) return postorder;
-        stack<TreeNode*> st;
-        TreeNode* current = root;
-        while(current != NULL || !st.empty()){
-            if(current != NULL){
-                st.push(current);
-                current = current->left;
-            }else{
-                TreeNode* temp = st.top() -> right;
-                if(temp == NULL){
-                    temp = st.top();
-                    st.pop();
-                    postorder.push_back(temp->val);
-                    while(!st.empty() && temp == st.top()->right){
-                        temp = st.top();
-                        st.pop();
-                        postorder.push_back(temp->val);
-                    }
-                }else{
-                    current = temp;
-                }
+        vector<int> preorder, inorder, postorder;
+        stack<pair<TreeNode*, int>> stack;
+        stack.push({root, 1});
+        if (root == NULL)
+            return postorder;
+        while (!stack.empty()) {
+            auto it = stack.top();
+            stack.pop();
+
+            // PREORDER
+            if (it.second == 1) {
+                preorder.push_back(it.first->val);
+                it.second++;
+                stack.push(it);
+                if (it.first->left != NULL)
+                    stack.push({it.first->left, 1});
+            }
+            // INORDER
+            else if (it.second == 2) {
+                inorder.push_back(it.first->val);
+                it.second++;
+                stack.push(it);
+                if (it.first->right != NULL)
+                    stack.push({it.first->right, 1});
+            }
+            // POSTORDER
+            else {
+                postorder.push_back(it.first->val);
             }
         }
         return postorder;
