f6ae75ce4767563205a8b3f23ef6f7ba772b8396
Ayush Pandey <114850053+AyushPandey1502@users.noreply.github.com>
2024-06-07 00:44:02 +0530

    Time: 4 ms (89.97%), Space: 38.2 MB (13.64%) - LeetHub

--- 0113-path-sum-ii/0113-path-sum-ii.cpp
+++ 0113-path-sum-ii/0113-path-sum-ii.cpp
@@ -1,26 +1,23 @@
 class Solution {
 public:
     vector<vector<int>> result;
-
-    void getPaths(TreeNode* root, vector<int>& path, int targetSum) {
+    void getPaths(TreeNode* root, vector<int> arr, int targetSum) {
         if (root == NULL)
             return;
-
-        path.push_back(root->val);
+        arr.push_back(root->val);
         targetSum -= root->val;
-
         if (root->left == NULL && root->right == NULL && targetSum == 0) {
-            result.push_back(path);
+            result.push_back(arr);
         } else {
-            getPaths(root->left, path, targetSum);
-            getPaths(root->right, path, targetSum);
+            getPaths(root->left, arr, targetSum);
+            getPaths(root->right, arr, targetSum);
         }
-        path.pop_back();
     }
-
     vector<vector<int>> pathSum(TreeNode* root, int targetSum) {
-        vector<int> path;
-        getPaths(root, path, targetSum);
+        if (root == NULL)
+            return result;
+        vector<int> arr;
+        getPaths(root, arr, targetSum);
         return result;
     }
 };
