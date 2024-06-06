a6ebc4fc5432895b3010654fa09f0ffb51d5dd08
Ayush Pandey <114850053+AyushPandey1502@users.noreply.github.com>
2024-06-06 15:31:26 +0530

    Time: 5 ms (32.17%), Space: 15.4 MB (6.43%) - LeetHub

--- 1029-vertical-order-traversal-of-a-binary-tree/1029-vertical-order-traversal-of-a-binary-tree.cpp
+++ 1029-vertical-order-traversal-of-a-binary-tree/1029-vertical-order-traversal-of-a-binary-tree.cpp
@@ -1,21 +1,21 @@
 // TIME COMPLEXITY : O(N*LOG(N))
 // SPACE COMPLEXITY : O(N)
-// INORDER TRAVERSAL
+// POSTORDER TRAVERSAL
 
 class Solution {
 public:
     map<int, map<int, multiset<int>>> nodes;
 
-    void inorderTraversal(TreeNode* root, int x, int y){
+    void postorderTraversal(TreeNode* root, int x, int y){
         if(root == NULL) return;
-        inorderTraversal(root -> left, x - 1, y + 1);
+        postorderTraversal(root -> left, x - 1, y + 1);
+        postorderTraversal(root -> right, x + 1, y + 1);
         nodes[x][y].insert(root -> val);
-        inorderTraversal(root -> right, x + 1, y + 1);
     }
     
 
     vector<vector<int>> verticalTraversal(TreeNode* root) {
-        inorderTraversal(root, 0, 0);
+        postorderTraversal(root, 0, 0);
         vector<vector<int>> result;
         for(auto p : nodes){
             vector<int> col;
