88fb6a4c41015f1f1be294c32601de6ce6554cf3
Ayush Pandey <114850053+AyushPandey1502@users.noreply.github.com>
2024-06-06 16:32:21 +0530

    Time: 0 ms (100%), Space: 14.2 MB (27.01%) - LeetHub

--- 0199-binary-tree-right-side-view/0199-binary-tree-right-side-view.cpp
+++ 0199-binary-tree-right-side-view/0199-binary-tree-right-side-view.cpp
@@ -1,26 +1,25 @@
+// ITERATIVE APPROACH
 class Solution {
 public:
     vector<int> rightSideView(TreeNode* root) {
         vector<int> result;
-        if (!root) return result;
+        if (root == NULL) return result;
 
         queue<TreeNode*> q;
         q.push(root);
 
         while (!q.empty()) {
-            int levelSize = q.size();
-            TreeNode* lastNode = nullptr; 
-
-            for (int i = 0; i < levelSize; ++i) {
+            int size = q.size();
+            vector<int> level(size);
+            for (int i = 0; i < size; i++) {
                 TreeNode* node = q.front();
                 q.pop();
-                lastNode = node;
+                level[i] = node -> val;
                 if (node->left) q.push(node->left);
                 if (node->right) q.push(node->right);
             }
-            result.push_back(lastNode->val);
+            result.push_back(level[size-1]);
         }
-
         return result;
     }
 };
