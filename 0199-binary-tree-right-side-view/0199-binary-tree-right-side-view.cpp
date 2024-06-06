28fec5a23d31969925111df4b789f51aa5a1a949
Ayush Pandey <114850053+AyushPandey1502@users.noreply.github.com>
2024-06-06 16:42:42 +0530

    Time: 0 ms (100%), Space: 14 MB (74.63%) - LeetHub

--- 0199-binary-tree-right-side-view/0199-binary-tree-right-side-view.cpp
+++ 0199-binary-tree-right-side-view/0199-binary-tree-right-side-view.cpp
@@ -1,25 +1,16 @@
-// ITERATIVE APPROACH
+// RECURSIVE APPROACH
 class Solution {
 public:
     vector<int> rightSideView(TreeNode* root) {
         vector<int> result;
-        if (root == NULL) return result;
-
-        queue<TreeNode*> q;
-        q.push(root);
-
-        while (!q.empty()) {
-            int size = q.size();
-            vector<int> level(size);
-            for (int i = 0; i < size; i++) {
-                TreeNode* node = q.front();
-                q.pop();
-                level[i] = node -> val;
-                if (node->left) q.push(node->left);
-                if (node->right) q.push(node->right);
-            }
-            result.push_back(level[size-1]);
-        }
+        modifiedPreorder(root, 0, result);
         return result;
+    }
+
+    void modifiedPreorder(TreeNode* root, int level, vector<int> &result){
+        if(root == NULL) return;
+        if(level == result.size()) result.push_back(root -> val);
+        modifiedPreorder(root -> right, level + 1, result);
+        modifiedPreorder(root -> left, level + 1, result);
     }
 };
