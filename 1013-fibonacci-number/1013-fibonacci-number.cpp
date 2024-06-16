ade3e9c8e9fb861c9c69a89bad703c286b4ef15e
Ayush Pandey <114850053+AyushPandey1502@users.noreply.github.com>
2024-06-17 00:08:59 +0530

    Time: 2 ms (80.7%), Space: 7.3 MB (11.79%) - LeetHub

--- 1013-fibonacci-number/1013-fibonacci-number.cpp
+++ 1013-fibonacci-number/1013-fibonacci-number.cpp
@@ -1,15 +1,16 @@
-// MEMOIZATION APPROACH - TOP - DOWN
+// TABULATION APPROACH - BOTTOM - UP
 // TIME COMPLEXITY = O(N)
 // SPACE COMPLEXITY = O(N)
 class Solution {
 public:
-    int fibonacci(int n, vector<int> &dp){
-        if(n <= 1) return n;
-        if(dp[n] != -1) return dp[n];
-        return dp[n] = fibonacci(n-1, dp) + fibonacci(n-2, dp);
-    }
     int fib(int n) {
+        if(n <= 1) return n;
         vector<int> dp(n+1, -1);
-        return fibonacci(n, dp);
+        dp[0] = 0;
+        dp[1] = 1;
+        for(int i = 2; i <= n; i++){
+            dp[i] = dp[i-1] + dp[i-2];
+        }
+        return dp[n];
     }
 };
