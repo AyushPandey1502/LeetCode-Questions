fd4e7ef8cd827d1b36aad064f4c69fb2947811ad
Ayush Pandey <114850053+AyushPandey1502@users.noreply.github.com>
2024-06-17 00:30:47 +0530

    Time: 0 ms (100%), Space: 7.4 MB (32.08%) - LeetHub

--- 0070-climbing-stairs/0070-climbing-stairs.cpp
+++ 0070-climbing-stairs/0070-climbing-stairs.cpp
@@ -1,12 +1,12 @@
 class Solution {
 public:
-    int countWays(int n, vector<int> &dp){
-        if(n <= 1) return 1;
-        if(dp[n] != -1) return dp[n];
-        return dp[n] = countWays(n-1, dp) + countWays(n-2, dp);
-    }
     int climbStairs(int n) {
+        if(n <= 1) return 1;
         vector<int> dp(n+1, -1);
-        return countWays(n, dp);
+        dp[0] = dp[1] = 1;
+        for(int i = 2; i <= n; i++){
+            dp[i] = dp[i-1] + dp[i-2];
+        }
+        return dp[n];
     }
 };
