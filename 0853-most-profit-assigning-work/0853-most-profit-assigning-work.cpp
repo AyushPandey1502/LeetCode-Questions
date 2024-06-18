b804f0470b781bae95482a254c3f8ca4aea1793e
Ayush Pandey <114850053+AyushPandey1502@users.noreply.github.com>
2024-06-18 10:25:32 +0530

    Time: 51 ms (57.86%), Space: 42.6 MB (27.19%) - LeetHub

--- 0853-most-profit-assigning-work/0853-most-profit-assigning-work.cpp
+++ 0853-most-profit-assigning-work/0853-most-profit-assigning-work.cpp
@@ -1,25 +1,34 @@
 class Solution {
 public:
-    int maxProfitAssignment(vector<int>& difficulty, vector<int>& profit,
-                            vector<int>& worker) {
+    int maxProfitAssignment(vector<int>& difficulty, vector<int>& profit, vector<int>& worker) {
         vector<pair<int, int>> jobs;
         for (int i = 0; i < profit.size(); i++) {
-            jobs.push_back({difficulty[i], profit[i]});
+            jobs.emplace_back(difficulty[i], profit[i]);
         }
+
         sort(jobs.begin(), jobs.end());
         sort(worker.begin(), worker.end());
 
+        vector<int> maxProfitAtDifficulty;
         int maxProfit = 0;
+
+        for (const auto& job : jobs) {
+            maxProfit = max(maxProfit, job.second);
+            maxProfitAtDifficulty.push_back(maxProfit);
+        }
+
         int result = 0;
         int j = 0;
 
-        for (int i = 0; i < worker.size(); i++) {
-            while (j < jobs.size() && jobs[j].first <= worker[i]) {
-                maxProfit = max(maxProfit, jobs[j].second);
+        for (const int& ability : worker) {
+            while (j < jobs.size() && jobs[j].first <= ability) {
                 j++;
             }
-            result += maxProfit;
+            if (j > 0) {
+                result += maxProfitAtDifficulty[j - 1];
+            }
         }
+
         return result;
     }
 };
