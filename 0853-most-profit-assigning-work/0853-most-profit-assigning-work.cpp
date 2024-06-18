04ee03068cea1e3ee65598e6421d9edb2a436579
Ayush Pandey <114850053+AyushPandey1502@users.noreply.github.com>
2024-06-18 10:24:30 +0530

    Time: 53 ms (51.93%), Space: 41.1 MB (57.35%) - LeetHub

--- 0853-most-profit-assigning-work/0853-most-profit-assigning-work.cpp
+++ 0853-most-profit-assigning-work/0853-most-profit-assigning-work.cpp
@@ -1,17 +1,18 @@
 class Solution {
 public:
-    int maxProfitAssignment(vector<int>& difficulty, vector<int>& profit, vector<int>& worker) {
+    int maxProfitAssignment(vector<int>& difficulty, vector<int>& profit,
+                            vector<int>& worker) {
         vector<pair<int, int>> jobs;
-        for(int i = 0; i < profit.size(); i++){
+        for (int i = 0; i < profit.size(); i++) {
             jobs.push_back({difficulty[i], profit[i]});
         }
         sort(jobs.begin(), jobs.end());
         sort(worker.begin(), worker.end());
-        
+
         int maxProfit = 0;
         int result = 0;
         int j = 0;
-        
+
         for (int i = 0; i < worker.size(); i++) {
             while (j < jobs.size() && jobs[j].first <= worker[i]) {
                 maxProfit = max(maxProfit, jobs[j].second);
