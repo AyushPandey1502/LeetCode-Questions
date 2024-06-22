f2ce543e1651d6289a740018403d2dcacc037c87
Ayush Pandey <114850053+AyushPandey1502@users.noreply.github.com>
2024-06-22 19:35:52 +0530

    Time: 92 ms (71.29%), Space: 73.1 MB (27.58%) - LeetHub

---
+++ 1370-count-number-of-nice-subarrays/1370-count-number-of-nice-subarrays.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    int numberOfSubarrays(vector<int>& nums, int k) {
+        int n = nums.size();
+        vector<int> count(n + 1, 0);
+        count[0] = 1;
+        int result = 0, t = 0;
+        for (int x : nums) {
+            t += x & 1;
+            if (t - k >= 0) {
+                result += count[t - k];
+            }
+            count[t]++;
+        }
+        return result;
+    }
+};
