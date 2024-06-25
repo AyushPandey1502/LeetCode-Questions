59e58314b855815cf301c3551dd93c5e85915bbd
Ayush Pandey <114850053+AyushPandey1502@users.noreply.github.com>
2024-06-25 14:51:39 +0530

    Time: 0 ms (100%), Space: 10.1 MB (42.71%) - LeetHub

--- 1114-binary-search-tree-to-greater-sum-tree/1114-binary-search-tree-to-greater-sum-tree.cpp
+++ 1114-binary-search-tree-to-greater-sum-tree/1114-binary-search-tree-to-greater-sum-tree.cpp
@@ -1,33 +1,18 @@
-// BRUTE FORCE APPROACH
+// OPTIMIZED APPROACH
 class Solution {
 public:
-    void inorderTrav(TreeNode* root, vector<int> &nodes) {
+    int sum = 0;
+    void formGst(TreeNode* root) {
         if (root == NULL) return;
-        inorderTrav(root->left, nodes);
-        nodes.push_back(root->val);
-        inorderTrav(root->right, nodes);
-    }
-
-    void formGreaterSumTree(TreeNode* root, unordered_map<int, int> &sumMap) {
-        if (root == NULL) return;
-        formGreaterSumTree(root->left, sumMap);
-        root->val = sumMap[root->val];
-        formGreaterSumTree(root->right, sumMap);
+        formGst(root->right);
+        root->val += sum;
+        sum = root->val;
+        formGst(root->left);
     }
 
     TreeNode* bstToGst(TreeNode* root) {
-        vector<int> nodes;
-        inorderTrav(root, nodes);
-        vector<int> prefixSum(nodes.size(), 0);
-        prefixSum[nodes.size() - 1] = nodes.back();
-        for (int i = nodes.size() - 2; i >= 0; i--) {
-            prefixSum[i] = nodes[i] + prefixSum[i + 1];
-        }
-        unordered_map<int, int> sumMap;
-        for (int i = 0; i < nodes.size(); i++) {
-            sumMap[nodes[i]] = prefixSum[i];
-        }
-        formGreaterSumTree(root, sumMap);
+        sum = 0;
+        formGst(root);
         return root;
     }
 };
