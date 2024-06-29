b694e45ad5bfd97adb007838fbc50be8f257ffc0
Ayush Pandey <114850053+AyushPandey1502@users.noreply.github.com>
2024-06-29 13:55:58 +0530

    Time: 211 ms (44.24%), Space: 74.5 MB (63.34%) - LeetHub

--- 1431-all-ancestors-of-a-node-in-a-directed-acyclic-graph/1431-all-ancestors-of-a-node-in-a-directed-acyclic-graph.cpp
+++ 1431-all-ancestors-of-a-node-in-a-directed-acyclic-graph/1431-all-ancestors-of-a-node-in-a-directed-acyclic-graph.cpp
@@ -1,17 +1,19 @@
+// BETTER APPROACH 2 : REVERSE DIRECTION OF ARROWS TO FIND ANCESTOR
+// TIME COMPLEXITY : O(V*(V+E))
+// SPACE COMPLEXITY : O(V+E)
 class Solution {
 public:
-    vector<vector<int>> result;
-
-    void dfs(int currNode, unordered_map<int, vector<int>>& adjList, vector<bool>& visited) {
-        visited[currNode] = true;
-        for (int& v : adjList[currNode]) {
-            if (!visited[v]) {
+    void dfs(int u, unordered_map<int, vector<int>>& adjList,
+             vector<bool>& visited) {
+        visited[u] = true;
+        for (int& v : adjList[u]) {
+            if(visited[v] != true){
                 dfs(v, adjList, visited);
             }
         }
     }
-
     vector<vector<int>> getAncestors(int n, vector<vector<int>>& edges) {
+        vector<vector<int>> result;
         unordered_map<int, vector<int>> adjList; // O(V+E)
         for (vector<int>& vec : edges) {
             int u = vec[0];
@@ -19,20 +21,16 @@ public:
             adjList[v].push_back(u); // V --> U
         }
 
-        result.clear();
-        result.resize(n);
-
         for (int u = 0; u < n; u++) { // O(V)
             vector<int> ancestor;
             vector<bool> visited(n, false);
             dfs(u, adjList, visited); // O(V+E)
 
             for (int i = 0; i < n; i++) { // O(V)
-                if (i != u && visited[i]) {
-                    ancestor.push_back(i);
-                }
+                if (i == u) continue;
+                if (visited[i] == true) ancestor.push_back(i);
             }
-            result[u] = ancestor;
+            result.push_back(ancestor);
         }
         return result;
     }
