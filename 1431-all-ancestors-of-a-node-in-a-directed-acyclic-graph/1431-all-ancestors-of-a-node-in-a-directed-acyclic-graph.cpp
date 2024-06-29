6f7ff6bb92fe7c88d853697fd10dac07e75fb351
Ayush Pandey <114850053+AyushPandey1502@users.noreply.github.com>
2024-06-29 13:48:48 +0530

    Time: 215 ms (44.13%), Space: 80.4 MB (62.35%) - LeetHub

--- 1431-all-ancestors-of-a-node-in-a-directed-acyclic-graph/1431-all-ancestors-of-a-node-in-a-directed-acyclic-graph.cpp
+++ 1431-all-ancestors-of-a-node-in-a-directed-acyclic-graph/1431-all-ancestors-of-a-node-in-a-directed-acyclic-graph.cpp
@@ -1,30 +1,38 @@
-// BETTER APPROACH
-// TIME COMPLEXITY : O(V*(V+E))
-// SPACE COMPLEXITY : O(V+E)
 class Solution {
 public:
     vector<vector<int>> result;
-    void dfs(int ancestor, unordered_map<int, vector<int>>& adjList, int currNode) {
-        for (int& ngbr : adjList[currNode]) {
-            if (result[ngbr].empty() || result[ngbr].back() != ancestor) {
-                result[ngbr].push_back(ancestor);
-                dfs(ancestor, adjList, ngbr);
+
+    void dfs(int currNode, unordered_map<int, vector<int>>& adjList, vector<bool>& visited) {
+        visited[currNode] = true;
+        for (int& v : adjList[currNode]) {
+            if (!visited[v]) {
+                dfs(v, adjList, visited);
             }
         }
     }
+
     vector<vector<int>> getAncestors(int n, vector<vector<int>>& edges) {
         unordered_map<int, vector<int>> adjList; // O(V+E)
         for (vector<int>& vec : edges) {
             int u = vec[0];
             int v = vec[1];
-            adjList[u].push_back(v);
+            adjList[v].push_back(u); // V --> U
         }
-        
+
+        result.clear();
         result.resize(n);
-        
-        for (int i = 0; i < n; i++) { // O(V)
-            int ancestor = i;
-            dfs(ancestor, adjList, i); // O(V+E)
+
+        for (int u = 0; u < n; u++) { // O(V)
+            vector<int> ancestor;
+            vector<bool> visited(n, false);
+            dfs(u, adjList, visited); // O(V+E)
+
+            for (int i = 0; i < n; i++) { // O(V)
+                if (i != u && visited[i]) {
+                    ancestor.push_back(i);
+                }
+            }
+            result[u] = ancestor;
         }
         return result;
     }
