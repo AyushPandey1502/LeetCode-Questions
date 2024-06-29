d390254a57e2817749d0ddd9c9bdd8e205de74b1
Ayush Pandey <114850053+AyushPandey1502@users.noreply.github.com>
2024-06-29 14:14:32 +0530

    Time: 316 ms (41.71%), Space: 139.4 MB (47.31%) - LeetHub

--- 1431-all-ancestors-of-a-node-in-a-directed-acyclic-graph/1431-all-ancestors-of-a-node-in-a-directed-acyclic-graph.cpp
+++ 1431-all-ancestors-of-a-node-in-a-directed-acyclic-graph/1431-all-ancestors-of-a-node-in-a-directed-acyclic-graph.cpp
@@ -1,36 +1,51 @@
-// BETTER APPROACH 2 : REVERSE DIRECTION OF ARROWS TO FIND ANCESTOR
+// OPTIMAL APPROACH : TOPOLOGICAL SORTED ORDER
 // TIME COMPLEXITY : O(V*(V+E))
 // SPACE COMPLEXITY : O(V+E)
 class Solution {
 public:
-    void dfs(int u, unordered_map<int, vector<int>>& adjList,
-             vector<bool>& visited) {
-        visited[u] = true;
-        for (int& v : adjList[u]) {
-            if(visited[v] != true){
-                dfs(v, adjList, visited);
-            }
-        }
-    }
     vector<vector<int>> getAncestors(int n, vector<vector<int>>& edges) {
-        vector<vector<int>> result;
+        vector<vector<int>> result(n);
         unordered_map<int, vector<int>> adjList; // O(V+E)
+        vector<int> indegree(n, 0);
+
         for (vector<int>& vec : edges) {
             int u = vec[0];
             int v = vec[1];
-            adjList[v].push_back(u); // V --> U
+            adjList[u].push_back(v); // V --> U
+            indegree[v]++;
         }
 
-        for (int u = 0; u < n; u++) { // O(V)
-            vector<int> ancestor;
-            vector<bool> visited(n, false);
-            dfs(u, adjList, visited); // O(V+E)
+        // TOPOLOGICAL SORTING
+        queue<int> q;
+        for (int i = 0; i < n; i++) {
+            if (indegree[i] == 0)
+                q.push(i);
+        }
 
-            for (int i = 0; i < n; i++) { // O(V)
-                if (i == u) continue;
-                if (visited[i] == true) ancestor.push_back(i);
+        vector<int> topoOrder;
+        while (!q.empty()) {
+            int currNode = q.front();
+            q.pop();
+            topoOrder.push_back(currNode);
+
+            for (int& v : adjList[currNode]) {
+                indegree[v]--;
+                if (indegree[v] == 0) {
+                    q.push(v);
+                }
             }
-            result.push_back(ancestor);
+        }
+        vector<unordered_set<int>> vec(n);
+        for (int& node : topoOrder) {
+            for (int& v : adjList[node]) {
+                vec[v].insert(node); // adding node ancestor
+                vec[v].insert(vec[node].begin(),
+                              vec[node].end()); // adding ancestor of node
+            }
+        }
+        for (int i = 0; i < n; i++) {
+            result[i] = vector<int>(vec[i].begin(), vec[i].end());
+            sort(result[i].begin(), result[i].end());
         }
         return result;
     }
