6d0295736f224fca24194b0e9701b4b2764ab839
Ayush Pandey <114850053+AyushPandey1502@users.noreply.github.com>
2024-07-02 09:03:41 +0530

    Time: 0 ms (100%), Space: 12.6 MB (59.7%) - LeetHub

--- 0350-intersection-of-two-arrays-ii/0350-intersection-of-two-arrays-ii.cpp
+++ 0350-intersection-of-two-arrays-ii/0350-intersection-of-two-arrays-ii.cpp
@@ -1,17 +1,18 @@
 class Solution {
 public:
-    vector<int> intersect(vector<int>& nums1, vector<int>& nums2) {       
-        unordered_map<int, int> frequency;
+    vector<int> intersect(vector<int>& nums1, vector<int>& nums2) {
+        sort(nums1.begin(), nums1.end());
+        sort(nums2.begin(), nums2.end());
+        int i = 0, j = 0;
 
         vector<int> result;
-        for (const auto& num : nums1) {
-            frequency[num]++;
-        }
-        for (const auto& num : nums2) {
-            if (frequency[num] > 0) {
-                result.push_back(num);
-                frequency[num]--;
-            }
+        while(i < nums1.size() && j < nums2.size()){
+            if(nums1[i] == nums2[j]){
+                result.push_back(nums1[i]);
+                i++;
+                j++;
+            }else if(nums1[i] < nums2[j]) i++;
+            else j++;
         }
         return result;
     }
