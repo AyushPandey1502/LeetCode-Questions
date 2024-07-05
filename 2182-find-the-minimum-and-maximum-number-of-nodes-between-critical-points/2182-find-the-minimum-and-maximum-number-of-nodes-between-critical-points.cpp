b70ac3edd1eb5c37aa809d628dae964fd64cb190
Ayush Pandey <114850053+AyushPandey1502@users.noreply.github.com>
2024-07-05 13:39:18 +0530

    Time: 170 ms (46.44%), Space: 119.6 MB (35.95%) - LeetHub

--- 2182-find-the-minimum-and-maximum-number-of-nodes-between-critical-points/2182-find-the-minimum-and-maximum-number-of-nodes-between-critical-points.cpp
+++ 2182-find-the-minimum-and-maximum-number-of-nodes-between-critical-points/2182-find-the-minimum-and-maximum-number-of-nodes-between-critical-points.cpp
@@ -1,22 +1,27 @@
 class Solution {
 public:
     vector<int> nodesBetweenCriticalPoints(ListNode* head) {
-        vector<int> pts, critIndex;
-        ListNode* current = head;
-        while(current != NULL) {
-            pts.push_back(current -> val);
-            current = current -> next;
-        }
-        for(int i = 1; i < pts.size() - 1; i++) {
-            if((pts[i] > pts[i - 1] && pts[i] > pts[i + 1]) || 
-               (pts[i] < pts[i - 1] && pts[i] < pts[i + 1])) {
-                critIndex.push_back(i);
+        vector<int> critIndex;
+        int index = 0;
+        int prevVal = head->val;
+        while (head->next != NULL) {
+            int currVal = head->val;
+            int nextVal = head->next->val;
+            if ((currVal > prevVal && nextVal < currVal) ||
+                (currVal < prevVal && nextVal > currVal)) {
+                critIndex.push_back(index);
             }
+            prevVal = currVal;
+            head = head->next;
+            index++;
         }
-        if(critIndex.size() < 2) return {-1, -1};
+        if (critIndex.size() < 2)
+            return {-1, -1};
+
         int maxDist = critIndex.back() - critIndex.front();
         int minDist = INT_MAX;
-        for(int i = 1; i < critIndex.size(); i++) {
+
+        for (int i = 1; i < critIndex.size(); ++i) {
             minDist = min(minDist, critIndex[i] - critIndex[i - 1]);
         }
         return {minDist, maxDist};
