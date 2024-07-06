2f8609e110512bf1a572f70f1b42ac2f1e2121fe
Ayush Pandey <114850053+AyushPandey1502@users.noreply.github.com>
2024-07-06 23:30:00 +0530

    Time: 123 ms (16.67%), Space: 76.1 MB (58.33%) - LeetHub

--- 3264-maximum-points-after-enemy-battles/3264-maximum-points-after-enemy-battles.cpp
+++ 3264-maximum-points-after-enemy-battles/3264-maximum-points-after-enemy-battles.cpp
@@ -1,26 +1,19 @@
 class Solution {
 public:
     long long maximumPoints(vector<int>& enemyEnergies, int currentEnergy) {
-        long long points = 0;
-        int N = enemyEnergies.size();
-        int i = 0;
-        int j = N - 1;
-        if (N == 1)
-            return currentEnergy / enemyEnergies[0];
         sort(enemyEnergies.begin(), enemyEnergies.end());
-
-        while (i <= j) {
-            if (currentEnergy < enemyEnergies[i]) {
-                if (points == 0)
-                    return 0;
-                else {
-                    while (currentEnergy < enemyEnergies[i]) {
-                        currentEnergy += enemyEnergies[j--];
-                    }
-                }
-            } else {
-                points += currentEnergy / enemyEnergies[i];
-                currentEnergy %= enemyEnergies[i];
+        int left = 0, right = enemyEnergies.size() -1;
+        long long  points = 0;
+        while(left <= right){
+            if(currentEnergy >= enemyEnergies[left]){
+                points = points + currentEnergy / enemyEnergies[left];
+                currentEnergy = currentEnergy % enemyEnergies[left];
+            }
+            else if(points > 0){
+                currentEnergy = currentEnergy + enemyEnergies[right];
+                right--;
+            }else{
+                break;
             }
         }
         return points;
