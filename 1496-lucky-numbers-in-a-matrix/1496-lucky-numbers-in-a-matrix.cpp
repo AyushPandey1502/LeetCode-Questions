1b91421f63ecfe7729ca0a47781c071cf309a67e
Ayush Pandey <114850053+AyushPandey1502@users.noreply.github.com>
2024-07-19 18:36:41 +0530

    Time: 19 ms (27.26%), Space: 14.4 MB (18.75%) - LeetHub

--- 1496-lucky-numbers-in-a-matrix/1496-lucky-numbers-in-a-matrix.cpp
+++ 1496-lucky-numbers-in-a-matrix/1496-lucky-numbers-in-a-matrix.cpp
@@ -1,26 +1,29 @@
 class Solution {
 public:
-    bool checkLuckiness(vector<vector<int>>& matrix, int m, int n, int row, int col){
-        for(int i = 0; i < m; i++){
-            if(matrix[i][col] > matrix[row][col]) return false;
-        }
-        for(int j = 0; j < n; j++){
-            if(matrix[row][j] < matrix[row][col]) return false;
-        }
-        return true;
-    }
     vector<int> luckyNumbers (vector<vector<int>>& matrix) {
         int m = matrix.size();
         int n = matrix[0].size();
         vector<int> result;
-        if(m == 0) return result;
-        for(int i = 0; i < m; i++){
-            for(int j = 0; j < n; j++){
-                if(checkLuckiness(matrix, m, n, i, j)){
+        
+        vector<int> rowMins(m, INT_MAX);
+        for(int i = 0; i < m; i++) {
+            for(int j = 0; j < n; j++) {
+                rowMins[i] = min(rowMins[i], matrix[i][j]);
+            }
+        }
+        
+        for(int j = 0; j < n; j++) {
+            int colMax = INT_MIN;
+            for(int i = 0; i < m; i++) {
+                colMax = max(colMax, matrix[i][j]);
+            }
+            for(int i = 0; i < m; i++) {
+                if(matrix[i][j] == rowMins[i] && matrix[i][j] == colMax) {
                     result.push_back(matrix[i][j]);
                 }
             }
         }
+        
         return result;
     }
 };
