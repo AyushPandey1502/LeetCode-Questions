68680f829f7e68fb385989c30781aa9de765b2f2
Ayush Pandey <114850053+AyushPandey1502@users.noreply.github.com>
2024-07-23 08:24:49 +0530

    Time: 0 ms (100%), Space: 15.4 MB (5.99%) - LeetHub

--- 1741-sort-array-by-increasing-frequency/1741-sort-array-by-increasing-frequency.cpp
+++ 1741-sort-array-by-increasing-frequency/1741-sort-array-by-increasing-frequency.cpp
@@ -2,24 +2,25 @@ class Solution {
 public:
     vector<int> frequencySort(vector<int>& nums) {
         unordered_map<int, int> freq;
-        int n = nums.size();
-        for (int i = 0; i < n; i++) {
-            freq[nums[i]]++;
+        for(int num : nums){
+            freq[num]++;
         }
-        vector<pair<int, int>> freqPairs;
-        for (auto it : freq) {
-            freqPairs.push_back({it.second, it.first});
+        
+        vector<pair<int, int>> elements;
+        for(auto it : freq){
+            elements.push_back({it.second, it.first});
         }
-        sort(freqPairs.begin(), freqPairs.end(),
-             [](pair<int, int>& a, pair<int, int>& b) {
-                 if (a.first == b.first)
-                     return a.second > b.second;
-                 return a.first < b.first;
-             });
+        
+        sort(elements.begin(), elements.end(), [](pair<int, int>& a, pair<int, int>& b) {
+            if(a.first == b.first) return a.second > b.second;
+            return a.first < b.first;
+        });
+        
         vector<int> sortedNums;
-        for (auto it : freqPairs) {
+        for(auto it : elements){
             sortedNums.insert(sortedNums.end(), it.first, it.second);
         }
+        
         return sortedNums;
     }
 };
