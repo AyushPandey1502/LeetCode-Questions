520a82778d7beb196b0e48cb7b0c7b27427cb95b
Ayush Pandey <114850053+AyushPandey1502@users.noreply.github.com>
2024-07-24 22:49:22 +0530

    Time: 141 ms (92.21%), Space: 80.3 MB (90.04%) - LeetHub

--- 1333-sort-the-jumbled-numbers/1333-sort-the-jumbled-numbers.cpp
+++ 1333-sort-the-jumbled-numbers/1333-sort-the-jumbled-numbers.cpp
@@ -3,23 +3,36 @@ public:
     vector<int> sortJumbled(vector<int>& mapping, vector<int>& nums) {
         int n = nums.size();
         vector<pair<int, int>> mappedNums(n);
+
         for (int i = 0; i < n; i++) {
-            string num = to_string(nums[i]);
-            int mapNum = 0;
-            for (auto dig : num) {
-                mapNum = mapNum * 10 + mapping[dig - '0'];
+            int original = nums[i];
+            int mappedValue = 0, place = 1;
+
+            if (original == 0) {
+                mappedValue = mapping[0];
+            } else {
+                while (original > 0) {
+                    int digit = original % 10;
+                    mappedValue = mapping[digit] * place + mappedValue;
+                    original /= 10;
+                    place *= 10;
+                }
             }
-            mappedNums[i] = {mapNum, i};
+
+            mappedNums[i] = {mappedValue, i};
         }
+
         sort(mappedNums.begin(), mappedNums.end(),
-             [&](const pair<int, int> a, const pair<int, int> b) {
+             [&](const pair<int, int>& a, const pair<int, int>& b) {
                  return a.first < b.first ||
                         (a.first == b.first && a.second < b.second);
              });
+
         vector<int> result(n);
         for (int i = 0; i < n; i++) {
             result[i] = nums[mappedNums[i].second];
         }
+
         return result;
     }
 };
