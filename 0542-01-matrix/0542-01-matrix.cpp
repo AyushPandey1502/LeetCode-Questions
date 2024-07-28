661d508c20976b14ae8213d2194d24e69dfd115a
Ayush Pandey <114850053+AyushPandey1502@users.noreply.github.com>
2024-07-28 19:27:50 +0530

    Time: 55 ms (54.05%), Space: 38.4 MB (15.26%) - LeetHub

--- 0542-01-matrix/0542-01-matrix.cpp
+++ 0542-01-matrix/0542-01-matrix.cpp
@@ -8,9 +8,9 @@ public:
         vector<vector<int>> dist(n, vector<int>(m, 0));
         queue<pair<pair<int, int>, int>> q;
 
-        for(int i = 0; i < n; i++){
-            for(int j = 0; j < m; j++){
-                if(mat[i][j] == 0){
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < m; j++) {
+                if (mat[i][j] == 0) {
                     q.push({{i, j}, 0});
                     visited[i][j] = 1;
                 }
@@ -20,19 +20,20 @@ public:
         int drow[] = {-1, 1, 0, 0};
         int dcol[] = {0, 0, 1, -1};
 
-        while(!q.empty()){
+        while (!q.empty()) {
             int row = q.front().first.first;
             int col = q.front().first.second;
             int distance = q.front().second;
             q.pop();
-            
+
             dist[row][col] = distance;
 
-            for(int i = 0; i < 4; i++){
+            for (int i = 0; i < 4; i++) {
                 int nrow = row + drow[i];
                 int ncol = col + dcol[i];
 
-                if(nrow >= 0 && nrow < n && ncol >= 0 && ncol < m && !visited[nrow][ncol]){
+                if (nrow >= 0 && nrow < n && ncol >= 0 && ncol < m &&
+                    !visited[nrow][ncol]) {
                     visited[nrow][ncol] = 1;
                     q.push({{nrow, ncol}, distance + 1});
                 }
