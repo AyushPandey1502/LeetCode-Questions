40bf3b53066d0b4d5d0990d3c41126e39631816c
Ayush Pandey <114850053+AyushPandey1502@users.noreply.github.com>
2024-07-28 16:18:58 +0530

    Time: 8 ms (47.6%), Space: 17.2 MB (24.54%) - LeetHub

--- 0733-flood-fill/0733-flood-fill.cpp
+++ 0733-flood-fill/0733-flood-fill.cpp
@@ -1,10 +1,11 @@
 class Solution {
 public:
-    vector<vector<int>> floodFill(vector<vector<int>>& image, int sr, int sc, int color) {
+    vector<vector<int>> floodFill(vector<vector<int>>& image, int sr, int sc,
+                                  int color) {
         int n = image.size();
         int m = image[0].size();
         int startColor = image[sr][sc];
-        
+
         if (startColor == color) {
             return image;
         }
@@ -16,15 +17,16 @@ public:
         int drow[] = {-1, 1, 0, 0};
         int dcol[] = {0, 0, 1, -1};
 
-        while(!q.empty()){
+        while (!q.empty()) {
             int row = q.front().first;
             int col = q.front().second;
             q.pop();
-            
-            for(int i = 0; i < 4; i++){
+
+            for (int i = 0; i < 4; i++) {
                 int nrow = row + drow[i];
                 int ncol = col + dcol[i];
-                if(nrow >= 0 && nrow < n && ncol >= 0 && ncol < m && image[nrow][ncol] == startColor){
+                if (nrow >= 0 && nrow < n && ncol >= 0 && ncol < m &&
+                    image[nrow][ncol] == startColor) {
                     q.push({nrow, ncol});
                     image[nrow][ncol] = color;
                 }
