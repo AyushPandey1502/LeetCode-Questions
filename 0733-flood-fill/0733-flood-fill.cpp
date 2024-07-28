6a3c0bc53938137f39c25a8a47ad253407316377
Ayush Pandey <114850053+AyushPandey1502@users.noreply.github.com>
2024-07-28 17:46:21 +0530

    Time: 8 ms (47.6%), Space: 16.7 MB (66.74%) - LeetHub

--- 0733-flood-fill/0733-flood-fill.cpp
+++ 0733-flood-fill/0733-flood-fill.cpp
@@ -1,37 +1,30 @@
 class Solution {
 public:
-    vector<vector<int>> floodFill(vector<vector<int>>& image, int sr, int sc,
-                                  int color) {
+    int drow[4] = {-1, 1, 0, 0};
+    int dcol[4] = {0, 0, 1, -1};
+
+    void dfs(int row, int col, vector<vector<int>>& image,
+             vector<vector<int>>& result, int color, int startColor) {
         int n = image.size();
         int m = image[0].size();
-        int startColor = image[sr][sc];
-
-        if (startColor == color) {
-            return image;
-        }
-
-        queue<pair<int, int>> q;
-        q.push({sr, sc});
-        image[sr][sc] = color;
-
-        int drow[] = {-1, 1, 0, 0};
-        int dcol[] = {0, 0, 1, -1};
-
-        while (!q.empty()) {
-            int row = q.front().first;
-            int col = q.front().second;
-            q.pop();
-
-            for (int i = 0; i < 4; i++) {
-                int nrow = row + drow[i];
-                int ncol = col + dcol[i];
-                if (nrow >= 0 && nrow < n && ncol >= 0 && ncol < m &&
-                    image[nrow][ncol] == startColor) {
-                    q.push({nrow, ncol});
-                    image[nrow][ncol] = color;
-                }
+        result[row][col] = color;
+        
+        for (int i = 0; i < 4; i++) {
+            int nrow = row + drow[i];
+            int ncol = col + dcol[i];
+            if (nrow >= 0 && nrow < n && ncol >= 0 && ncol < m &&
+                image[nrow][ncol] == startColor &&
+                result[nrow][ncol] != color) {
+                dfs(nrow, ncol, image, result, color, startColor);
             }
         }
-        return image;
+    }
+
+    vector<vector<int>> floodFill(vector<vector<int>>& image, int sr, int sc,                        int color) {
+        int startColor = image[sr][sc];
+
+        vector<vector<int>> result = image;
+        dfs(sr, sc, image, result, color, startColor);
+        return result;
     }
 };
