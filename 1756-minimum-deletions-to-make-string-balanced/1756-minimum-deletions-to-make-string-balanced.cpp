b99af8c813a34716f5b62103166f27f07c2376c4
Ayush Pandey <114850053+AyushPandey1502@users.noreply.github.com>
2024-07-30 07:07:52 +0530

    Time: 94 ms (54.23%), Space: 53 MB (20.25%) - LeetHub

--- 1756-minimum-deletions-to-make-string-balanced/1756-minimum-deletions-to-make-string-balanced.cpp
+++ 1756-minimum-deletions-to-make-string-balanced/1756-minimum-deletions-to-make-string-balanced.cpp
@@ -5,16 +5,16 @@ public:
         vector<int> countB(n + 1, 0);
         vector<int> countA(n + 1, 0);
 
-        for (int i = 0; i < n; ++i) {
+        for (int i = 0; i < n; i++) {
             countB[i + 1] = countB[i] + (s[i] == 'b' ? 1 : 0);
         }
 
-        for (int i = n - 1; i >= 0; --i) {
+        for (int i = n - 1; i >= 0; i--) {
             countA[i] = countA[i + 1] + (s[i] == 'a' ? 1 : 0);
         }
 
         int minDeletions = INT_MAX;
-        for (int i = 0; i <= n; ++i) {
+        for (int i = 0; i <= n; i++) {
             minDeletions = min(minDeletions, countB[i] + countA[i]);
         }
 
