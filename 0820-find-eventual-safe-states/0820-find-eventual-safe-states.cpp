a06a1f67140474dfe70c4e8e509acfc74db506eb
Ayush Pandey <114850053+AyushPandey1502@users.noreply.github.com>
2024-07-31 09:05:28 +0530

    Time: 154 ms (19.78%), Space: 65.6 MB (15.39%) - LeetHub

--- 0820-find-eventual-safe-states/0820-find-eventual-safe-states.cpp
+++ 0820-find-eventual-safe-states/0820-find-eventual-safe-states.cpp
@@ -1,28 +1,32 @@
+// TOPOLOGICAL SORT
+// KAHN'S ALGORITHM -> CYCLE DETECTION APPROACH
 class Solution {
 public:
     vector<int> eventualSafeNodes(vector<vector<int>>& graph) {
         int n = graph.size();
         vector<int> indegree(n, 0);
         vector<vector<int>> adj(n);
-        for(int i = 0; i < n; i++){
-            for(auto it : graph[i]){
+        for (int i = 0; i < n; i++) {
+            for (auto it : graph[i]) {
                 adj[it].push_back(i);
                 indegree[i]++;
             }
         }
-        
+
         queue<int> q;
-        for(int i = 0; i < n; i++){
-            if(indegree[i] == 0) q.push(i);
+        for (int i = 0; i < n; i++) {
+            if (indegree[i] == 0)
+                q.push(i);
         }
         vector<int> result;
-        while(!q.empty()){
+        while (!q.empty()) {
             int node = q.front();
             q.pop();
             result.push_back(node);
-            for(int it : adj[node]){
+            for (int it : adj[node]) {
                 indegree[it]--;
-                if(indegree[it] == 0) q.push(it);
+                if (indegree[it] == 0)
+                    q.push(it);
             }
         }
         sort(result.begin(), result.end());
