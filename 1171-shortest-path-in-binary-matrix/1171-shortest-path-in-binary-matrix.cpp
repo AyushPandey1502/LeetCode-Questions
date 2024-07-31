8f8165762cb83bae12f2e58fddfac19c7c2a3680
Ayush Pandey <114850053+AyushPandey1502@users.noreply.github.com>
2024-07-31 16:44:45 +0530

    Time: 45 ms (65.47%), Space: 25.4 MB (27.36%) - LeetHub

--- 1171-shortest-path-in-binary-matrix/1171-shortest-path-in-binary-matrix.cpp
+++ 1171-shortest-path-in-binary-matrix/1171-shortest-path-in-binary-matrix.cpp
@@ -6,21 +6,18 @@ public:
             return -1;
         }
         vector<vector<int>> dist(n, vector<int>(n, INT_MAX));
-        priority_queue<pair<int, pair<int, int>>,
-                       vector<pair<int, pair<int, int>>>,
-                       greater<pair<int, pair<int, int>>>>
-            pq;
-        pq.push({1, {0, 0}});
+        queue<pair<int, pair<int, int>>> q;
+        q.push({1, {0, 0}});
         dist[0][0] = 1;
 
         int drow[8] = {-1, 0, 1, -1, 1, -1, 0, 1};
         int dcol[8] = {-1, -1, -1, 0, 0, 1, 1, 1};
 
-        while (!pq.empty()) {
-            int dis = pq.top().first;
-            int row = pq.top().second.first;
-            int col = pq.top().second.second;
-            pq.pop();
+        while (!q.empty()) {
+            int dis = q.front().first;
+            int row = q.front().second.first;
+            int col = q.front().second.second;
+            q.pop();
 
             if (row == n - 1 && col == n - 1) {
                 return dis;
@@ -30,11 +27,9 @@ public:
                 int nrow = row + drow[i];
                 int ncol = col + dcol[i];
                 if (nrow >= 0 && nrow < n && ncol >= 0 && ncol < n &&
-                    grid[nrow][ncol] == 0) {
-                    if (dis + 1 < dist[nrow][ncol]) {
-                        dist[nrow][ncol] = dis + 1;
-                        pq.push({dist[nrow][ncol], {nrow, ncol}});
-                    }
+                    grid[nrow][ncol] == 0 && dis + 1 < dist[nrow][ncol]) {
+                    dist[nrow][ncol] = dis + 1;
+                    q.push({dist[nrow][ncol], {nrow, ncol}});
                 }
             }
         }
