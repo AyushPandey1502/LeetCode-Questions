e89f8e5a7a4a7df8bbacc3ff1ef6d8c732d23400
Ayush Pandey <114850053+AyushPandey1502@users.noreply.github.com>
2024-07-31 19:00:34 +0530

    Time: 537 ms (5%), Space: 124 MB (5.02%) - LeetHub

--- 1753-path-with-minimum-effort/1753-path-with-minimum-effort.cpp
+++ 1753-path-with-minimum-effort/1753-path-with-minimum-effort.cpp
@@ -4,23 +4,18 @@ public:
         int n = heights.size();
         int m = heights[0].size();
         vector<vector<int>> efforts(n, vector<int>(m, INT_MAX));
-        priority_queue<pair<int, pair<int, int>>, vector<pair<int, pair<int, int>>>, greater<pair<int, pair<int, int>>>> pq;
-        
-        pq.push({0, {0, 0}});
+        queue<pair<int, pair<int, int>>> q;
+        q.push({0, {0, 0}});
         efforts[0][0] = 0;
 
         int drow[4] = {0, 0, 1, -1};
         int dcol[4] = {-1, 1, 0, 0};
 
-        while (!pq.empty()) {
-            int eff = pq.top().first;
-            int row = pq.top().second.first;
-            int col = pq.top().second.second;
-            pq.pop();
-
-            if (row == n-1 && col == m-1) {
-                return eff;
-            }
+        while (!q.empty()) {
+            int eff = q.front().first;
+            int row = q.front().second.first;
+            int col = q.front().second.second;
+            q.pop();
 
             for (int i = 0; i < 4; i++) {
                 int nrow = row + drow[i];
@@ -30,12 +25,12 @@ public:
                     int newEff = max(eff, abs(heights[row][col] - heights[nrow][ncol]));
                     if (newEff < efforts[nrow][ncol]) {
                         efforts[nrow][ncol] = newEff;
-                        pq.push({newEff, {nrow, ncol}});
+                        q.push({newEff, {nrow, ncol}});
                     }
                 }
             }
         }
 
-        return 0; 
+        return efforts[n-1][m-1];
     }
 };
