bf2f52ac82e9505c1f4c346cd7ab7d6384b5ff9e
Ayush Pandey <114850053+AyushPandey1502@users.noreply.github.com>
2024-08-02 15:33:24 +0530

    Time: 69 ms (53.24%), Space: 82.9 MB (68.76%) - LeetHub

--- 2255-minimum-swaps-to-group-all-1s-together-ii/2255-minimum-swaps-to-group-all-1s-together-ii.cpp
+++ 2255-minimum-swaps-to-group-all-1s-together-ii/2255-minimum-swaps-to-group-all-1s-together-ii.cpp
@@ -4,12 +4,14 @@ public:
         int n = nums.size();
         int countOnes = accumulate(nums.begin(), nums.end(), 0);
         int currentCount = 0;
-        for(int i = 0; i < countOnes; i++){
-            if(nums[i] == 1) currentCount++;
+        for (int i = 0; i < countOnes; i++) {
+            if (nums[i] == 1)
+                currentCount++;
         }
         int result = countOnes - currentCount;
-        for(int i = countOnes; i < countOnes + n; i++){
-            currentCount = currentCount - nums[(i - countOnes) % n] + nums[i % n];
+        for (int i = countOnes; i < countOnes + n; i++) {
+            currentCount =
+                currentCount - nums[(i - countOnes) % n] + nums[i % n];
             result = min(result, countOnes - currentCount);
         }
 
