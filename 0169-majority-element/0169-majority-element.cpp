e34878a0fe717356ad17dafc5d43cdf68c5e67e3
Ayush Pandey <114850053+AyushPandey1502@users.noreply.github.com>
2024-08-08 10:39:30 +0530

    Time: 13 ms (84.15%), Space: 27 MB (55.78%) - LeetHub

--- 0169-majority-element/0169-majority-element.cpp
+++ 0169-majority-element/0169-majority-element.cpp
@@ -1,14 +1,33 @@
+// OPTIMAL APPROACH : MOORE'S VOTING ALGORITHM
+// TIME COMPLEXITY : O(N)
+// SPACE COMPLEXITY : O(1)
+
 class Solution {
 public:
     int majorityElement(vector<int>& nums) {
         int n = nums.size();
-        unordered_map<int, int> map;
+        int element = 0;
+        int count = 0;
         for(int i = 0; i < n; i++){
-            map[nums[i]]++;
+            if(count == 0){
+                element = nums[i];
+                count++;
+            }else if(nums[i] == element){
+                count++;
+            }else{
+                count--;
+            }
         }
-        for(auto it : map){
-            if(it.second > n/2){
-                return it.first;
+
+        // If it is possible that majority element not present then
+        // we need to check the occurence of element in array.
+        count = 0;
+        for(int i = 0; i < n; i++){
+            if(nums[i] == element){
+                count++;
+            }
+            if(count > n/2){
+                return element;
             }
         }
         return -1;
