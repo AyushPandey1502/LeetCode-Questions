dce4c9fc19f3a9d4c9ebbaea5e150d06282075ab
Ayush Pandey <114850053+AyushPandey1502@users.noreply.github.com>
2024-08-09 09:10:04 +0530

    Time: 416 ms (58.13%), Space: 11.8 MB (57.87%) - LeetHub

--- 0204-count-primes/0204-count-primes.cpp
+++ 0204-count-primes/0204-count-primes.cpp
@@ -1,28 +1,20 @@
+// SIEVE OF ERATOSTHENES   
 class Solution {
 public:
     int countPrimes(int n) {
-        if (n <= 2) {
-            return 0;
-        }
-
-        vector<bool> isPrime(n, true);
-        isPrime[0] = isPrime[1] = false;
-
-        for (int i = 2; i * i < n; i++) {
-            if (isPrime[i]) {
-                for (int j = i * i; j < n; j += i) {
-                    isPrime[j] = false;
+        if (n <= 1) return 0;
+        vector<bool> primes(n + 1, true);
+        for (int p = 2; p * p <= n; p++) {
+            if (primes[p] == true) {
+                for (int i = p * p; i <= n; i += p) {
+                    primes[i] = false;
                 }
             }
         }
-
         int count = 0;
         for (int i = 2; i < n; i++) {
-            if (isPrime[i]) {
-                count++;
-            }
+            if (primes[i]) count++;
         }
-
         return count;
     }
 };
