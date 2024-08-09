0d7f9dad4e32dc17b6f958c17dc77d0a464fea9d
Ayush Pandey <114850053+AyushPandey1502@users.noreply.github.com>
2024-08-09 11:28:30 +0530

    Time: 3 ms (98.18%), Space: 18.7 MB (48.04%) - LeetHub

--- 0229-majority-element-ii/0229-majority-element-ii.cpp
+++ 0229-majority-element-ii/0229-majority-element-ii.cpp
@@ -1,53 +1,24 @@
-#include <vector>
-
 class Solution {
 public:
-    vector<int> majorityElement(std::vector<int>& nums) {
-        vector<int> result;
-        
-        if (nums.empty()) {
-            return result;
-        }
-        
-        int candidate1 = 0, candidate2 = 1;
-        int count1 = 0, count2 = 0;
-        
-        for (int n : nums) {
-            if (n == candidate1) {
-                count1++;
-            } else if (n == candidate2) {
-                count2++;
-            } else if (count1 == 0) {
-                candidate1 = n;
-                count1 = 1;
-            } else if (count2 == 0) {
-                candidate2 = n;
-                count2 = 1;
-            } else {
-                count1--;
-                count2--;
-            }
-        }
-        count1 = 0;
-        count2 = 0;
-        
-        for (int n : nums) {
-            if (n == candidate1) {
-                count1++;
-            } else if (n == candidate2) {
-                count2++;
-            }
-        }
-        
+    vector<int> majorityElement(vector<int>& nums) {
         int n = nums.size();
-        
-        if (count1 > n / 3) {
-            result.push_back(candidate1);
+        int minNumElem = n / 3;
+        vector<int> result;
+        for (int i = 0; i < n; i++) {
+            if (result.empty() ||
+                (result.size() > 0 && nums[i] != result.back())) {
+                int count = 0;
+                for (int j = 0; j < n; j++) {
+                    if (nums[i] == nums[j])
+                        count++;
+                }
+                if (count > minNumElem) {
+                    result.push_back(nums[i]);
+                }
+                if (result.size() == 2)
+                    break;
+            }
         }
-        if (count2 > n / 3) {
-            result.push_back(candidate2);
-        }
-        
         return result;
     }
 };
