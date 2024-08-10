dc53b74b4683386214aaae1123133c63888042c8
Ayush Pandey <114850053+AyushPandey1502@users.noreply.github.com>
2024-08-10 12:09:37 +0530

    Time: 291 ms (70.98%), Space: 22.1 MB (37.77%) - LeetHub

--- 2610-closest-prime-numbers-in-range/2610-closest-prime-numbers-in-range.cpp
+++ 2610-closest-prime-numbers-in-range/2610-closest-prime-numbers-in-range.cpp
@@ -1,37 +1,39 @@
 class Solution {
 public:
     vector<int> closestPrimes(int left, int right) {
-        if (right < 2) return {-1, -1};
-
         vector<bool> primes(right + 1, true);
-        primes[0] = primes[1] = false;
-
+        
+        if (left == 1) {
+            primes[1] = false;
+            left = 2; 
+        }
+        
         for (int p = 2; p * p <= right; p++) {
             if (primes[p]) {
-                for (int i = p * p; i <= right; i += p) {
+                int sm = max(p * p, (left + p - 1) / p * p);
+                for (int i = sm; i <= right; i += p) {
                     primes[i] = false;
                 }
             }
         }
-
+        
         vector<int> primeNum;
-        for (int i = max(left, 2); i <= right; i++) {
+        for (int i = left; i <= right; i++) {
             if (primes[i]) {
                 primeNum.push_back(i);
             }
         }
-
+        
         if (primeNum.size() <= 1) return {-1, -1};
-
-        int index = 0;
+        
+        int index = -1;
         int minDiff = INT_MAX;
         for (int i = 1; i < primeNum.size(); i++) {
-            if ((primeNum[i] - primeNum[i - 1]) < minDiff) {
-                minDiff = primeNum[i] - primeNum[i - 1];
+            if ((primeNum[i] - primeNum[i-1]) < minDiff) {
+                minDiff = primeNum[i] - primeNum[i-1];
                 index = i;
             }
         }
-
-        return {primeNum[index - 1], primeNum[index]};
+        return {primeNum[index-1], primeNum[index]};
     }
 };
