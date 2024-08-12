cebf812bb4bb5e7f210c69a6117195c2f7666fab
Ayush Pandey <114850053+AyushPandey1502@users.noreply.github.com>
2024-08-12 09:08:53 +0530

    Time: 22 ms (86.49%), Space: 24.9 MB (17.04%) - LeetHub

--- 0789-kth-largest-element-in-a-stream/0789-kth-largest-element-in-a-stream.cpp
+++ 0789-kth-largest-element-in-a-stream/0789-kth-largest-element-in-a-stream.cpp
@@ -1,21 +1,17 @@
 class KthLargest {
 public:
     int k;
-    multiset<int, less<int>> container; 
-    KthLargest(int k, vector<int>& nums) : k(k) {
-        for (int x : nums) {
-            container.insert(x);
-            if (container.size() > k) {
-                container.erase(container.begin());
-            }
+    priority_queue<int, vector<int>, greater<int>> pq;
+    KthLargest(int k, vector<int>& nums): k(k){
+        for(int x: nums) {
+            pq.push(x);
+            if (pq.size()> k) pq.pop();
         }
     }
-
+    
     int add(int val) {
-        container.insert(val);
-        if (container.size() > k) {
-            container.erase(container.begin());
-        }
-        return *(container.begin());
+        pq.push(val);
+        if (pq.size()> k) pq.pop();   
+        return pq.top();      
     }
 };
