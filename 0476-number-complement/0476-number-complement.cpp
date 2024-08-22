8c423fa39904ad91456e4bf6e089cd5c4bbbdd1f
Ayush Pandey <114850053+AyushPandey1502@users.noreply.github.com>
2024-08-22 08:15:37 +0530

    Time: 3 ms (41.07%), Space: 7.5 MB (33.08%) - LeetHub

--- 0476-number-complement/0476-number-complement.cpp
+++ 0476-number-complement/0476-number-complement.cpp
@@ -1,10 +1,14 @@
 class Solution {
 public:
     int findComplement(int num) {
-        int mask = ~0;
-        while (num & mask) {
-            mask <<= 1;
+        int result = 0;
+        int shift = 0;
+        while(num > 0) {
+            int dig = !(num & 1);
+            result |= (dig << shift);
+            num = num >> 1;
+            shift++;
         }
-        return ~mask & ~num;
+        return result;
     }
 };
