f70b221621d5a61130c9c80ca6b125c436a46d4c
Ayush Pandey <114850053+AyushPandey1502@users.noreply.github.com>
2024-09-01 19:02:06 +0530

    Time: 3 ms (72.9%), Space: 15 MB (30.43%) - LeetHub

--- 1029-vertical-order-traversal-of-a-binary-tree/1029-vertical-order-traversal-of-a-binary-tree.cpp
+++ 1029-vertical-order-traversal-of-a-binary-tree/1029-vertical-order-traversal-of-a-binary-tree.cpp
@@ -1,47 +1,34 @@
 class Solution {
 public:
     vector<vector<int>> verticalTraversal(TreeNode* root) {
-        if (root == NULL) return {};
-
-        // Map to store the nodes (x-coordinate -> (y-coordinate -> sorted node values))
-        map<int, map<int, multiset<int>>> nodes;
-
-        // Stack to store nodes along with their coordinates and a visited flag
-        stack<pair<TreeNode*, pair<pair<int, int>, bool>>> stk;
-        stk.push({root, {{0, 0}, false}});
-        
-        while (!stk.empty()) {
-            auto p = stk.top();
-            stk.pop();
-            TreeNode* node = p.first;
-            int x = p.second.first.first;
-            int y = p.second.first.second;
-            bool visited = p.second.second;
-
-            if (node == NULL) continue;
-
-            if (visited) {
-                // If the node has been visited, process it
-                nodes[x][y].insert(node->val);
-            } else {
-                // Push the current node back onto the stack with the visited flag set to true
-                stk.push({node, {{x, y}, true}});
-                // Push the right child, then the left child onto the stack
-                stk.push({node->right, {{x + 1, y + 1}, false}});
-                stk.push({node->left, {{x - 1, y + 1}, false}});
-            }
-        }
-
-        // Building the result from the map
         vector<vector<int>> result;
-        for (auto& p : nodes) {
-            vector<int> col;
-            for (auto& q : p.second) {
-                col.insert(col.end(), q.second.begin(), q.second.end());
-            }
-            result.push_back(col);
-        }
+        if(root == nullptr) return result;
+        map<int, map<int, multiset<int>>> nodes;
+        queue<pair<TreeNode*, pair<int, int>>> q;
+        q.push({root, {0, 0}});
 
+        while(!q.empty()){
+            TreeNode* node = q.front().first;
+            int x = q.front().second.first;
+            int y = q.front().second.second;
+            q.pop();
+
+            nodes[x][y].insert(node -> val);
+            if(node -> left){
+                q.push({node -> left, {x - 1, y + 1}});
+            }
+            if(node -> right){
+                q.push({node -> right, {x + 1, y + 1}});
+            }
+        } 
+
+        for(auto x : nodes){
+            vector<int> temp;
+            for(auto y : x.second){
+                temp.insert(temp.end(), y.second.begin(), y.second.end());
+            }
+            result.push_back(temp);
+        }
         return result;
     }
 };
