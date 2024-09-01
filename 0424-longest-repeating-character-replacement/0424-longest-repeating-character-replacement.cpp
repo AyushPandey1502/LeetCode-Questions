9dbcfc11444fa097aaa2b5e160947e27c9ac0cc2
Ayush Pandey <114850053+AyushPandey1502@users.noreply.github.com>
2024-09-01 11:12:09 +0530

    Time: 6 ms (79%), Space: 8.5 MB (78.24%) - LeetHub

--- 0424-longest-repeating-character-replacement/0424-longest-repeating-character-replacement.cpp
+++ 0424-longest-repeating-character-replacement/0424-longest-repeating-character-replacement.cpp
@@ -1,14 +1,17 @@
+// MOST OPTIMIZED APPROACH
+// TIME COMPLEXITY : O(N)
+// SPACE COMPLEXITY : O(26)
 class Solution {
 public:
     int characterReplacement(string s, int k) {
         int left = 0, right = 0, maxLen = 0, maxFreq = 0;
         vector<int> freq(26, 0);
         
-        while (right < s.size()) {
+        while(right < s.size()) {
             freq[s[right] - 'A']++;
             maxFreq = max(maxFreq, freq[s[right] - 'A']);
             
-            while ((right - left + 1) - maxFreq > k) {
+            if((right - left + 1) - maxFreq > k) {
                 freq[s[left] - 'A']--;
                 left++;
             }
