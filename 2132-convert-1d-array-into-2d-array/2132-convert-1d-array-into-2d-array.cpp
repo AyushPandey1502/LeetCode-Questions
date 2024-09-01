d628e94e0213a83aed5d0c2551fabe10c392c854
Ayush Pandey <114850053+AyushPandey1502@users.noreply.github.com>
2024-09-01 07:49:22 +0530

    Time: 85 ms (44.77%), Space: 88.1 MB (55.59%) - LeetHub

--- 2132-convert-1d-array-into-2d-array/2132-convert-1d-array-into-2d-array.cpp
+++ 2132-convert-1d-array-into-2d-array/2132-convert-1d-array-into-2d-array.cpp
@@ -1,14 +1,13 @@
 class Solution {
 public:
     vector<vector<int>> construct2DArray(vector<int>& original, int m, int n) {
-        if (m * n != original.size()) return {}; 
-
-        vector<vector<int>> grid(m, vector<int>(n));
-        for(int k = 0; k < m * n; k++){
-            int i = k / n;  
-            int j = k % n; 
-            grid[i][j] = original[k];
+        if (original.size() != m * n) {
+            return {};
         }
-        return grid;
+        vector<vector<int>> target(m, vector<int>(n));
+        for (int i = 0; i < original.size(); i++) {
+            target[i / n][i % n] = original[i];
+        }
+        return target;
     }
 };
