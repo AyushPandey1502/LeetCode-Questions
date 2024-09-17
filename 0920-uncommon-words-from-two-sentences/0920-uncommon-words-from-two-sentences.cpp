99bbd0bc2ab2ca719a7d3f266ec4b26a506d0c2e
Ayush Pandey <114850053+AyushPandey1502@users.noreply.github.com>
2024-09-17 09:44:49 +0530

    Time: 0 ms (100%), Space: 8.6 MB (97.06%) - LeetHub

--- 0920-uncommon-words-from-two-sentences/0920-uncommon-words-from-two-sentences.cpp
+++ 0920-uncommon-words-from-two-sentences/0920-uncommon-words-from-two-sentences.cpp
@@ -1,18 +1,17 @@
 class Solution {
 public:
     vector<string> uncommonFromSentences(string s1, string s2) {
-        unordered_map<string, int> wordCount;
+        unordered_set<string> uniqueWords, repeatedWords;
         stringstream ss(s1 + " " + s2);
         string word;
         while (ss >> word) {
-            wordCount[word]++;
-        }
-        vector<string> result;
-        for (auto entry : wordCount) {
-            if (entry.second == 1) {
-                result.push_back(entry.first);
+            if (uniqueWords.find(word) != uniqueWords.end()) {
+                uniqueWords.erase(word);
+                repeatedWords.insert(word);
+            } else if (repeatedWords.find(word) == repeatedWords.end()) {
+                uniqueWords.insert(word);
             }
         }
-        return result;
+        return vector<string>(uniqueWords.begin(), uniqueWords.end());
     }
 };
