8ead3c3cdadae0311f53f56ac3d8e662ec811676
Ayush Pandey <114850053+AyushPandey1502@users.noreply.github.com>
2024-09-17 11:24:18 +0530

    Time: 3 ms (58.6%), Space: 9.1 MB (27.54%) - LeetHub

--- 0008-string-to-integer-atoi/0008-string-to-integer-atoi.cpp
+++ 0008-string-to-integer-atoi/0008-string-to-integer-atoi.cpp
@@ -1,9 +1,18 @@
 class Solution {
 public:
+
+    long recursiveAtoi(const string& s, int i, int sign, long result) {
+        if (i >= s.size() || !isdigit(s[i])) {
+            return result * sign;
+        }
+        result = result * 10 + (s[i] - '0');
+        if (result * sign > INT_MAX) return INT_MAX;
+        if (result * sign < INT_MIN) return INT_MIN;
+        return recursiveAtoi(s, i + 1, sign, result);
+    }
     int myAtoi(string s) {
         int i = 0;
         int sign = 1;
-        long result = 0;
         while (i < s.size() && s[i] == ' ') {
             i++;
         }
@@ -11,12 +20,7 @@ public:
             sign = (s[i] == '-') ? -1 : 1;
             i++;
         }
-        while (i < s.size() && isdigit(s[i])) {
-            result = result * 10 + (s[i] - '0');
-            if (result * sign > INT_MAX) return INT_MAX;
-            if (result * sign < INT_MIN) return INT_MIN;
-            i++;
-        }
-        return result * sign;
+        long result = recursiveAtoi(s, i, sign, 0);
+        return static_cast<int>(result);
     }
 };
