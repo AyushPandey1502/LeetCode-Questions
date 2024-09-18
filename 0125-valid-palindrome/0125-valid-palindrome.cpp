8595c20855ebe5f9c78366e972af62baa92c8f93
Ayush Pandey <114850053+AyushPandey1502@users.noreply.github.com>
2024-09-18 22:26:44 +0530

    Time: 3 ms (90.31%), Space: 8.9 MB (55.34%) - LeetHub

--- 0125-valid-palindrome/0125-valid-palindrome.cpp
+++ 0125-valid-palindrome/0125-valid-palindrome.cpp
@@ -1,20 +1,17 @@
 class Solution {
 public:
     bool checkPalindrome(int left, int right, const string& s) {
-        while (left < right) {
-            if (!isalnum(s[left])) {
-                ++left;
-            } else if (!isalnum(s[right])) {
-                --right;
-            } else {
-                if (tolower(s[left]) != tolower(s[right])) {
-                    return false;
-                }
-                ++left;
-                --right;
-            }
+        if (left >= right) return true;
+        if (!isalnum(s[left])) {
+            return checkPalindrome(left + 1, right, s);
         }
-        return true;
+        if (!isalnum(s[right])) {
+            return checkPalindrome(left, right - 1, s);
+        }
+        if (tolower(s[left]) != tolower(s[right])) {
+            return false;
+        }
+        return checkPalindrome(left + 1, right - 1, s);
     }
     
     bool isPalindrome(string s) {
