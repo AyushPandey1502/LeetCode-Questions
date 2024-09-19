07162fdd41b74e443f185834d769975d7a77325b
Ayush Pandey <114850053+AyushPandey1502@users.noreply.github.com>
2024-09-19 19:37:13 +0530

    Time: 159 ms (5.06%), Space: 127.6 MB (5.07%) - LeetHub

--- 0039-combination-sum/0039-combination-sum.cpp
+++ 0039-combination-sum/0039-combination-sum.cpp
@@ -1,27 +1,23 @@
 class Solution {
 public:
-    vector<vector<int>> result;
-
-    void solve(vector<int>& candidates, int index, int target, vector<int>& temp) {
-        if (target == 0) {
-            result.push_back(temp);
+    void generateComb(int i, vector<int> nums, int target, int sum,
+                      vector<int> temp, vector<vector<int>>& result) {
+        if (i == nums.size()) {
+            if (sum == target) {
+                result.push_back(temp);
+            }
             return;
         }
-        if (target < 0) {
-            return;
-        }
-        
-        for (int i = index; i < candidates.size(); i++) {
-            temp.push_back(candidates[i]);
-            solve(candidates, i, target - candidates[i], temp);
-            temp.pop_back();
-        }
+        if(sum > target) return;
+        temp.push_back(nums[i]);
+        generateComb(i, nums, target, sum + nums[i], temp, result);
+        temp.pop_back();
+        generateComb(i + 1, nums, target, sum, temp, result);
     }
-
     vector<vector<int>> combinationSum(vector<int>& candidates, int target) {
-        result.clear();
         vector<int> temp;
-        solve(candidates, 0, target, temp);
+        vector<vector<int>> result;
+        generateComb(0, candidates, target, 0, temp, result);
         return result;
     }
 };
