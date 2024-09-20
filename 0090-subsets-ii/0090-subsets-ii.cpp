9be7682caf84788dae026095fbb682840ce1c24d
Ayush Pandey <114850053+AyushPandey1502@users.noreply.github.com>
2024-09-20 11:18:18 +0530

    Time: 0 ms (100%), Space: 16 MB (11.03%) - LeetHub

--- 0090-subsets-ii/0090-subsets-ii.cpp
+++ 0090-subsets-ii/0090-subsets-ii.cpp
@@ -1,22 +1,26 @@
 class Solution {
 public:
-    void generateComb(int i, vector<int>& nums, vector<int>& temp, vector<vector<int>>& result) {
-        result.push_back(temp);
-        
-        for (int j = i; j < nums.size(); ++j) {
-            if (j > i && nums[j] == nums[j - 1]) {
-                continue;  
-            }
-            temp.push_back(nums[j]);
-            generateComb(j + 1, nums, temp, result); 
-            temp.pop_back(); 
+    void generateComb(int i, vector<int> nums, vector<int> temp, vector<vector<int>>& result){
+        if(i == nums.size()){
+            result.push_back(temp);
+            return;
         }
-    }
+        temp.push_back(nums[i]);
+        generateComb(i + 1, nums, temp, result);
+        temp.pop_back();
 
+        for(int j = i + 1; j < nums.size(); j++){
+            if(nums[j] != nums[i]){
+                generateComb(j, nums, temp, result);
+                return;
+            }
+        }
+        generateComb(nums.size(), nums, temp, result);
+    }
     vector<vector<int>> subsetsWithDup(vector<int>& nums) {
         vector<int> temp;
         vector<vector<int>> result;
-        sort(nums.begin(), nums.end());  
+        sort(nums.begin(), nums.end());
         generateComb(0, nums, temp, result);
         return result;
     }
