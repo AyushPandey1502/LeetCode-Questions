28c94900f80d0be20723142a63eb16faeb68c8fd
Ayush Pandey <114850053+AyushPandey1502@users.noreply.github.com>
2024-09-20 19:01:42 +0530

    Time: 3 ms (38.1%), Space: 8.3 MB (74.96%) - LeetHub

--- 0017-letter-combinations-of-a-phone-number/0017-letter-combinations-of-a-phone-number.cpp
+++ 0017-letter-combinations-of-a-phone-number/0017-letter-combinations-of-a-phone-number.cpp
@@ -1,28 +1,21 @@
 class Solution {
 public:
-    vector<string> letterCombinations(string digits) {
-        if (digits.empty()) return {};
+    vector<string> dig = {"", "", "abc", "def", "ghi", "jkl", "mno", "pqrs", "tuv", "wxyz"};
 
-        unordered_map<char, vector<char>> map = {
-            {'2', {'a', 'b', 'c'}}, {'3', {'d', 'e', 'f'}}, {'4', {'g', 'h', 'i'}},
-            {'5', {'j', 'k', 'l'}}, {'6', {'m', 'n', 'o'}}, {'7', {'p', 'q', 'r', 's'}},
-            {'8', {'t', 'u', 'v'}}, {'9', {'w', 'x', 'y', 'z'}}
-        };
-
-        unordered_set<string> set;
-        set.insert("");
-
-        for (int i = 0; i < digits.size(); i++) {
-            vector<char> vec = map[digits[i]];
-            unordered_set<string> temp;
-
-            for (auto str : set) {
-                for (auto it : vec) {
-                    temp.insert(str + it);
-                }
-            }
-            set = move(temp);
+    void generateComb(int i, string& digits, string current, vector<string>& result) {
+        if (i == digits.size()) {
+            result.push_back(current);
+            return;
         }
-        return vector<string>(set.begin(), set.end());
+        for (char ch : dig[digits[i] - '0']) {
+            generateComb(i + 1, digits, current + ch, result);
+        }
+    }
+
+    vector<string> letterCombinations(string digits) {
+        vector<string> result;
+        if (digits.empty()) return result;
+        generateComb(0, digits, "", result);
+        return result;
     }
 };
