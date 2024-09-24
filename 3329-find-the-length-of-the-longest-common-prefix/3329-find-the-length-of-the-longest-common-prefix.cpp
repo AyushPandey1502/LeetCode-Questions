9687f9aaab7f733619bc7d5e2fe9be581082adbc
Ayush Pandey <114850053+AyushPandey1502@users.noreply.github.com>
2024-09-24 09:08:00 +0530

    Time: 281 ms (63.75%), Space: 135.7 MB (84.5%) - LeetHub

--- 3329-find-the-length-of-the-longest-common-prefix/3329-find-the-length-of-the-longest-common-prefix.cpp
+++ 3329-find-the-length-of-the-longest-common-prefix/3329-find-the-length-of-the-longest-common-prefix.cpp
@@ -2,23 +2,22 @@ class Solution {
 public:
     int longestCommonPrefix(vector<int>& arr1, vector<int>& arr2) {
         unordered_set<int> set;
-        for (auto x : arr1) {
-            while (x > 0) {
+        for(auto x : arr1){
+            while(x > 0){
                 set.insert(x);
-                x /= 10;
+                x = x / 10;
             }
         }
-
-        int maximum = INT_MIN;
-        for (auto x : arr2) {
-            while (x > 0) {
-                if (set.find(x) != set.end()) {
-                    maximum = max(maximum, static_cast<int>(to_string(x).size()));
+        int maxLen = 0;
+        for(auto x : arr2){
+            while(x > 0){
+                if(set.find(x) != set.end()){
+                    maxLen = max(maxLen, static_cast<int>(to_string(x).size()));
                     break;
                 }
-                x /= 10;
+                x = x / 10;
             }
         }
-        return (maximum == INT_MIN) ? 0 : maximum;
+        return maxLen;
     }
 };
