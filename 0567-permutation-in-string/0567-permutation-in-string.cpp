e2c4136590fc5a7bf9a2d39355431cf8a0308fca
Ayush Pandey <114850053+AyushPandey1502@users.noreply.github.com>
2024-10-01 10:17:32 +0530

    Time: 6 ms (68.17%), Space: 8.8 MB (51.08%) - LeetHub

---
+++ 0567-permutation-in-string/0567-permutation-in-string.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    bool checkInclusion(string s1, string s2) {
+        if (s1.size() > s2.size()) return false;
+        vector<int> s1Count(26, 0), s2Count(26, 0);
+        
+        for (int i = 0; i < s1.size(); i++) {
+            s1Count[s1[i] - 'a']++;
+            s2Count[s2[i] - 'a']++;
+        }
+        
+        for (int i = s1.size(); i < s2.size(); i++) {
+            if (s1Count == s2Count) return true;
+            s2Count[s2[i] - 'a']++;
+            s2Count[s2[i - s1.size()] - 'a']--;
+        }
+        return s1Count == s2Count;
+    }
+};
