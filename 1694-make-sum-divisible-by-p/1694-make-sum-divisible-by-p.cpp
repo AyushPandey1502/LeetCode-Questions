b9f87e0e50d5e0b31923e7708db97ea73c27c8ba
Ayush Pandey <114850053+AyushPandey1502@users.noreply.github.com>
2024-10-03 08:19:49 +0530

    Time: 118 ms (95.63%), Space: 70.5 MB (45.24%) - LeetHub

--- 1694-make-sum-divisible-by-p/1694-make-sum-divisible-by-p.cpp
+++ 1694-make-sum-divisible-by-p/1694-make-sum-divisible-by-p.cpp
@@ -4,22 +4,22 @@ public:
         int n = nums.size();
         long long totalSum = accumulate(nums.begin(), nums.end(), 0LL);
         int rem = totalSum % p;
-        if(rem == 0) return 0; 
+        if (rem == 0) return 0;
         
         unordered_map<int, int> prefixMap;
-        prefixMap[0] = -1; 
+        prefixMap[0] = -1;
         long long prefixSum = 0;
         int minLen = n;
         
-        for(int i = 0; i < n; ++i) {
+        for (int i = 0; i < n; ++i) {
             prefixSum = (prefixSum + nums[i]) % p;
-            int target = (prefixSum - rem + p) % p;  
-            
-            if(prefixMap.find(target) != prefixMap.end()) {
+            int target = (prefixSum - rem + p) % p;
+            if (prefixMap.find(target) != prefixMap.end()) {
                 minLen = min(minLen, i - prefixMap[target]);
             }
             prefixMap[prefixSum] = i;
         }
+        
         return minLen == n ? -1 : minLen;
     }
 };
