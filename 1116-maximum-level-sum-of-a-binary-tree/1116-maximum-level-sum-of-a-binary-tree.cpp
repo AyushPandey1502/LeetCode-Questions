ecd47bb1fd150d55da583906f07f438fd2228a09
Ayush Pandey <114850053+AyushPandey1502@users.noreply.github.com>
2024-10-22 08:36:24 +0530

    Time: 3 ms (99%), Space: 105.9 MB (54.58%) - LeetHub

--- 1116-maximum-level-sum-of-a-binary-tree/1116-maximum-level-sum-of-a-binary-tree.cpp
+++ 1116-maximum-level-sum-of-a-binary-tree/1116-maximum-level-sum-of-a-binary-tree.cpp
@@ -3,28 +3,26 @@ public:
     int maxLevelSum(TreeNode* root) {
         queue<TreeNode*> q;
         q.push(root);
-        set<pair<int, int>> levelSum;
-        int lvl = 1;
-        while(!q.empty()){
+        int maxSum = INT_MIN;  
+        int maxLevel = 0;     
+        int currentLevel = 1;  
+
+        while(!q.empty()) {
             int size = q.size();
-            int sum = 0;
-            for(int i = 0; i < size; i++){
+            int levelSum = 0;
+            for(int i = 0; i < size; i++) {
                 TreeNode* node = q.front();
                 q.pop();
-                sum += node -> val;
-                if(node -> left) q.push(node -> left);
-                if(node -> right) q.push(node -> right);
+                levelSum += node->val;
+                if(node->left) q.push(node->left);
+                if(node->right) q.push(node->right);
             }
-            levelSum.insert({sum, lvl++});
-        }
-        int maxLevelSum = INT_MIN;
-        int level = -1;
-        for(auto it : levelSum){
-            if(it.first > maxLevelSum){
-                maxLevelSum = it.first;
-                level = it.second;
+            if(levelSum > maxSum) {
+                maxSum = levelSum;
+                maxLevel = currentLevel;
             }
+            currentLevel++; 
         }
-        return level;
+        return maxLevel;
     }
 };
