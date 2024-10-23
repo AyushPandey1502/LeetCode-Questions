22e43abc60d7d173380061656628ecd8af2b245f
Ayush Pandey <114850053+AyushPandey1502@users.noreply.github.com>
2024-10-23 22:48:44 +0530

    Time: 46 ms (95.32%), Space: 332.2 MB (21.6%) - LeetHub

--- 2677-cousins-in-binary-tree-ii/2677-cousins-in-binary-tree-ii.cpp
+++ 2677-cousins-in-binary-tree-ii/2677-cousins-in-binary-tree-ii.cpp
@@ -1,45 +1,39 @@
 class Solution {
 public:
     TreeNode* replaceValueInTree(TreeNode* root) {
-        if (root == NULL) return root;
+        if(!root) return root;
         queue<TreeNode*> q;
-        vector<int> levelSum;
         q.push(root);
-        while (!q.empty()) {
+        root->val = 0;  
+        
+        while(!q.empty()) {
             int size = q.size();
-            int lvlSum = 0;
-            for (int i = 0; i < size; i++) {
+            vector<TreeNode*> currLevel;
+            int totalSum = 0;
+            
+            for(int i = 0; i < size; i++) {
                 TreeNode* node = q.front();
                 q.pop();
-                lvlSum += node->val;
-                if (node->left)
+                currLevel.push_back(node);
+                if(node->left) {
                     q.push(node->left);
-                if (node->right)
-                    q.push(node->right);
-            }
-            levelSum.push_back(lvlSum);
-        }
-
-        q.push(root);
-        root->val = 0;
-        int lvl = 1;
-        while (!q.empty()) {
-            int size = q.size();
-            for (int i = 0; i < size; i++) {
-                TreeNode* node = q.front();
-                q.pop();
-                int siblingSum = (node->left != NULL) ? node->left->val : 0;
-                siblingSum += (node->right != NULL) ? node->right->val : 0;
-                if (node->left) {
-                    node->left->val = levelSum[lvl] - siblingSum;
-                    q.push(node->left);
+                    totalSum += node->left->val;
                 }
-                if (node->right) {
-                    node->right->val = levelSum[lvl] - siblingSum;
+                if(node->right) {
                     q.push(node->right);
+                    totalSum += node->right->val;
                 }
             }
-            lvl++;
+            for(TreeNode* node : currLevel) {
+                int siblingSum = 0;
+                if(node->left) siblingSum += node->left->val;
+                if(node->right) siblingSum += node->right->val;
+                
+                if(node->left) 
+                    node->left->val = totalSum - siblingSum;
+                if(node->right)
+                    node->right->val = totalSum - siblingSum;
+            }
         }
         return root;
     }
