e80c2caf2b3eb543a9094b00087b4a564e398b85
Ayush Pandey <114850053+AyushPandey1502@users.noreply.github.com>
2024-11-04 09:13:40 +0530

    Time: 71 ms (13.68%), Space: 30 MB (24.06%) - LeetHub

--- 3451-string-compression-iii/3451-string-compression-iii.cpp
+++ 3451-string-compression-iii/3451-string-compression-iii.cpp
@@ -1,21 +1,16 @@
 class Solution {
 public:
     string compressedString(string word) {
-        vector<pair<char, int>> result;
-        int n = word.size(), count = 1;
-        char lastChar = word[0];
-        for(int i = 1; i < n; i++){
-            if(word[i] != lastChar || count == 9){
-                result.push_back({lastChar, count});
-                lastChar = word[i];
+        string result;
+        int count = 1;
+        for (int i = 1; i <= word.size(); ++i) {
+            if (i == word.size() || word[i] != word[i - 1] || count == 9) {
+                result += to_string(count) + word[i - 1];
                 count = 1;
-            }else{
+            } else {
                 count++;
             }
         }
-        string output = "";
-        for(auto it : result) output += (to_string(it.second) + it.first);
-        output += to_string(count) + lastChar;
-        return output;
+        return result;
     }
 };
