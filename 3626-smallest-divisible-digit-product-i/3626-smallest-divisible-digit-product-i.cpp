dae80bb34c28d7c6faf78a778936f2ea004dfa47
Ayush Pandey <114850053+AyushPandey1502@users.noreply.github.com>
2024-11-09 22:13:38 +0530

    Time: 2 ms (27.27%), Space: 8.3 MB (90.91%) - LeetHub

---
+++ 3626-smallest-divisible-digit-product-i/3626-smallest-divisible-digit-product-i.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    int digProd(int num){
+        int result = 1;
+        while(num > 0){
+            result *= num % 10;
+            num /= 10;
+        }
+        return result; 
+    }
+
+    int smallestNumber(int n, int t) {
+        int result = n;
+        while(true){
+            if(digProd(result) % t == 0) return result;
+            result++;
+        }    
+        return -1;
+    }
+};
