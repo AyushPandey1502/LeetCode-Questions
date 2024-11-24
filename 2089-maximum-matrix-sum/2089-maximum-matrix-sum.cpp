de83cb23ef051bab11609d0e304fed04f5069e5b
Ayush Pandey <114850053+AyushPandey1502@users.noreply.github.com>
2024-11-24 08:11:04 +0530

    Time: 0 ms (100%), Space: 42.8 MB (8.11%) - LeetHub

--- 2089-maximum-matrix-sum/2089-maximum-matrix-sum.cpp
+++ 2089-maximum-matrix-sum/2089-maximum-matrix-sum.cpp
@@ -1,26 +1,23 @@
 class Solution {
 public:
     long long maxMatrixSum(vector<vector<int>>& matrix) {
-        int countNeg = 0;
-        int n = matrix.size(), m = matrix[0].size();
-        for(int i = 0; i < n; i++){
-            for(int j = 0; j < m; j++){
-                if(matrix[i][j] < 0){
-                    countNeg++;
-                    matrix[i][j] = abs(matrix[i][j]);
+        int negativeCount = 0;
+        long long totalSum = 0;
+        int minElement = INT_MAX;
+
+        for (auto row : matrix) {
+            for (auto value : row) {
+                if (value < 0) {
+                    negativeCount++;
+                    value = -value;
                 }
+                totalSum += value;
+                minElement = min(minElement, value);
             }
         }
-        long long totalSum = 0;
-        vector<int> col;
-        for(auto it : matrix){
-            sort(it.begin(), it.end());
-            totalSum += accumulate(it.begin(), it.end(), 0);
-            col.push_back(it[0]);
-        }
-        sort(col.begin(), col.end());
-        if(countNeg % 2 == 1){
-            totalSum -= (2 * col[0]);
+
+        if (negativeCount % 2 == 1) {
+            totalSum -= 2 * minElement;
         }
         return totalSum;
     }
