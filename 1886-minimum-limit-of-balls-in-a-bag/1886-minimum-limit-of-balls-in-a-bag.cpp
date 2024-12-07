70c964ee8e9f409f6558eaec171314a9fce3e6c4
Ayush Pandey <114850053+AyushPandey1502@users.noreply.github.com>
2024-12-07 08:52:08 +0530

    Time: 39 ms (41.78%), Space: 59.8 MB (22.28%) - LeetHub

--- 1886-minimum-limit-of-balls-in-a-bag/1886-minimum-limit-of-balls-in-a-bag.cpp
+++ 1886-minimum-limit-of-balls-in-a-bag/1886-minimum-limit-of-balls-in-a-bag.cpp
@@ -3,7 +3,7 @@ public:
 
     bool check(int mid, vector<int> &nums, int maxOperations) {
         int n = nums.size(); 
-        for (auto &num: nums) {
+        for (auto num: nums) {
             maxOperations -= (num + mid - 1) / mid - 1;
         }
         return maxOperations >= 0; 
@@ -11,17 +11,17 @@ public:
 
     int minimumSize(vector<int>& nums, int maxOperations) {
         int n = nums.size(); 
-        int l = 1, r = 1e9; 
-        int ans = 1e9; 
-        while (l <= r) {
-            int mid = l + (r - l) / 2; 
+        int left = 1, right = 1e9; 
+        int result = 1e9; 
+        while (left <= right) {
+            int mid = left + (right - left) / 2; 
             if (check(mid, nums, maxOperations)) {
-                ans = mid; 
-                r = mid - 1; 
+                result = mid; 
+                right = mid - 1; 
             } else {
-                l = mid + 1; 
+                left = mid + 1; 
             }
         }
-        return ans; 
+        return result; 
     }
 };
