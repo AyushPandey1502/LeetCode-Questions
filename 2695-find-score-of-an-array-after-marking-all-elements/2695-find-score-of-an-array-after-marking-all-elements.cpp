f30987d40acb6d692a86f72c6494bb9d7702a399
Ayush Pandey <114850053+AyushPandey1502@users.noreply.github.com>
2024-12-13 21:10:41 +0530

    Time: 4 ms (99.28%), Space: 92.2 MB (99.67%) - LeetHub

---
+++ 2695-find-score-of-an-array-after-marking-all-elements/2695-find-score-of-an-array-after-marking-all-elements.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    long long findScore(vector<int>& nums) {
+        int n = nums.size();
+        long long sum = 0;
+        for (int left, right = 0; right < n; right += 2){
+            left = right;
+            while (right + 1 < n && nums[right] > nums[right + 1]) right++;
+            for (int i = right; i >= left; i -= 2) sum += nums[i];
+        }
+        return sum;
+    }
+};
