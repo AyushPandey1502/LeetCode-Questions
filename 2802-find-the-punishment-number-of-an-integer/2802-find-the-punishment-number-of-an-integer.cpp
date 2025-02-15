344cf50aa12fce28e226d55d6aa14a451b6889ef
Ayush Pandey <114850053+AyushPandey1502@users.noreply.github.com>
2025-02-15 17:47:58 +0530

    Time: 1202 ms (5.04%), Space: 194.7 MB (12.34%) - LeetHub

--- 2802-find-the-punishment-number-of-an-integer/2802-find-the-punishment-number-of-an-integer.cpp
+++ 2802-find-the-punishment-number-of-an-integer/2802-find-the-punishment-number-of-an-integer.cpp
@@ -1,24 +1,32 @@
 class Solution {
 public:
-    bool checkWays(string sq, int num, int index, int currSum){
-        if(index == sq.size()) return num == currSum;
-        if(currSum > num) return false;
+    unordered_map<string, bool> memo;
+    
+    bool checkWays(string sq, int num, int index, int currSum) {
+        if (index == sq.size()) return num == currSum;
+        if (currSum > num) return false;
+        
+        string key = to_string(index) + "," + to_string(currSum);
+        if (memo.find(key) != memo.end()) return memo[key];
+        
         bool flag = false;
-        for(int j = index; j < sq.size(); j++){
-            string sub = sq.substr(index, j-index+1);
+        for (int j = index; j < sq.size(); j++) {
+            string sub = sq.substr(index, j - index + 1);
             int val = stoi(sub);
-
-            flag = flag || checkWays(sq, num, j+1, currSum+val);
-            if(flag) return true;
+            
+            if (checkWays(sq, num, j + 1, currSum + val)) {
+                return memo[key] = true;
+            }
         }
-        return false;
+        return memo[key] = false;
     }
-
+    
     int punishmentNumber(int n) {
         int count = 0;
-        for(int i = 1; i <= n; i++){
+        for (int i = 1; i <= n; i++) {
             int sq = i * i;
-            if(checkWays(to_string(sq), i, 0, 0)) count += sq;
+            memo.clear(); 
+            if (checkWays(to_string(sq), i, 0, 0)) count += sq;
         }
         return count;
     }
