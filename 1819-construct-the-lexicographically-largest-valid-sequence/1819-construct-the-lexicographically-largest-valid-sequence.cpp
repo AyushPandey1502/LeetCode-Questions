8bb602ba3c1f770eea21ec02f22b32c05eae8f24
Ayush Pandey <114850053+AyushPandey1502@users.noreply.github.com>
2025-02-16 10:52:59 +0530

    Time: 0 ms (100%), Space: 8.6 MB (64.94%) - LeetHub

--- 1819-construct-the-lexicographically-largest-valid-sequence/1819-construct-the-lexicographically-largest-valid-sequence.cpp
+++ 1819-construct-the-lexicographically-largest-valid-sequence/1819-construct-the-lexicographically-largest-valid-sequence.cpp
@@ -1,31 +1,28 @@
 class Solution {
 public:
     vector<int> constructDistancedSequence(int n) {
-        vector<int> result(2*n-1, 0);
-        vector<bool> visited(n+1, false);
+        vector<int> result(2 * n - 1, 0);
+        vector<bool> visited(n + 1, false);
         backtrack(result, visited, n, 0);
         return result;
     }
 
-    bool backtrack(vector<int>& result, vector<bool>& visited, int n, int index){
-        while(index < result.size() && result[index] != 0) index++;
-        if(index == result.size()) return true;
+private:
+    bool backtrack(vector<int>& result, vector<bool>& visited, int n, int index) {
+        while (index < result.size() && result[index] != 0) index++;
+        if (index == result.size()) return true;
 
-        for(int i = n; i >= 1; i--){
-            if(visited[i]) continue;
-            if(i == 1){
-                result[index] = 1;
-                visited[1] = true;
-                if(backtrack(result, visited, n, index+1)) return true;
-                result[index] = 0;
-                visited[i] = false;
-            }else if(index + i < result.size() && result[index + i] == 0){
+        for (int i = n; i >= 1; i--) {
+            if (visited[i]) continue;
+            
+            int secondIndex = (i > 1) ? index + i : index;
+            if (secondIndex < result.size() && result[secondIndex] == 0) {
                 result[index] = i;
-                result[index+i] = i;
+                if (i > 1) result[secondIndex] = i;
                 visited[i] = true;
-                if(backtrack(result, visited, n, index+1)) return true;
+                if (backtrack(result, visited, n, index + 1)) return true;
                 result[index] = 0;
-                result[index+i] = 0;
+                if (i > 1) result[secondIndex] = 0;
                 visited[i] = false;
             }
         }
