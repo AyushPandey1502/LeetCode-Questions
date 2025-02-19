484fe04fa4525a1cea9738a1379e98bfeb9a7950
Ayush Pandey <114850053+AyushPandey1502@users.noreply.github.com>
2025-02-19 10:22:36 +0530

    Time: 91 ms (5.69%), Space: 30.8 MB (6.63%) - LeetHub

---
+++ 1516-the-k-th-lexicographical-string-of-all-happy-strings-of-length-n/1516-the-k-th-lexicographical-string-of-all-happy-strings-of-length-n.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    set<string> result;
+
+    void generateStrings(string s, int n) {
+        if (n == 0) {
+            result.insert(s);
+            return;
+        }
+        for (char ch = 'a'; ch <= 'c'; ch++) { 
+            if (!s.empty() && s.back() == ch) continue; 
+            generateStrings(s + ch, n - 1);
+        }
+    }
+
+    string getHappyString(int n, int k) {
+        generateStrings("", n);
+        if (result.size() < k) return "";
+        auto it = result.begin();
+        advance(it, k - 1); 
+        return *it;
+    }
+};
