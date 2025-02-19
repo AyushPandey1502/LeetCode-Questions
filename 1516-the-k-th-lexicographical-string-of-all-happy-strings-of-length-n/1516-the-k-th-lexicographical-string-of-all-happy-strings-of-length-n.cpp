57f58cb23c092566d73a091faeef96b9589a6aaa
Ayush Pandey <114850053+AyushPandey1502@users.noreply.github.com>
2025-02-19 10:55:30 +0530

    Time: 0 ms (100%), Space: 8.5 MB (83.1%) - LeetHub

--- 1516-the-k-th-lexicographical-string-of-all-happy-strings-of-length-n/1516-the-k-th-lexicographical-string-of-all-happy-strings-of-length-n.cpp
+++ 1516-the-k-th-lexicographical-string-of-all-happy-strings-of-length-n/1516-the-k-th-lexicographical-string-of-all-happy-strings-of-length-n.cpp
@@ -1,23 +1,37 @@
 class Solution {
 public:
     string getHappyString(int n, int k) {
-        stack<string> st;
-        st.push("");
-        int index = 0;
+        int total = 3 * (1 << (n-1));
+        if(total < k) return "";
+        string result(n, 'a');
+        unordered_map<char, char> nextSmallest = {
+            {'a', 'b'}, {'b', 'a'}, {'c', 'a'}
+        };
+        unordered_map<char, char> nextGreatest = {
+            {'a', 'c'}, {'b', 'c'}, {'c', 'b'}
+        };
 
-        while(!st.empty()){
-            string s = st.top();
-            st.pop();
-            if(s.size() == n){
-                index++;
-                if(index == k) return s;
-                continue;
-            }
-            for(char ch = 'c'; ch >= 'a'; ch--){
-                if(!s.empty() && s.back() == ch) continue;
-                st.push(s+ch);
-            }            
+        int startA = 1;
+        int startB = startA + (1 << (n-1));
+        int startC = startB + (1 << (n-1));
+
+        if(k < startB) k -= startA;
+        else if(k < startC){
+            result[0] = 'b';
+            k -= startB;
+        }else{
+            result[0] = 'c';
+            k -= startC;
         }
-        return "";
+
+        for(int i = 1; i < n; i++){
+            int mid = (1 << (n - i - 1));
+            if(k < mid) result[i] = nextSmallest[result[i-1]];
+            else{
+                result[i] = nextGreatest[result[i-1]];
+                k -= mid;
+            }
+        }
+        return result;
     }
 };
