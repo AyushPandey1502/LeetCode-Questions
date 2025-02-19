f5841987dca496afaa509c7a86af9113fe4eef6e
Ayush Pandey <114850053+AyushPandey1502@users.noreply.github.com>
2025-02-19 10:41:14 +0530

    Time: 0 ms (100%), Space: 9.4 MB (74.09%) - LeetHub

--- 1516-the-k-th-lexicographical-string-of-all-happy-strings-of-length-n/1516-the-k-th-lexicographical-string-of-all-happy-strings-of-length-n.cpp
+++ 1516-the-k-th-lexicographical-string-of-all-happy-strings-of-length-n/1516-the-k-th-lexicographical-string-of-all-happy-strings-of-length-n.cpp
@@ -1,22 +1,23 @@
 class Solution {
 public:
-    void generateStrings(string s, int n, int k, int& index, string& result) {
-        if (n == 0) {
-            index++;
-            if (index == k) result = s;
-            return;
-        }
-        for (char ch = 'a'; ch <= 'c'; ch++) { 
-            if (!s.empty() && s.back() == ch) continue;
-            generateStrings(s + ch, n - 1, k, index, result);
-            if (!result.empty()) return; 
-        }
-    }
-
     string getHappyString(int n, int k) {
-        string result;
+        stack<string> st;
+        st.push("");
         int index = 0;
-        generateStrings("", n, k, index, result);
-        return result;
+
+        while(!st.empty()){
+            string s = st.top();
+            st.pop();
+            if(s.size() == n){
+                index++;
+                if(index == k) return s;
+                continue;
+            }
+            for(char ch = 'c'; ch >= 'a'; ch--){
+                if(!s.empty() && s.back() == ch) continue;
+                st.push(s+ch);
+            }            
+        }
+        return "";
     }
 };
