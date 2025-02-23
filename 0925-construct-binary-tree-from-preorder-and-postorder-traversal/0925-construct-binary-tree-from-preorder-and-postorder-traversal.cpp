9d485e235a61774467f44500d1f5e35388fbb2c1
Ayush Pandey <114850053+AyushPandey1502@users.noreply.github.com>
2025-02-23 18:28:14 +0530

    Time: 0 ms (100%), Space: 28.6 MB (21.54%) - LeetHub

--- 0925-construct-binary-tree-from-preorder-and-postorder-traversal/0925-construct-binary-tree-from-preorder-and-postorder-traversal.cpp
+++ 0925-construct-binary-tree-from-preorder-and-postorder-traversal/0925-construct-binary-tree-from-preorder-and-postorder-traversal.cpp
@@ -1,22 +1,28 @@
 class Solution {
 public:
     TreeNode* constructFromPrePost(vector<int>& preorder, vector<int>& postorder) {
-        int n = preorder.size();
         unordered_map<int, int> postMap;
-        for(int i = 0; i < n; i++) postMap[postorder[i]] = i;
-        TreeNode* root = constructTree(preorder, 0, n-1, postorder, 0, n-1, postMap);
-        return root;
+        for(int i = 0; i < postorder.size(); i++) postMap[postorder[i]] = i;
+        return constructTree(preorder, 0, preorder.size() - 1, postorder, 0, postorder.size() - 1, postMap);
     }
 
-    TreeNode* constructTree(vector<int>& preorder, int preStart, int preEnd, vector<int>& postorder, int postStart, int postEnd, unordered_map<int, int>& postMap){
-        if(preStart > preEnd || postStart > postEnd) return nullptr;
+private:
+    TreeNode* constructTree(vector<int>& preorder, int preStart, int preEnd, 
+                            vector<int>& postorder, int postStart, int postEnd, 
+                            unordered_map<int, int>& postMap) {
+        if (preStart > preEnd) return nullptr;
 
         TreeNode* root = new TreeNode(preorder[preStart]);
-        if(preStart == preEnd) return root;
-        int numsLeft = postMap[preorder[preStart + 1]] - postStart + 1;
-
-        root->left = constructTree(preorder, preStart + 1, numsLeft + preStart, postorder, postStart, postStart + numsLeft - 1, postMap); 
-        root->right = constructTree(preorder, preStart + numsLeft + 1, preEnd, postorder, postStart + numsLeft, postEnd - 1, postMap);
+        if (preStart == preEnd) return root;
+        
+        int leftChildIdx = postMap[preorder[preStart + 1]];
+        int numsLeft = leftChildIdx - postStart + 1;
+        
+        root->left = constructTree(preorder, preStart + 1, preStart + numsLeft, 
+                                   postorder, postStart, leftChildIdx, postMap);
+        root->right = constructTree(preorder, preStart + numsLeft + 1, preEnd, 
+                                    postorder, leftChildIdx + 1, postEnd - 1, postMap);
+        
         return root;
     }
 };
