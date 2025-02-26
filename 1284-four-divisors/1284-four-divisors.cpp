813ca61e1b29163caa980e10bf996fec24e48b02
Ayush Pandey <114850053+AyushPandey1502@users.noreply.github.com>
2025-02-26 11:53:57 +0530

    Time: 100 ms (31.52%), Space: 40.8 MB (6.74%) - LeetHub

--- 1284-four-divisors/1284-four-divisors.cpp
+++ 1284-four-divisors/1284-four-divisors.cpp
@@ -1,29 +1,44 @@
 class Solution {
 public:
-    vector<int> sumDivisors;
+    vector<int> primes;
 
-    void precomputeDivisors(int limit) {
-        sumDivisors.assign(limit + 1, 0);
-        vector<int> countDivisors(limit + 1, 0);
-
-        for (int i = 1; i <= limit; i++) {
-            for (int j = i; j <= limit; j += i) {
-                countDivisors[j]++;
-                sumDivisors[j] += i;
+    void sieve() {
+        vector<bool> is_prime(1e5 + 1, true);
+        is_prime[0] = is_prime[1] = false;
+        for (int i = 2; i * i <= 1e5; i++) {
+            if (is_prime[i]) {
+                for (int p = i * i; p <= 1e5; p += i) {
+                    is_prime[p] = false;
+                }
             }
         }
-        for (int i = 1; i <= limit; i++) {
-            if (countDivisors[i] != 4) sumDivisors[i] = 0;  
+        for (int i = 2; i <= 1e5; i++) {
+            if (is_prime[i]) primes.push_back(i);
         }
     }
 
-    int sumFourDivisors(vector<int>& nums) {
-        int maxNum = *max_element(nums.begin(), nums.end());
-        precomputeDivisors(maxNum);
+    vector<int> findDivisors(int num) {
+        vector<int> divisors;
+        for (int i = 1; i * i <= num; i++) {
+            if (num % i == 0) {
+                divisors.push_back(i);
+                if (i != num / i) {
+                    divisors.push_back(num / i);
+                }
+            }
+        }
+        return divisors;
+    }
 
+    int sumFourDivisors(vector<int>& nums) {
         int result = 0;
+        sieve();
+        
         for (int num : nums) {
-            result += sumDivisors[num]; 
+            vector<int> divisors = findDivisors(num);
+            if (divisors.size() == 4) {
+                result += accumulate(divisors.begin(), divisors.end(), 0);
+            }
         }
         return result;
     }
