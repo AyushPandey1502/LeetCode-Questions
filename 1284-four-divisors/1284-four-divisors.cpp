2d7831c2ae468e2880cf76c4061b7193f8e561c8
Ayush Pandey <114850053+AyushPandey1502@users.noreply.github.com>
2025-02-26 11:49:01 +0530

    Time: 62 ms (41.96%), Space: 27.6 MB (17.61%) - LeetHub

--- 1284-four-divisors/1284-four-divisors.cpp
+++ 1284-four-divisors/1284-four-divisors.cpp
@@ -1,20 +1,29 @@
 class Solution {
 public:
+    vector<int> sumDivisors;
+
+    void precomputeDivisors(int limit) {
+        sumDivisors.assign(limit + 1, 0);
+        vector<int> countDivisors(limit + 1, 0);
+
+        for (int i = 1; i <= limit; i++) {
+            for (int j = i; j <= limit; j += i) {
+                countDivisors[j]++;
+                sumDivisors[j] += i;
+            }
+        }
+        for (int i = 1; i <= limit; i++) {
+            if (countDivisors[i] != 4) sumDivisors[i] = 0;  
+        }
+    }
+
     int sumFourDivisors(vector<int>& nums) {
+        int maxNum = *max_element(nums.begin(), nums.end());
+        precomputeDivisors(maxNum);
+
         int result = 0;
         for (int num : nums) {
-            int sum = 0, count = 0;
-            for (int i = 1; i * i <= num; i++) {
-                if (num % i == 0) {
-                    sum += i;
-                    count++;
-                    if (i != num / i) { 
-                        sum += num / i;
-                        count++;
-                    }
-                }
-            }
-            if (count == 4) result += sum;
+            result += sumDivisors[num]; 
         }
         return result;
     }
