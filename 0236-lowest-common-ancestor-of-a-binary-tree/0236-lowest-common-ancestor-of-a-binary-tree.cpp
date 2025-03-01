c3e984b70f8b3bf2081387115ef28317bacf6425
Ayush Pandey <114850053+AyushPandey1502@users.noreply.github.com>
2025-03-01 13:30:18 +0530

    Time: 13 ms (45.94%), Space: 17.3 MB (67.7%) - LeetHub

--- 0236-lowest-common-ancestor-of-a-binary-tree/0236-lowest-common-ancestor-of-a-binary-tree.cpp
+++ 0236-lowest-common-ancestor-of-a-binary-tree/0236-lowest-common-ancestor-of-a-binary-tree.cpp
@@ -1,23 +1,18 @@
-// BRUTE FORCE APPROACH
 class Solution {
 public:
-    bool getPath(TreeNode* root, TreeNode* p, vector<TreeNode*> &arr){
-        if(root == NULL) return false;
-        arr.push_back(root);
-        if(root == p) return true;
-        if(getPath(root->left, p, arr) || getPath(root->right, p, arr)) return true;
-        arr.pop_back();
-        return false;
+    TreeNode* dfs(TreeNode* root, TreeNode* p, TreeNode* q){
+        if(root == nullptr) return nullptr;
+        if(root == p) return p;
+        if(root == q) return q;
+        TreeNode* lst = dfs(root->left, p, q);
+        TreeNode* rst = dfs(root->right, p, q);
+        if(lst && rst) return root;
+        if(lst) return lst;
+        return rst;
     }
+
     TreeNode* lowestCommonAncestor(TreeNode* root, TreeNode* p, TreeNode* q) {
-        vector<TreeNode*> path1, path2;
-        getPath(root, p, path1);
-        getPath(root, q, path2);
-        int i = 0;
-        while(i < path1.size() && i < path2.size()){
-            if(path1[i] != path2[i]) return path1[i-1];
-            i++;
-        }
-        return path1[i-1];
+        if(root == nullptr) return root;
+        return dfs(root, p, q);
     }
 };
