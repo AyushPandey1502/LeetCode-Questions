f78b01a2198ca54ed0ba190c62f139503d0dfb64
Ayush Pandey <114850053+AyushPandey1502@users.noreply.github.com>
2025-03-01 14:57:16 +0530

    Time: 3 ms (82.04%), Space: 15.4 MB (68.75%) - LeetHub

--- 0893-all-nodes-distance-k-in-binary-tree/0893-all-nodes-distance-k-in-binary-tree.cpp
+++ 0893-all-nodes-distance-k-in-binary-tree/0893-all-nodes-distance-k-in-binary-tree.cpp
@@ -1,27 +1,15 @@
 class Solution {
 public:
-    vector<int> result;
-    unordered_map<TreeNode*, TreeNode*> parent;
-
-    void findNodes(TreeNode* root, TreeNode* prev, int k) {
-        if (!root || k < 0) return;
-        if (k == 0) {
-            result.push_back(root->val);
-            return;
-        }
-        if (prev != parent[root]) findNodes(parent[root], root, k - 1);
-        if (prev != root->left) findNodes(root->left, root, k - 1);
-        if (prev != root->right) findNodes(root->right, root, k - 1);
-    }
-
     vector<int> distanceK(TreeNode* root, TreeNode* target, int k) {
-        result.clear();
-        parent.clear();
+        if (!root) return {};
 
+        unordered_map<TreeNode*, TreeNode*> parent;
         queue<TreeNode*> q;
+        unordered_set<TreeNode*> visited;
+        vector<int> result;
+
         q.push(root);
         parent[root] = nullptr;
-
         while (!q.empty()) {
             TreeNode* node = q.front();
             q.pop();
@@ -34,7 +22,39 @@ public:
                 q.push(node->right);
             }
         }
-        findNodes(target, nullptr, k);
+
+        q.push(target);
+        visited.insert(target);
+        int level = 0;
+
+        while (!q.empty()) {
+            int size = q.size();
+            if (level == k) {
+                while (!q.empty()) {
+                    result.push_back(q.front()->val);
+                    q.pop();
+                }
+                return result;
+            }
+            for (int i = 0; i < size; i++) {
+                TreeNode* node = q.front();
+                q.pop();
+                if (node->left && visited.find(node->left) == visited.end()) {
+                    q.push(node->left);
+                    visited.insert(node->left);
+                }
+                if (node->right && visited.find(node->right) == visited.end()) {
+                    q.push(node->right);
+                    visited.insert(node->right);
+                }
+                if (parent[node] && visited.find(parent[node]) == visited.end()) {
+                    q.push(parent[node]);
+                    visited.insert(parent[node]);
+                }
+            }
+            level++;
+        }
+
         return result;
     }
 };
