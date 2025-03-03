a2dd03af28bd4c12e435f3502b06227441bf8df6
Ayush Pandey <114850053+AyushPandey1502@users.noreply.github.com>
2025-03-03 09:40:17 +0530

    Time: 8 ms (50.55%), Space: 127.8 MB (71.24%) - LeetHub

--- 2265-partition-array-according-to-given-pivot/2265-partition-array-according-to-given-pivot.cpp
+++ 2265-partition-array-according-to-given-pivot/2265-partition-array-according-to-given-pivot.cpp
@@ -1,16 +1,18 @@
 class Solution {
 public:
     vector<int> pivotArray(vector<int>& nums, int pivot) {
-        vector<int> lesser, greater;
-        int count = 0, n = nums.size();
-        for(int i = 0; i < n; i++){
-            if(nums[i] < pivot) lesser.push_back(nums[i]);
-            else if(nums[i] == pivot) count++;
-            else greater.push_back(nums[i]);
+        vector<int> result;
+        result.reserve(nums.size());
+        
+        for (int num : nums){
+            if (num < pivot) result.push_back(num);
         }
-        vector<int> result(lesser.begin(), lesser.end());
-        for(int i = 0; i < count; i++) result.push_back(pivot);
-        result.insert(result.end(), greater.begin(), greater.end());
+        for (int num : nums){
+            if (num == pivot) result.push_back(num);
+        }
+        for (int num : nums){
+            if (num > pivot) result.push_back(num);
+        }
         return result;
     }
 };
