44eb8a807bf4871bb387d8504b5254d63cd38d43
Ayush Pandey <114850053+AyushPandey1502@users.noreply.github.com>
2025-03-04 02:13:41 +0530

    Time: 0 ms (100%), Space: 11.7 MB (79.6%) - LeetHub

--- 0063-unique-paths-ii/0063-unique-paths-ii.cpp
+++ 0063-unique-paths-ii/0063-unique-paths-ii.cpp
@@ -4,16 +4,17 @@ public:
         int m = mat.size();
         int n = mat[0].size();
         if(mat[0][0] == 1) return 0; 
-        vector<vector<int>> dp(m, vector<int>(n, -1));
+        vector<int> dp(n, 0), temp(n, 0);
         for(int i = 0; i < m; i++){
             for(int j = 0; j < n; j++){
-                if(i == 0 && j == 0) dp[i][j] = 1;
+                if(i == 0 && j == 0) temp[j] = 1;
                 else{
-                    if(mat[i][j] == 1) dp[i][j] = 0;
-                    else dp[i][j] = (i > 0 ? dp[i-1][j] : 0) + (j > 0 ? dp[i][j-1] : 0);
+                    if(mat[i][j] == 1) temp[j] = 0;
+                    else temp[j] = (i > 0 ? dp[j] : 0) + (j > 0 ? temp[j-1] : 0);
                 }
             }
+            dp = temp;
         }
-        return dp[m-1][n-1];
+        return dp[n-1];
     }
 };
