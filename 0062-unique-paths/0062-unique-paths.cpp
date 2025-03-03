692b501253a2f020842fa8f2d1e7798cb1befc86
Ayush Pandey <114850053+AyushPandey1502@users.noreply.github.com>
2025-03-04 02:00:34 +0530

    Time: 0 ms (100%), Space: 9.4 MB (8.96%) - LeetHub

--- 0062-unique-paths/0062-unique-paths.cpp
+++ 0062-unique-paths/0062-unique-paths.cpp
@@ -1,14 +1,13 @@
 class Solution {
 public:
-    int countPaths(int row, int col, vector<vector<int>>& dp){
-        if(row == 0 && col == 0) return 1;
-        if(row < 0 || col < 0) return 0;
-        if(dp[row][col] != -1) return dp[row][col];
-        return dp[row][col] =  countPaths(row-1, col, dp) + countPaths(row, col-1, dp);
-    }
-
     int uniquePaths(int m, int n) {
         vector<vector<int>> dp(m, vector<int>(n, -1));
-        return countPaths(m-1, n-1, dp);
+        for(int i = 0; i < m; i++){
+            for(int j = 0; j < n; j++){
+                if(i == 0 && j == 0) dp[0][0] = 1;
+                else dp[i][j] = (i > 0 ? dp[i-1][j] : 0) + (j > 0 ? dp[i][j-1] : 0);
+            }
+        }
+        return dp[m-1][n-1];
     }
 };
