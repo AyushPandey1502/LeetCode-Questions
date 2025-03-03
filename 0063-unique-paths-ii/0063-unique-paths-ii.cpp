b0d52bd5c32b6d8eef249c85fda3e474f110c837
Ayush Pandey <114850053+AyushPandey1502@users.noreply.github.com>
2025-03-04 02:11:38 +0530

    Time: 0 ms (100%), Space: 12 MB (28.73%) - LeetHub

--- 0063-unique-paths-ii/0063-unique-paths-ii.cpp
+++ 0063-unique-paths-ii/0063-unique-paths-ii.cpp
@@ -1,19 +1,19 @@
 class Solution {
-public:
-    int countPaths(int row, int col, vector<vector<int>>& dp, vector<vector<int>>& mat) {
-        if(row < 0 || col < 0) return 0;
-        if(mat[row][col] == 1) return 0; 
-        if(row == 0 && col == 0) return 1;
-        if(dp[row][col] != -1) return dp[row][col];
-
-        return dp[row][col] = countPaths(row-1, col, dp, mat) + countPaths(row, col-1, dp, mat);
-    }
-    
-    int uniquePathsWithObstacles(vector<vector<int>>& obstacleGrid) {
-        int m = obstacleGrid.size();
-        int n = obstacleGrid[0].size();
-        if(obstacleGrid[0][0] == 1) return 0; 
+public:    
+    int uniquePathsWithObstacles(vector<vector<int>>& mat) {
+        int m = mat.size();
+        int n = mat[0].size();
+        if(mat[0][0] == 1) return 0; 
         vector<vector<int>> dp(m, vector<int>(n, -1));
-        return countPaths(m-1, n-1, dp, obstacleGrid);
+        for(int i = 0; i < m; i++){
+            for(int j = 0; j < n; j++){
+                if(i == 0 && j == 0) dp[i][j] = 1;
+                else{
+                    if(mat[i][j] == 1) dp[i][j] = 0;
+                    else dp[i][j] = (i > 0 ? dp[i-1][j] : 0) + (j > 0 ? dp[i][j-1] : 0);
+                }
+            }
+        }
+        return dp[m-1][n-1];
     }
 };
