0b09239b0043495b22812359370e6c14a0597d44
Ayush Pandey <114850053+AyushPandey1502@users.noreply.github.com>
2025-03-04 13:22:17 +0530

    Time: 0 ms (100%), Space: 7.9 MB (19.92%) - LeetHub

--- 0231-power-of-two/0231-power-of-two.cpp
+++ 0231-power-of-two/0231-power-of-two.cpp
@@ -1,8 +1,6 @@
 class Solution {
 public:
     bool isPowerOfTwo(int n) {
-        if(n <= 0) return false;
-        int setBits = __builtin_popcount(n);
-        return (setBits == 1);
+        return ((n > 0) && ((n & (n - 1)) == 0));
     }
 };
