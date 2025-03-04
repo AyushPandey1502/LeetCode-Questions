8e74cb19e033def8faaca403b8acaea2cc882f0d
Ayush Pandey <114850053+AyushPandey1502@users.noreply.github.com>
2025-03-04 16:11:10 +0530

    Time: 74 ms (19.75%), Space: 20.9 MB (66.63%) - LeetHub

--- 1559-cherry-pickup-ii/1559-cherry-pickup-ii.cpp
+++ 1559-cherry-pickup-ii/1559-cherry-pickup-ii.cpp
@@ -1,32 +1,32 @@
 class Solution {
 public:
-    int m, n;
-
-    int maxCherry(int row, int col1, int col2, vector<vector<vector<int>>>& dp, vector<vector<int>>& grid) {
-        if (col1 < 0 || col1 >= n || col2 < 0 || col2 >= n) return -1e8;
-        if (row == m - 1) {
-            if (col1 == col2) return grid[row][col1];
-            else return grid[row][col1] + grid[row][col2];
+    int cherryPickup(vector<vector<int>>& grid) {
+        int m = grid.size(), n = grid[0].size();
+        vector<vector<vector<int>>> dp(m, vector<vector<int>>(n, vector<int>(n, 0)));
+        for(int i = 0; i < n; i++){
+            for(int j = 0; j < n; j++){
+                dp[m-1][i][j] = (i == j ? grid[m-1][i] : grid[m-1][i] + grid[m-1][j]);
+            }
         }
-        if (dp[row][col1][col2] != -1) return dp[row][col1][col2];
 
-        int result = -1e8;
-        for (int d1 = -1; d1 <= 1; d1++) {
-            for (int d2 = -1; d2 <= 1; d2++) {
-                int ncol1 = col1 + d1, ncol2 = col2 + d2;
-                if (ncol1 >= 0 && ncol1 < n && ncol2 >= 0 && ncol2 < n) {
-                    int val = grid[row][col1];
-                    if (col1 != col2) val += grid[row][col2];
-                    result = max(result, val + maxCherry(row + 1, ncol1, ncol2, dp, grid));
+        for(int i = m-2; i >= 0; i--){
+            for(int j1 = 0; j1 < n; j1++){
+                for(int j2 = 0; j2 < n; j2++){
+                    int result = -1e8;
+                    for(int d1 = -1; d1 <= 1; d1++){
+                        for(int d2 = -1; d2 <= 1; d2++){
+                            int ncol1 = j1 + d1, ncol2 = j2 + d2;
+                            if(ncol1 >= 0 && ncol1 < n && ncol2 >= 0 && ncol2 < n){
+                                int val = grid[i][j1];
+                                if (j1 != j2) val += grid[i][j2];
+                                result = max(result, val + dp[i+1][ncol1][ncol2]);
+                            }
+                        }
+                    }
+                    dp[i][j1][j2] = result; 
                 }
             }
         }
-        return dp[row][col1][col2] = result;
-    }
-
-    int cherryPickup(vector<vector<int>>& grid) {
-        m = grid.size(), n = grid[0].size();
-        vector<vector<vector<int>>> dp(m, vector<vector<int>>(n, vector<int>(n, -1)));
-        return maxCherry(0, 0, n - 1, dp, grid);
+        return dp[0][0][n-1];
     }
 };
