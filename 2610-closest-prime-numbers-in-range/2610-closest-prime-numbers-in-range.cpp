a6cb9555fb809f6425e0a658651583ef003592ba
Ayush Pandey <114850053+AyushPandey1502@users.noreply.github.com>
2025-03-07 09:54:47 +0530

    Time: 21 ms (88.52%), Space: 8.2 MB (96.48%) - LeetHub

--- 2610-closest-prime-numbers-in-range/2610-closest-prime-numbers-in-range.cpp
+++ 2610-closest-prime-numbers-in-range/2610-closest-prime-numbers-in-range.cpp
@@ -1,36 +1,37 @@
 class Solution {
 public:
-    unordered_set<int> primes;
-     Solution() { 
-        sieve();
+    static bitset<1000001> is_prime;
+    
+    Solution() { 
+        if (!is_prime[2]) sieve();
     }
-    void sieve(){
-        vector<bool> is_prime(1e6+1, true);
-        for(int i = 2; i * i <= 1e6; i++){
-            if(is_prime[i]){
-                for(int p = i*i; p <= 1e6; p+=i) is_prime[p] = false;
+
+    void sieve() {
+        is_prime.set();
+        is_prime[0] = is_prime[1] = 0;
+        for (int i = 2; i * i <= 1e6; i++) {
+            if (is_prime[i]) {
+                for (int p = i * i; p <= 1e6; p += i) 
+                    is_prime[p] = 0;
             }
-        }
-        for(int i = 2; i <= 1e6; i++){
-            if(is_prime[i]) primes.insert(i);
         }
     }
 
     vector<int> closestPrimes(int left, int right) {
-        vector<int> range;
-        for(int i = left; i <= right; i++){
-            if(primes.find(i) != primes.end()) range.push_back(i);
-        }
-        if(range.size() <= 1) return {-1, -1};
-        int num1 = -1, num2 = -1, minDiff = INT_MAX;
-        for(int i = 0; i < range.size()-1; i++){
-            int diff = range[i+1] - range[i];
-            if(diff < minDiff){
-                minDiff = diff;
-                num1 = range[i];
-                num2 = range[i+1];
+        int prev = -1, num1 = -1, num2 = -1, minDiff = INT_MAX;
+        
+        for (int i = left; i <= right; i++) {
+            if (is_prime[i]) {
+                if (prev != -1 && i - prev < minDiff) {
+                    minDiff = i - prev;
+                    num1 = prev;
+                    num2 = i;
+                }
+                prev = i;
             }
         }
-        return {num1, num2};
+        return num1 == -1 ? vector<int>{-1, -1} : vector<int>{num1, num2};
     }
 };
+
+bitset<1000001> Solution::is_prime;
