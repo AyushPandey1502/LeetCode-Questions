59fe2c7c00ea90bb7f410be11dee5572d241292b
Ayush Pandey <114850053+AyushPandey1502@users.noreply.github.com>
2025-03-13 23:18:39 +0530

    Time: 14 ms (96.3%), Space: 12.5 MB (9.26%) - LeetHub

--- 3210-count-beautiful-substrings-i/3210-count-beautiful-substrings-i.cpp
+++ 3210-count-beautiful-substrings-i/3210-count-beautiful-substrings-i.cpp
@@ -1,16 +1,24 @@
 class Solution {
 public:
-    int beautifulSubstrings(string s, int k) {
-        int result = 0;
+    long long beautifulSubstrings(string s, int k) {
+        long long result = 0;
+        unordered_map<int, vector<pair<int, int>>> map;
+        map[0].push_back({0, 0}); 
         
-        for (int i = 0; i < s.size(); i++) {
-            int vow = 0, cons = 0;
-            for (int j = i; j < s.size(); j++) {
-                if (s[j] == 'a' || s[j] == 'e' || s[j] == 'i' || s[j] == 'o' || s[j] == 'u') vow++;
-                else cons++;
-                if (vow == cons && (vow * cons) % k == 0) result++;
+        int vows = 0, cons = 0;
+        for (int i = 0; i < s.length(); i++) {
+            char ch = s[i];
+            if (ch == 'a' || ch == 'e' || ch == 'i' || ch == 'o' || ch == 'u') vows++;
+            else cons++;
+            int diff = vows - cons;
+            for (auto it : map[diff]) {
+                int vowels = vows - it.first; 
+                int consonants = cons - it.second;  
+                if (vowels == consonants && (vowels * vowels) % k == 0) result++;
             }
+            map[diff].push_back({vows, cons});
         }
+        
         return result;
     }
 };
