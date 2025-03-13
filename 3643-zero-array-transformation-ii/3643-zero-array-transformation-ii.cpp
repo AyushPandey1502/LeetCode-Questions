5895ec4036cfbe1d6e96ed31ae581ce05eab1c70
Ayush Pandey <114850053+AyushPandey1502@users.noreply.github.com>
2025-03-13 14:54:45 +0530

    Time: 43 ms (67.98%), Space: 330.4 MB (66.1%) - LeetHub

--- 3643-zero-array-transformation-ii/3643-zero-array-transformation-ii.cpp
+++ 3643-zero-array-transformation-ii/3643-zero-array-transformation-ii.cpp
@@ -1,22 +1,35 @@
 class Solution {
 public:
     int minZeroArray(vector<int>& nums, vector<vector<int>>& queries) {
-        int n = nums.size(), sum = 0, k = 0;
-        vector<int> diffArr(n + 1);
+        int n = nums.size(), q = queries.size();
+        int left = 1, right = q;
 
-        for (int i = 0; i < n; i++) {
-            while (sum + diffArr[i] < nums[i]) {
-                k++;
-                if (k > queries.size()) return -1;
-                int left = queries[k - 1][0], right = queries[k - 1][1],
-                    val = queries[k - 1][2];
-                if (right >= i) {
-                    diffArr[max(left, i)] += val;
-                    diffArr[right + 1] -= val;
-                }
+        if(all_of(nums.begin(), nums.end(), [](int x){return x == 0;})) return 0;
+
+        auto check = [&](int mid) {
+            vector<int> diffArr(n + 1, 0);
+            for (int i = 0; i < mid; i++) {
+                int start = queries[i][0];
+                int end = queries[i][1];
+                int val = queries[i][2];
+                diffArr[start] += val;
+                diffArr[end + 1] -= val;
             }
-            sum += diffArr[i];
+            int sum = 0;
+            for (int i = 0; i < n; i++) {
+                sum += diffArr[i];
+                if (sum < nums[i]) return false; 
+            }
+            return true;
+        };
+
+        if (!check(q)) return -1;
+        while (left <= right) {
+            int mid = left + (right - left) / 2;
+            if (check(mid)) right = mid - 1;
+            else left = mid + 1;
         }
-        return k;
+
+        return left;
     }
 };
