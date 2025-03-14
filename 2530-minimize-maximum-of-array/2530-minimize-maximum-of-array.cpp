b5407c061ea6d3d31148425572aa1dd3e46fc559
Ayush Pandey <114850053+AyushPandey1502@users.noreply.github.com>
2025-03-14 16:21:18 +0530

    Time: 0 ms (100%), Space: 74.9 MB (86.03%) - LeetHub

--- 2530-minimize-maximum-of-array/2530-minimize-maximum-of-array.cpp
+++ 2530-minimize-maximum-of-array/2530-minimize-maximum-of-array.cpp
@@ -1,26 +1,12 @@
 class Solution {
 public:
     int minimizeArrayValue(vector<int>& nums) {
-        int left = 0, right = *max_element(nums.begin(), nums.end());
-        int result = right;
-
-        auto check = [&](int mid) {
-            long long extra = 0;
-            for (int i = nums.size() - 1; i >= 0; i--) {
-                if (nums[i] + extra > mid) {
-                    extra = nums[i] + extra - mid;
-                } else extra = 0;
-            }
-            return extra == 0;
-        };
-
-        while (left <= right) {
-            int mid = left + (right - left) / 2;
-            if (check(mid)) {
-                result = mid;
-                right = mid - 1;
-            } else left = mid + 1;
+        long long result = 0, prefixSum = 0;
+        for (int i = 0; i < nums.size(); ++i) {
+            prefixSum += nums[i];
+            result = max(result, (prefixSum + i) / (i + 1));
         }
+        
         return result;
     }
 };
