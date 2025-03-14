52491a43778972166607880bff8d7d0e03c814d6
Ayush Pandey <114850053+AyushPandey1502@users.noreply.github.com>
2025-03-14 14:49:01 +0530

    Time: 86 ms (30.39%), Space: 105.4 MB (40.69%) - LeetHub

--- 2000-minimum-speed-to-arrive-on-time/2000-minimum-speed-to-arrive-on-time.cpp
+++ 2000-minimum-speed-to-arrive-on-time/2000-minimum-speed-to-arrive-on-time.cpp
@@ -2,13 +2,13 @@ class Solution {
 public:
     int minSpeedOnTime(vector<int>& dist, double hour) {
         int n = dist.size(), result = -1;
-        int left = 1, right = *max_element(dist.begin(), dist.end());
+        int left = 1, right = 1e9;
 
         auto check = [&](int mid){
             double time = 0;
             for(int i = 0; i < n; i++){
                 if(i < n-1) time += (dist[i] + mid -1) / mid;
-                else time += dist[i] / mid;
+                else time += (double)dist[i] / mid;
                 if(time > hour) return false;
             }
             return time <= hour;
