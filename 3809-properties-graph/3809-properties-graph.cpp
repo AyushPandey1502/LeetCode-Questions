caaf482cdcfe8bd62c0594983b30cdfb8c063eeb
Ayush Pandey <114850053+AyushPandey1502@users.noreply.github.com>
2025-03-24 02:08:39 +0530

    Time: 2818 ms (11.11%), Space: 500.8 MB (11.11%) - LeetHub

--- 3809-properties-graph/3809-properties-graph.cpp
+++ 3809-properties-graph/3809-properties-graph.cpp
@@ -1,54 +1,43 @@
 class Solution {
 public:
-    int numberOfComponents(vector<vector<int>>& properties, int k) {
-        int n = properties.size();
-        vector<unordered_set<int>> propSets(n);
-        vector<int> parent(n), rank(n, 1);
-
-        for (int i = 0; i < n; i++) {
-            propSets[i] = unordered_set<int>(properties[i].begin(), properties[i].end());
-            parent[i] = i;
+    bool intersect(unordered_set<int> a, unordered_set<int> b, int k){
+        int count = 0;
+        for(auto it : b){
+            if(a.count(it)) count++;
+            if(count >= k) return true;
         }
+        return false;
+    }
 
-        function<int(int)> find = [&](int x) {
-            return parent[x] == x ? x : parent[x] = find(parent[x]);
-        };
+    void dfs(int node, vector<int>& visited, unordered_map<int, vector<int>>& adjList){
+        visited[node] = 1;
+        for(auto it : adjList[node]){
+            if(!visited[it]) dfs(it, visited, adjList);
+        }
+    }
 
-        auto unite = [&](int x, int y) {
-            int rootX = find(x), rootY = find(y);
-            if (rootX != rootY) {
-                if (rank[rootX] > rank[rootY]) swap(rootX, rootY);
-                parent[rootX] = rootY;
-                if (rank[rootX] == rank[rootY]) rank[rootY]++;
-            }
-        };
-
-        for (int i = 0; i < n; i++) {
-            for (int j = i + 1; j < n; j++) {
-                int rootI = find(i), rootJ = find(j);
-                if (rootI == rootJ) continue;
-                
-                unordered_set<int>& smaller = propSets[i].size() < propSets[j].size() ? propSets[i] : propSets[j];
-                unordered_set<int>& larger = propSets[i].size() >= propSets[j].size() ? propSets[i] : propSets[j];
-
-                int common = 0;
-                for (int val : smaller) {
-                    if (larger.count(val)) {
-                        common++;
-                        if (common >= k) {
-                            unite(i, j);
-                            break;
-                        }
-                    }
+    int numberOfComponents(vector<vector<int>>& properties, int k) {
+        int result = 0;
+        unordered_map<int, vector<int>> adjList;
+        int m = properties.size();
+        vector<unordered_set<int>> prop(m);
+        for(int i = 0 ; i < m; i++) prop[i] = unordered_set<int>(properties[i].begin(), properties[i].end());
+        for(int i = 0; i < m; i++){
+            for(int j = i+1; j < m; j++){
+                if(intersect(prop[i], prop[j], k)){
+                    adjList[i].push_back(j);
+                    adjList[j].push_back(i);
                 }
             }
         }
-
-        unordered_set<int> result;
-        for (int i = 0; i < n; i++) {
-            result.insert(find(i));
+        
+        vector<int> visited(m, 0);
+        for(int i = 0; i < m; i++){
+            if(!visited[i]){
+                result++;
+                dfs(i, visited, adjList);
+            }
         }
-
-        return result.size();
+        return result;
     }
 };
