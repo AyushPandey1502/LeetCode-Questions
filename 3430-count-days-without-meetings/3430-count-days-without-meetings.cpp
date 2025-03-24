c3c7bdd6a9d67139dba8bd45d53b3ce75d7aff8c
Ayush Pandey <114850053+AyushPandey1502@users.noreply.github.com>
2025-03-24 10:21:20 +0530

    Time: 151 ms (16.33%), Space: 142.5 MB (17.35%) - LeetHub

--- 3430-count-days-without-meetings/3430-count-days-without-meetings.cpp
+++ 3430-count-days-without-meetings/3430-count-days-without-meetings.cpp
@@ -1,22 +1,13 @@
 class Solution {
 public:
     int countDays(int days, vector<vector<int>>& meetings) {
-        vector<pair<int, int>> events;
-        
-        for (auto it : meetings) {
-            events.push_back({it[0], 1});
-            events.push_back({it[1] + 1, -1});
+        int result = 0, end = 0;
+        sort(meetings.begin(), meetings.end());
+        for(auto it : meetings){
+            if(it[0] > end + 1) result += it[0] - end - 1;
+            end = max(end, it[1]);
         }
-        sort(events.begin(), events.end());
-        int result = 0, active = 0, end = 1;
-        
-        for (auto it : events) {
-            int currDay = it.first;
-            if (active == 0) result += (currDay - end);
-            active += it.second;
-            end = currDay;
-        }
-        if (active == 0) result += (days - end + 1);
+        result += days - end;
         return result;
     }
 };
