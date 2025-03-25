ea365be8381c0186cb3e3f49b5c33e2a87b9c8a6
Ayush Pandey <114850053+AyushPandey1502@users.noreply.github.com>
2025-03-25 23:44:59 +0530

    Time: 3 ms (63.28%), Space: 118.1 MB (71.23%) - LeetHub

--- 0234-palindrome-linked-list/0234-palindrome-linked-list.cpp
+++ 0234-palindrome-linked-list/0234-palindrome-linked-list.cpp
@@ -1,15 +1,32 @@
 class Solution {
 public:
-    ListNode* curr;
-    bool isPalindrome(ListNode* head) {
-        curr = head;
-        return check(head);
+    ListNode* reverse(ListNode* head) {
+        ListNode* prev = nullptr;
+        ListNode* curr = head;
+        while (curr != nullptr) {
+            ListNode* next = curr->next;
+            curr->next = prev;
+            prev = curr;
+            curr = next;
+        }
+        return prev;
     }
-
-    bool check(ListNode* head){
-        if(head == nullptr) return true;
-        bool result = check(head->next) && head->val == curr->val;
-        curr = curr->next;
-        return result;
+    
+    bool isPalindrome(ListNode* head) {
+        ListNode* slow = head;
+        ListNode* fast = head;
+        while (fast != nullptr && fast->next != nullptr) {
+            slow = slow->next;
+            fast = fast->next->next;
+        }
+        ListNode* rev = reverse(slow);
+        while (rev != nullptr) {
+            if (head->val != rev->val) {
+                return false;
+            }
+            head = head->next;
+            rev = rev->next;
+        }
+        return true;
     }
 };
