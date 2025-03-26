23c97e02b1e0e7e9f010b04d80051239a8ab9ee4
Ayush Pandey <114850053+AyushPandey1502@users.noreply.github.com>
2025-03-27 00:29:17 +0530

    Time: 6 ms (9.18%), Space: 13.1 MB (7.9%) - LeetHub

---
+++ 0046-permutations/0046-permutations.cpp
@@ -0,0 +1,30 @@
+class Solution {
+public:
+
+    vector<vector<int>> permute(vector<int>& nums) {
+        vector<vector<int>> result;
+        if(nums.size() == 0) return result;
+        int n = nums.size();
+        vector<bool> taken(n, false);
+        vector<int> temp;
+        findPerm(nums, temp, taken, result);
+        return result;
+    }
+
+    void findPerm(vector<int> nums, vector<int> temp, vector<bool> taken, vector<vector<int>>& result){
+        if(temp.size() == nums.size()){
+            result.push_back(temp);
+            return;
+        }
+
+        for(int i = 0; i < nums.size(); i++){
+            if(!taken[i]){
+                temp.push_back(nums[i]);
+                taken[i] = true;
+                findPerm(nums, temp, taken, result);
+                temp.pop_back();
+                taken[i] = false;
+            }
+        }
+    }
+};
