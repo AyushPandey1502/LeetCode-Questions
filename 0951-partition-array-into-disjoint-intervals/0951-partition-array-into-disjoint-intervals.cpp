58eb16132f8e968b1e175bc425c67ab37cb39c8a
Ayush Pandey <114850053+AyushPandey1502@users.noreply.github.com>
2025-03-27 12:10:19 +0530

    Time: 7 ms (29.63%), Space: 102.2 MB (54.58%) - LeetHub

--- 0951-partition-array-into-disjoint-intervals/0951-partition-array-into-disjoint-intervals.cpp
+++ 0951-partition-array-into-disjoint-intervals/0951-partition-array-into-disjoint-intervals.cpp
@@ -2,20 +2,17 @@ class Solution {
 public:
     int partitionDisjoint(vector<int>& nums) {
         int n = nums.size();
-        vector<int> prefix(n), suffix(n);
+        vector<int> suffix(n);
 
         for (int i = 0; i < n; i++) {
-            if (i == 0) {
-                prefix[i] = nums[i];
-                suffix[n-1] = nums[n-1];  
-            } else {
-                prefix[i] = max(nums[i], prefix[i-1]);
-                suffix[n-i-1] = min(nums[n-i-1], suffix[n-i]); 
-            }
+            if (i == 0) suffix[n-1] = nums[n-1];  
+            else suffix[n-i-1] = min(nums[n-i-1], suffix[n-i]); 
         }
 
-        for (int i = 0; i < n - 1; i++) {
-            if (prefix[i] <= suffix[i + 1]) return i + 1;
+        int maxLeft = nums[0];
+        for (int i = 1; i < n; i++) {
+            maxLeft = max(maxLeft, nums[i-1]);
+            if (maxLeft <= suffix[i]) return i;
         }
 
         return -1; 
