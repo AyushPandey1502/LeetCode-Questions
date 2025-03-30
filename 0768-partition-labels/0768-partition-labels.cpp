016919322a209358ce271ecbcdc09f7ab6b072e0
Ayush Pandey <114850053+AyushPandey1502@users.noreply.github.com>
2025-03-30 11:41:29 +0530

    Time: 2 ms (40.51%), Space: 10 MB (5.45%) - LeetHub

--- 0768-partition-labels/0768-partition-labels.cpp
+++ 0768-partition-labels/0768-partition-labels.cpp
@@ -1,19 +1,32 @@
 class Solution {
 public:
     vector<int> partitionLabels(string s) {
+        unordered_map<char, vector<int>> map;
         int n = s.size();
-        vector<int> index(26, 0);
-        for(int i = 0; i < n; i++) index[s[i]-'a'] = i;
-        vector<int> result;
-        int maxIndex = 0, count = 0;
-        for(int i = 0; i < n; i++){
-            maxIndex = max(maxIndex, index[s[i]-'a']);
-            count++;
-            if(maxIndex == i){
-                result.push_back(count);
-                count = 0;
-            }
+        
+        for(int i = 0; i < n; i++) {
+            if(map.count(s[i]) == 0) map[s[i]] = {i, i}; 
+            else map[s[i]][1] = i;
         }
+
+        vector<vector<int>> intervals;
+        for(auto it : map) {
+            intervals.push_back(it.second);
+        }
+
+        sort(intervals.begin(), intervals.end());
+        vector<int> result;
+        int start = intervals[0][0], end = intervals[0][1];
+
+        for(int i = 1; i < intervals.size(); i++) {
+            if(intervals[i][0] > end) {  
+                result.push_back(end - start + 1);
+                start = intervals[i][0];
+                end = intervals[i][1];
+            } else end = max(end, intervals[i][1]);
+        }
+
+        result.push_back(end - start + 1);
         return result;
     }
 };
