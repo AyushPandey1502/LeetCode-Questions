dd688df62f1470cd4bf34f941fe64e73f2411706
Ayush Pandey <114850053+AyushPandey1502@users.noreply.github.com>
2025-04-04 10:02:02 +0530

    Time: 6 ms (8.97%), Space: 23.7 MB (13.57%) - LeetHub

--- 1218-lowest-common-ancestor-of-deepest-leaves/1218-lowest-common-ancestor-of-deepest-leaves.cpp
+++ 1218-lowest-common-ancestor-of-deepest-leaves/1218-lowest-common-ancestor-of-deepest-leaves.cpp
@@ -1,17 +1,43 @@
 class Solution {
 public:
-    pair<int, TreeNode*> dfs(TreeNode* root) {
-        if (!root) return {0, nullptr};
-
-        auto [leftDepth, leftLCA] = dfs(root->left);
-        auto [rightDepth, rightLCA] = dfs(root->right);
-
-        if (leftDepth > rightDepth) return {leftDepth + 1, leftLCA};
-        if (rightDepth > leftDepth) return {rightDepth + 1, rightLCA};
-        return {leftDepth + 1, root};
-    }
-
     TreeNode* lcaDeepestLeaves(TreeNode* root) {
-        return dfs(root).second;
+        if (!root) return nullptr;
+
+        unordered_map<TreeNode*, TreeNode*> parent; 
+        queue<TreeNode*> q;
+        q.push(root);
+
+        vector<TreeNode*> leaves;
+
+        while (!q.empty()) {
+            int size = q.size();
+            leaves.clear();
+
+            for (int i = 0; i < size; i++) {
+                TreeNode* node = q.front();
+                q.pop();
+                leaves.push_back(node);
+
+                if (node->left) {
+                    q.push(node->left);
+                    parent[node->left] = node;
+                }
+                if (node->right) {
+                    q.push(node->right);
+                    parent[node->right] = node;
+                }
+            }
+        }
+
+        while (leaves.size() > 1) {
+            unordered_set<TreeNode*> ancest;
+            for (auto node : leaves) {
+                node = parent[node]; 
+                ancest.insert(node);
+            }
+            leaves.assign(ancest.begin(), ancest.end());
+        }
+
+        return leaves.front();
     }
 };
