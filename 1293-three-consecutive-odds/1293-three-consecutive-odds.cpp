839298b774dea875851e350eaec9230e6c0fbf77
Ayush Pandey <114850053+AyushPandey1502@users.noreply.github.com>
2025-05-11 23:40:13 +0530

    Time: 2 ms (1.15%), Space: 11.8 MB (88.25%) - LeetHub

--- 1293-three-consecutive-odds/1293-three-consecutive-odds.cpp
+++ 1293-three-consecutive-odds/1293-three-consecutive-odds.cpp
@@ -5,12 +5,8 @@ public:
         for (int num : arr) {
             if (num % 2 == 1) {
                 count++;
-                if (count == 3) {
-                    return true;
-                }
-            } else {
-                count = 0;
-            }
+                if (count == 3) return true;
+            } else count = 0;
         }
         return false; 
     }
