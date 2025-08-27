72347adf1c47d99ade55c47d84168a72cef39160
Ayush Pandey <114850053+AyushPandey1502@users.noreply.github.com>
2025-08-28 00:57:17 +0530

    Time: 32 ms (92.12%), Space: 22.3 MB (48.56%) - LeetHub

--- 1129-longest-string-chain/1129-longest-string-chain.cpp
+++ 1129-longest-string-chain/1129-longest-string-chain.cpp
@@ -1,31 +1,22 @@
-// TIME COMPLEXITY: O(N*N*L)
+// BETTER SOLUTION
+// TIME COMPLEXITY: O(N*L*L)
 
 class Solution {
 public:
-    bool checkPossible(string a, string b) {
-        if(a.size() + 1 != b.size()) return false;
-        int i = 0, j = 0;
-        while(j < b.size()) {
-            if(i < a.size() && a[i] == b[j]) i++;
-            j++;
-        }
-        return i == a.size();
-    }
-
     int longestStrChain(vector<string>& words) {
         int n = words.size(), result = 0;
         sort(words.begin(), words.end(), [](string& a, string& b){
             return a.size() < b.size();
         });
-        vector<int> dp(n, 1);
+        unordered_map<string, int> dp;
 
-        for(int i = 0; i < n; i++){
-            for(int j = 0; j < i; j++){
-                if(checkPossible(words[j], words[i])) {
-                    dp[i] = max(dp[i], dp[j] + 1);
-                }
+        for(auto word : words){
+            dp[word] = 1;
+            for(int i = 0; i < word.size(); i++){
+                string pred = word.substr(0, i) + word.substr(i+1);
+                if(dp.count(pred)) dp[word] = max(dp[word], 1+dp[pred]);
             }
-            result = max(result, dp[i]);
+            result = max(result, dp[word]);
         }
         return result;
     }
