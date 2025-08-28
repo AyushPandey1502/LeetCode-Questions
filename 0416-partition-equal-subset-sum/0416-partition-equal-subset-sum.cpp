ae61e48d0d421052e06bc6eb92170a2759705d4c
Ayush Pandey <114850053+AyushPandey1502@users.noreply.github.com>
2025-08-28 13:45:24 +0530

    Time: 83 ms (82.57%), Space: 13.5 MB (88.08%) - LeetHub

--- 0416-partition-equal-subset-sum/0416-partition-equal-subset-sum.cpp
+++ 0416-partition-equal-subset-sum/0416-partition-equal-subset-sum.cpp
@@ -9,9 +9,9 @@ public:
         vector<bool> dp(targetSum + 1, false);
         dp[0] = true; 
 
-        for (int i = 0; i < n; i++) {
-            for (int j = targetSum; j >= nums[i]; j--) {
-                dp[j] = dp[j] || dp[j - nums[i]];
+        for(int i = 0; i < n; i++){
+            for(int j = targetSum; j >= nums[i]; j--){
+                dp[j] = dp[j] || dp[j-nums[i]];
             }
         }
         return dp[targetSum];
