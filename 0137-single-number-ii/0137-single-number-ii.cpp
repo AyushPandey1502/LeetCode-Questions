7e70e54e87828b68894757d04d9c87567d8ad219
Ayush Pandey <114850053+AyushPandey1502@users.noreply.github.com>
2025-09-08 00:55:06 +0530

    Time: 0 ms (100%), Space: 13.4 MB (65.97%) - LeetHub

--- 0137-single-number-ii/0137-single-number-ii.cpp
+++ 0137-single-number-ii/0137-single-number-ii.cpp
@@ -1,13 +1,10 @@
-// Concept of Buckets : Most Optimal Solution
-// Time Complexity : O(n) 
-// Space Complexity : O(1)
 class Solution {
 public:
     int singleNumber(vector<int>& nums) {
         int ones = 0, twos = 0;
-        for (int i = 0; i < nums.size(); i++) {
-            ones = (ones ^ nums[i]) & ~twos;
-            twos = (twos ^ nums[i]) & ~ones;
+        for (int it : nums) {
+            ones = (ones ^ it) & ~twos;
+            twos = (twos ^ it) & ~ones;
         }
         return ones;
     }
