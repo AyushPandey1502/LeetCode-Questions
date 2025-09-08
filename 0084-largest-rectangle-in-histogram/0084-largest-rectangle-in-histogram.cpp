2f790921905c904d0adc5e79b1130b9a6626e172
Ayush Pandey <114850053+AyushPandey1502@users.noreply.github.com>
2025-09-08 22:51:22 +0530

    Time: 25 ms (49.19%), Space: 85.8 MB (28.4%) - LeetHub

--- 0084-largest-rectangle-in-histogram/0084-largest-rectangle-in-histogram.cpp
+++ 0084-largest-rectangle-in-histogram/0084-largest-rectangle-in-histogram.cpp
@@ -1,49 +1,39 @@
 class Solution {
-private:
-    vector<int> nextSmallerElement(vector<int> arr, int n){
-        stack<int> stack;
-        stack.push(-1);
+   public:
+    vector<int> findPSE(vector<int>& nums, int n) {
         vector<int> result(n);
+        stack<int> st;
 
-        for(int i = n-1; i >= 0; i--){
-            int current = arr[i];
-            while(stack.top() != -1 && arr[stack.top()] >= current) stack.pop();
-            result[i] = stack.top();
-            stack.push(i);
+        for (int i = 0; i < n; i++) {
+            while (!st.empty() && nums[st.top()] >= nums[i]) st.pop();
+            result[i] = st.empty() ? -1 : st.top();
+            st.push(i);
         }
         return result;
     }
-    vector<int> prevSmallerElement(vector<int> arr, int n){
-        stack<int> stack;
-        stack.push(-1);
-        vector<int> result(n);
 
-        for(int i = 0; i < n; i++){
-            int current = arr[i];
-            while(stack.top() != -1 && arr[stack.top()] >= current) stack.pop();
-            result[i] = stack.top();
-            stack.push(i);
+    vector<int> findNSE(vector<int>& nums, int n) {
+        vector<int> result(n);
+        stack<int> st;
+
+        for (int i = n - 1; i >= 0; i--) {
+            while (!st.empty() && nums[st.top()] >= nums[i]) st.pop();
+            result[i] = st.empty() ? n : st.top();
+            st.push(i);
         }
         return result;
     }
-public:
+
     int largestRectangleArea(vector<int>& heights) {
-        int size = heights.size();
+        int n = heights.size();
+        vector<int> nse = findNSE(heights, n);
+        vector<int> pse = findPSE(heights, n);
 
-        vector<int> next(size);
-        next = nextSmallerElement(heights, size);
-        vector<int> prev(size);
-        prev = prevSmallerElement(heights, size);
-
-        int area = 0;
-        for(int i = 0; i < size; i++){
-            int length = heights[i];
-            if(next[i] == -1) next[i] = size;
-            int width = next[i] - prev[i] - 1;
-
-            int newArea = length * width;
-            area = max(area, newArea);
+        int result = 0;
+        for (int i = 0; i < n; i++) {
+            int area = (nse[i] - pse[i] - 1) * heights[i];
+            result = max(result, area);
         }
-        return area;
+        return result;
     }
 };
