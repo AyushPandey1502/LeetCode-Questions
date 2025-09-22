6826686adf65dd98ca71c1df11136f6698feee0b
Ayush Pandey <114850053+AyushPandey1502@users.noreply.github.com>
2025-09-23 01:14:35 +0530

    Time: 148 ms (58.01%), Space: 134.1 MB (51.18%) - LeetHub

--- 2543-most-popular-video-creator/2543-most-popular-video-creator.cpp
+++ 2543-most-popular-video-creator/2543-most-popular-video-creator.cpp
@@ -1,18 +1,26 @@
 class Solution {
 public:
-    vector<vector<string>> mostPopularCreator(vector<string>& creators, vector<string>& ids, vector<int>& views) {
-        unordered_map<string, set<pair<long long, string>>> mp;
+    vector<vector<string>> mostPopularCreator(vector<string>& creators,
+                                              vector<string>& ids,
+                                              vector<int>& views) {
+        unordered_map<string, pair<int, string>> mp;
         unordered_map<string, long long> popularity;
         long long maxViews = 0;
-        for(int i = 0; i < views.size(); i++){
+
+        for (int i = 0; i < views.size(); i++) {
             popularity[creators[i]] += views[i];
             maxViews = max(maxViews, popularity[creators[i]]);
-            mp[creators[i]].insert({-views[i], ids[i]});
+            if (!mp.count(creators[i]) || mp[creators[i]].first < views[i] ||
+                (mp[creators[i]].first == views[i] &&
+                 mp[creators[i]].second > ids[i])) {
+                mp[creators[i]] = {views[i], ids[i]};
+            }
         }
+
         vector<vector<string>> result;
-        for(auto it : popularity){
-            if(it.second == maxViews){
-                result.push_back({it.first, mp[it.first].begin()->second});
+        for (auto& it : popularity) {
+            if (it.second == maxViews) {
+                result.push_back({it.first, mp[it.first].second});
             }
         }
         return result;
