bbda072a57c84faecbe09e26cfac3455271bd1c5
Ayush Pandey <114850053+AyushPandey1502@users.noreply.github.com>
2025-09-23 00:29:03 +0530

    Time: 176 ms (39.96%), Space: 267.6 MB (32.33%) - LeetHub

--- 2434-design-a-number-container-system/2434-design-a-number-container-system.cpp
+++ 2434-design-a-number-container-system/2434-design-a-number-container-system.cpp
@@ -1,26 +1,21 @@
 class NumberContainers {
 public:
     unordered_map<int, int> index;
-    unordered_map<int, priority_queue<int, vector<int>, greater<int>>> map;
+    unordered_map<int, set<int>> mp;
 
     NumberContainers() {}
-
-    void change(int ind, int number) {
-        if (index.count(ind)) {
-            int num = index[ind];
-            if (num == number) return;
+    
+    void change(int idx, int number) {
+        if(index.count(idx)){
+            int num = index[idx];
+            mp[num].erase(idx);
         }
-        index[ind] = number;
-        map[number].push(ind);
+        mp[number].insert(idx);
+        index[idx] = number;
     }
-
+    
     int find(int number) {
-        if (map.count(number) == 0) return -1;
-        while (!map[number].empty()) {
-            int ind = map[number].top();
-            if (index[ind] == number) return ind;
-            map[number].pop();
-        }
+        if(mp.count(number) && !mp[number].empty()) return *mp[number].begin();
         return -1;
     }
 };
