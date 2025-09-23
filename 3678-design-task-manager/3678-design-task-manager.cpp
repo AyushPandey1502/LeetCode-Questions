90365d0e40613568bcac19d08a887f6ca7334a67
Ayush Pandey <114850053+AyushPandey1502@users.noreply.github.com>
2025-09-23 11:36:48 +0530

    Time: 202 ms (81.9%), Space: 354.5 MB (78.82%) - LeetHub

--- 3678-design-task-manager/3678-design-task-manager.cpp
+++ 3678-design-task-manager/3678-design-task-manager.cpp
@@ -1,66 +1,40 @@
 class TaskManager {
-private:
-    struct Task {
-        int userId;
-        int taskId;
-        int priority;
-        Task() : userId(0), taskId(0), priority(0) {}
-        Task(int u, int t, int p) : userId(u), taskId(t), priority(p) {}
-        bool operator<(Task other) const {
-            if (priority == other.priority) {
-                return taskId > other.taskId;
-            }
-            return priority > other.priority;
-        }
-    };
-
-    unordered_map<int, Task> taskMap;
-    set<Task> taskSet;
-
 public:
+    unordered_map<int, pair<int, int>> taskInfo;
+    priority_queue<pair<long long, int>> pq;
     TaskManager(vector<vector<int>>& tasks) {
-        for (const auto& t : tasks) {
-            int userId = t[0], taskId = t[1], priority = t[2];
-            Task task(userId, taskId, priority);
-            taskMap[taskId] = task;
-            taskSet.insert(task);
+        for(int i = 0; i < tasks.size(); i++){
+            taskInfo[tasks[i][1]] = {tasks[i][0], tasks[i][2]};
+            pq.push({(long long)tasks[i][2] * 1000000LL + tasks[i][1], tasks[i][1]});
         }
     }
-
+    
     void add(int userId, int taskId, int priority) {
-        Task task(userId, taskId, priority);
-        taskMap[taskId] = task;
-        taskSet.insert(task);
+        taskInfo[taskId] = {userId, priority};
+        pq.push({(long long)priority * 1000000LL + taskId, taskId});
     }
-
+    
     void edit(int taskId, int newPriority) {
-        auto it = taskMap.find(taskId);
-        if (it != taskMap.end()) {
-            Task oldTask = it->second;
-            taskSet.erase(oldTask);
-            Task newTask(oldTask.userId, taskId, newPriority);
-            taskMap[taskId] = newTask;
-            taskSet.insert(newTask);
-        }
+        int userId = taskInfo[taskId].first;
+        taskInfo[taskId] = {userId, newPriority};
+        pq.push({(long long)newPriority * 1000000LL + taskId, taskId});
     }
-
+    
     void rmv(int taskId) {
-        auto it = taskMap.find(taskId);
-        if (it != taskMap.end()) {
-            Task task = it->second;
-            taskSet.erase(task);
-            taskMap.erase(taskId);
-        }
+        taskInfo.erase(taskId);
     }
-
+    
     int execTop() {
-        if (taskSet.empty()) {
-            return -1;
+        while(!pq.empty()){
+            auto [key, taskId] = pq.top();
+            pq.pop();
+            if(!taskInfo.count(taskId)) continue;
+            auto [userId, priority] = taskInfo[taskId];
+            long long expKey = (long long)priority * 1000000LL + taskId;
+            if(key != expKey) continue;
+            taskInfo.erase(taskId);
+            return userId;
         }
-        auto it = taskSet.begin();
-        Task topTask = *it;
-        taskSet.erase(it);
-        taskMap.erase(topTask.taskId);
-        return topTask.userId;
+        return -1;
     }
 };
