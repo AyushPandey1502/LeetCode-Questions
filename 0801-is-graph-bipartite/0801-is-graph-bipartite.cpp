469f911fda6163cb4c4f29b143ae8035ec5674b9
Ayush Pandey <114850053+AyushPandey1502@users.noreply.github.com>
2025-09-28 17:51:46 +0530

    Time: 0 ms (100%), Space: 17.6 MB (26.54%) - LeetHub

--- 0801-is-graph-bipartite/0801-is-graph-bipartite.cpp
+++ 0801-is-graph-bipartite/0801-is-graph-bipartite.cpp
@@ -3,21 +3,28 @@ public:
     bool isBipartite(vector<vector<int>>& graph) {
         int V = graph.size();
         vector<int> color(V, -1);
-        for(int i = 0; i < V; i++){
-            if(color[i] != -1) continue;
+
+        auto bfs = [&](int start) {
             queue<int> q;
-            q.push(i);
-            color[i] = 0;
-            while(!q.empty()){
-                int node = q.front();
-                q.pop();
-                for(auto it : graph[node]){
-                    if(color[it] == -1){
-                        color[it] = 1 - color[node];
-                        q.push(it);
-                    }else if(color[it] == color[node]) return false;
+            q.push(start);
+            color[start] = 0;
+
+            while(!q.empty()) {
+                int node = q.front(); q.pop();
+                for(int neighbor : graph[node]) {
+                    if(color[neighbor] == -1) {
+                        color[neighbor] = 1 - color[node];
+                        q.push(neighbor);
+                    } else if(color[neighbor] == color[node]) {
+                        return false;
+                    }
                 }
             }
+            return true;
+        };
+
+        for(int i = 0; i < V; i++) {
+            if(color[i] == -1 && !bfs(i)) return false;
         }
         return true;
     }
