3a27bc264e13afd1638457674cc3d244d782d94a
Ayush Pandey <114850053+AyushPandey1502@users.noreply.github.com>
2025-10-01 22:15:55 +0530

    Time: 0 ms (100%), Space: 11.7 MB (49.26%) - LeetHub

--- 0075-sort-colors/0075-sort-colors.cpp
+++ 0075-sort-colors/0075-sort-colors.cpp
@@ -1,22 +1,12 @@
-// OPTIMAL APPROACH : DUTCH NATIONAL FLAG ALGORITHM
-// TIME COMPLEXITY: O(N)
-// SPACE COMPLEXITY: O(1)
-
 class Solution {
 public:
     void sortColors(vector<int>& nums) {
         int n = nums.size();
         int low = 0, mid = 0, high = n-1;
         while(mid <= high){
-            if(nums[mid] == 0){
-                swap(nums[low], nums[mid]);
-                low++;
-                mid++;
-            }else if(nums[mid] == 1) mid++;
-            else{
-                swap(nums[mid], nums[high]);
-                high--;
-            }
+            if(nums[mid] == 0) swap(nums[low++], nums[mid++]);
+            else if(nums[mid] == 1) mid++;
+            else swap(nums[mid], nums[high--]);
         }
     }
 };
