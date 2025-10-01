f343c1578ebb697599167d2361bef9ea684c2094
Ayush Pandey <114850053+AyushPandey1502@users.noreply.github.com>
2025-10-01 21:57:38 +0530

    Time: 0 ms (100%), Space: 71.7 MB (52.54%) - LeetHub

--- 0053-maximum-subarray/0053-maximum-subarray.cpp
+++ 0053-maximum-subarray/0053-maximum-subarray.cpp
@@ -1,34 +1,11 @@
 class Solution {
 public:
-    int crossingSum(vector<int>& nums, int low, int mid, int high) {
-        int leftSum = INT_MIN, rightSum = INT_MIN, sum = 0;
-        
-        for (int i = mid; i >= low; i--) {
-            sum += nums[i];
-            leftSum = max(leftSum, sum);
-        }
-        
-        sum = 0;
-        for (int i = mid + 1; i <= high; i++) {
-            sum += nums[i];
-            rightSum = max(rightSum, sum);
-        }
-        
-        return leftSum + rightSum;
-    }
-
-    int maxSum(vector<int>& nums, int low, int high) {
-        if (low == high) return nums[low];
-
-        int mid = low + (high - low) / 2;
-        int leftSum = maxSum(nums, low, mid);
-        int rightSum = maxSum(nums, mid + 1, high);
-        int crossSum = crossingSum(nums, low, mid, high);
-
-        return max({leftSum, rightSum, crossSum});
-    }
-
     int maxSubArray(vector<int>& nums) {
-        return maxSum(nums, 0, nums.size() - 1);
+        int result = nums[0], sum = 0;
+        for(int i = 0; i < nums.size(); i++){
+            sum = max(sum + nums[i], nums[i]);
+            result = max(result, sum);
+        }
+        return result;
     }
 };
