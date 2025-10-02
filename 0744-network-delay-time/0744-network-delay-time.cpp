6eadde5805a190668a87772fab9b9a800fdfefec
Ayush Pandey <114850053+AyushPandey1502@users.noreply.github.com>
2025-10-02 20:31:26 +0530

    Time: 96 ms (23.56%), Space: 47.8 MB (39.53%) - LeetHub

--- 0744-network-delay-time/0744-network-delay-time.cpp
+++ 0744-network-delay-time/0744-network-delay-time.cpp
@@ -1,34 +1,30 @@
 class Solution {
 public:
     int networkDelayTime(vector<vector<int>>& times, int n, int k) {
-        vector<vector<pair<int, int>>> adj(n+1);
-        for(auto it : times){
-            adj[it[0]].push_back({it[1], it[2]});
-        }
-        priority_queue<pair<int, int>, vector<pair<int, int>>, greater<pair<int, int>>> pq;
-        vector<int> time(n+1, INT_MAX);
+        vector<pair<int, int>> adj[n+1];
+        for(auto it : times) adj[it[0]].push_back({it[1], it[2]});
+        priority_queue<pair<int ,int>, vector<pair<int, int>>, greater<>> pq;
+        vector<int> dist(n+1, INT_MAX);
+        dist[k] = 0;
         pq.push({0, k});
-        time[k] = 0;
 
         while(!pq.empty()){
-            int tm = pq.top().first;
-            int node = pq.top().second;
+            auto [time, node] = pq.top();
             pq.pop();
+            if(time > dist[node]) continue;
 
             for(auto it : adj[node]){
-                int ngbr = it.first;
-                int edgeTm = it.second;
-                if(tm + edgeTm < time[ngbr]){
-                    time[ngbr] = tm + edgeTm;
-                    pq.push({time[ngbr], ngbr});
+                if(time + it.second < dist[it.first]){
+                    dist[it.first] = time + it.second;
+                    pq.push({dist[it.first], it.first});
                 }
             }
         }
-        int minTime = 0;
+        int result = 0;
         for(int i = 1; i <= n; i++){
-            if(time[i] == INT_MAX) return -1;
-            else minTime = max(minTime, time[i]);
+            if(dist[i] == INT_MAX) return -1;
+            result = max(result, dist[i]);
         }
-        return minTime;
+        return result;
     }
 };
