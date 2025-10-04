f0b37af9777042f4ea3db89c48a6e8936ed3c9b9
Ayush Pandey <114850053+AyushPandey1502@users.noreply.github.com>
2025-10-04 15:35:25 +0530

    Time: 12 ms (67.66%), Space: 46.7 MB (55.32%) - LeetHub

--- 1442-number-of-operations-to-make-network-connected/1442-number-of-operations-to-make-network-connected.cpp
+++ 1442-number-of-operations-to-make-network-connected/1442-number-of-operations-to-make-network-connected.cpp
@@ -13,13 +13,14 @@ public:
         return parent[node] = find(parent[node]);
     }
 
-    void unionBySize(int u, int v) {
+    bool unionBySize(int u, int v) {
         int pu = find(u);
         int pv = find(v);
-        if (pu == pv) return;
+        if (pu == pv) return false;
         if (size[pu] < size[pv]) swap(pu, pv);
         parent[pv] = pu;
         size[pu] += size[pv];
+        return true;
     }
 };
 
@@ -27,21 +28,12 @@ class Solution {
 public:
     int makeConnected(int n, vector<vector<int>>& connections) {
         DisjointSet ds(n);
-        int extra = 0, count = 0;
+        int extra = 0, components = n;
         for(auto it : connections){
             int u = it[0], v = it[1];
-            if(ds.find(u) == ds.find(v)) extra++;
-            ds.unionBySize(u, v);
+            if(!ds.unionBySize(u, v)) extra++;
+            else components--;
         }
-        unordered_set<int> seen;
-        for(int i = 0; i < n; i++){
-            int parent = ds.find(i);
-            if(seen.find(parent) == seen.end()){
-                seen.insert(parent);
-                count++;
-            }
-        }
-        if((count-1) <= extra) return count-1;
-        return -1;
+        return (extra >= components-1) ? components-1 : -1;
     }
 };
