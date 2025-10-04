e17d2b4321259cb88aa67bdbdf2eb05c70ab0824
Ayush Pandey <114850053+AyushPandey1502@users.noreply.github.com>
2025-10-04 19:12:34 +0530

    Time: 19 ms (23.69%), Space: 17.3 MB (11.95%) - LeetHub

--- 0794-swim-in-rising-water/0794-swim-in-rising-water.cpp
+++ 0794-swim-in-rising-water/0794-swim-in-rising-water.cpp
@@ -1,51 +1,30 @@
-class DisjointSet {
-    vector<int> parent, size;
-
-public:
-    DisjointSet(int n) {
-        parent.resize(n + 1, 0);
-        size.resize(n + 1, 1);
-        iota(parent.begin(), parent.end(), 0);
-    }
-
-    int find(int node) {
-        if (node == parent[node]) return node;
-        return parent[node] = find(parent[node]);
-    }
-
-    void unionBySize(int u, int v) {
-        int pu = find(u);
-        int pv = find(v);
-        if (pu == pv) return;
-        if (size[pu] < size[pv]) swap(pu, pv);
-        parent[pv] = pu;
-        size[pu] += size[pv];
-    }
-};
-
 class Solution {
 public:
-    vector<vector<int>> dir = {{0, 1}, {0, -1}, {1, 0}, {-1, 0}};
+    vector<vector<int>> dir = {{0,1},{1,0},{0,-1},{-1,0}};
     int swimInWater(vector<vector<int>>& grid) {
         int n = grid.size();
         vector<vector<int>> visited(n, vector<int>(n, 0));
-        unordered_map<int, pair<int, int>> mp;
-        for(int i = 0; i < n; i++){
-            for(int j = 0; j < n; j++) mp[grid[i][j]] = {i, j};
-        }
-        DisjointSet ds(n*n);
-        for(int i = 0; i < n*n; i++){
-            auto [row, col] = mp[i];
-            visited[row][col] = 1;
-            for(auto it : dir){
-                int nrow = row + it[0], ncol = col + it[1];
-                if(nrow >= 0 && nrow < n && ncol >= 0 && ncol < n && visited[nrow][ncol]){
-                    int node = row * n + col, adjNode = nrow * n + ncol;
-                    ds.unionBySize(node, adjNode);
+        priority_queue<array<int, 3>, vector<array<int, 3>>, greater<>> pq;
+
+        pq.push({grid[0][0], 0, 0});
+        visited[0][0] = 1;
+        int result = 0;
+
+        while(!pq.empty()){
+            auto [elev, row, col] = pq.top(); 
+            pq.pop();
+            result = max(result, elev);
+
+            if(row == n-1 && col == n-1) return result;
+
+            for(auto d : dir){
+                int nrow = row + d[0], ncol = col + d[1];
+                if(nrow >= 0 && nrow < n && ncol >= 0 && ncol < n && !visited[nrow][ncol]){
+                    visited[nrow][ncol] = 1;
+                    pq.push({grid[nrow][ncol], nrow, ncol});
                 }
             }
-            if(ds.find(0) == ds.find(n*n-1)) return i;
         }
-        return n*n;
+        return result;
     }
 };
