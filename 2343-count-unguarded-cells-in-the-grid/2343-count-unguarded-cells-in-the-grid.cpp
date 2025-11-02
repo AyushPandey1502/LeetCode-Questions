4ab56d9e5712e5de9e5c20dce8a8f79e4e00bf36
Ayush Pandey <114850053+AyushPandey1502@users.noreply.github.com>
2025-11-02 18:27:12 +0530

    Time: 199 ms (27.52%), Space: 209.1 MB (25.23%) - LeetHub

--- 2343-count-unguarded-cells-in-the-grid/2343-count-unguarded-cells-in-the-grid.cpp
+++ 2343-count-unguarded-cells-in-the-grid/2343-count-unguarded-cells-in-the-grid.cpp
@@ -2,10 +2,8 @@ class Solution {
 public:
     int countUnguarded(int m, int n, vector<vector<int>>& guards, vector<vector<int>>& walls){
         vector<vector<int>>mat(m, vector<int>(n));
-        for(auto vec : walls)
-            mat[vec[0]][vec[1]] = -2;
-        for(auto vec : guards)
-            mat[vec[0]][vec[1]] = -1;
+        for(auto vec : walls) mat[vec[0]][vec[1]] = -2;
+        for(auto vec : guards) mat[vec[0]][vec[1]] = -1;
 
         for(auto vec : guards){
             int x = vec[0], y = vec[1];
@@ -28,10 +26,11 @@ public:
         }
 
         int result = 0;
-        for(int i = 0; i < m; i++)
-            for(int j = 0; j < n; j++)
+        for(int i = 0; i < m; i++){
+            for(int j = 0; j < n; j++){
                 if(mat[i][j] == 0) result++;
-
+            }
+        }
         return result;
     }
 };
