8ec8dcbad08f13f552a8140c096e4048c569bfbd
Ayush Pandey <114850053+AyushPandey1502@users.noreply.github.com>
2025-11-10 16:07:05 +0530

    Time: 0 ms (100%), Space: 8.7 MB (41.68%) - LeetHub

---
+++ 0009-palindrome-number/0009-palindrome-number.cpp
@@ -0,0 +1,12 @@
+class Solution {
+public:
+    bool isPalindrome(int x) {
+        if(x < 0) return false;
+        long long org = x, num = 0;
+        while(x){
+            num = num * 10 + x % 10;
+            x /= 10;
+        }
+        return org == num;
+    }
+};
