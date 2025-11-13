35fa06cb1283304a1c2019196a2651e467a04231
Ayush Pandey <114850053+AyushPandey1502@users.noreply.github.com>
2025-11-14 03:54:11 +0530

    Time: 8 ms (30.66%), Space: 15.8 MB (75.18%) - LeetHub

--- 3493-maximum-number-of-operations-to-move-ones-to-the-end/3493-maximum-number-of-operations-to-move-ones-to-the-end.cpp
+++ 3493-maximum-number-of-operations-to-move-ones-to-the-end/3493-maximum-number-of-operations-to-move-ones-to-the-end.cpp
@@ -1,18 +1,12 @@
 class Solution {
 public:
     int maxOperations(string s) {
-        int result = 0;
-        int ones = 0;
-        bool check = false;
-        
-        for (char num : s) {
-            if (num == '1') {
-                ones++;
-                check = true;
-            } else if (check) {
-                result += ones;
-                check = false;
-            }
+        int count = 0, result = 0;
+        for (int i = 0; i < s.length(); i++) {
+            if (s[i] == '0') {
+                while ((i + 1) < s.length() && s[i + 1] == '0') i++;
+                result += count;
+            } else count++;
         }
         return result;
     }
