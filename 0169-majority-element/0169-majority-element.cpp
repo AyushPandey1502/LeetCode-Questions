0d82145983c942a87a45f0ab4afb386092c52630
Ayush Pandey <114850053+AyushPandey1502@users.noreply.github.com>
2025-11-16 15:48:28 +0530

    Time: 0 ms (100%), Space: 28.1 MB (63.26%) - LeetHub

--- 0169-majority-element/0169-majority-element.cpp
+++ 0169-majority-element/0169-majority-element.cpp
@@ -1,23 +1,20 @@
-// OPTIMAL APPROACH : MOORE'S VOTING ALGORITHM
-// TIME COMPLEXITY : O(N)
-// SPACE COMPLEXITY : O(1)
-
 class Solution {
 public:
     int majorityElement(vector<int>& nums) {
         int n = nums.size();
-        int element = 0;
-        int count = 0;
+        int count = 0, elem;
         for(int i = 0; i < n; i++){
             if(count == 0){
-                element = nums[i];
-                count++;
-            }else if(nums[i] == element){
-                count++;
-            }else{
-                count--;
-            }
+                elem = nums[i];
+                count = 1;
+            }else if(elem == nums[i]) count++;
+            else count--;
         }
-        return element;
+        int cnt = 0;
+        for(auto it : nums){
+            if(it == elem) cnt++;
+        }
+        if(cnt > (n / 2)) return elem;
+        else return -1;
     }
 };
