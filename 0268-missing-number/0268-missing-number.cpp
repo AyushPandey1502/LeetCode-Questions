53318c5672d988c68f8037d1eabe887cd4f78565
Ayush Pandey <114850053+AyushPandey1502@users.noreply.github.com>
2025-11-16 15:12:19 +0530

    Time: 0 ms (100%), Space: 21.7 MB (92.48%) - LeetHub

--- 0268-missing-number/0268-missing-number.cpp
+++ 0268-missing-number/0268-missing-number.cpp
@@ -1,13 +1,9 @@
-// MOST OPTIMAL SOLUTION
 class Solution {
 public:
     int missingNumber(vector<int>& nums) {
         int n = nums.size();
-        int XOR1 = 0, XOR2 = 0;
-        for(int i = 0; i < n; i++){
-            XOR1 = XOR1 ^ nums[i];
-            XOR2 = XOR2 ^ (i + 1);
-        }
-        return XOR1 ^ XOR2;
+        int sum1 = n * (n + 1) / 2;
+        int sum2 = accumulate(nums.begin(), nums.end(), 0);
+        return sum1 - sum2;
     }
 };
