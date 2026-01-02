9f8baaa29353c592876d0780dd32c01462a03958
Ayush Pandey <114850053+AyushPandey1502@users.noreply.github.com>
2026-01-02 23:41:14 +0530

    Time: 0 ms (100%), Space: 28.6 MB (53.6%) - LeetHub

---
+++ 0961-n-repeated-element-in-size-2n-array/0961-n-repeated-element-in-size-2n-array.cpp
@@ -0,0 +1,11 @@
+class Solution {
+public:
+    int repeatedNTimes(vector<int>& nums) {
+        unordered_set<int> seen;
+        for(auto it : nums){
+            if(seen.count(it)) return it;
+            seen.insert(it);
+        }
+        return -1;
+    }
+};
