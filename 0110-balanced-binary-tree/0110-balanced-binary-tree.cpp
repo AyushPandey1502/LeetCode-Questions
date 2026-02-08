1debc8916f5cbc6c12f64e08575af4e4ce81a013
Ayush Pandey <114850053+AyushPandey1502@users.noreply.github.com>
2026-02-08 21:10:10 +0530

    Time: 0 ms (100%), Space: 22.9 MB (82.16%) - LeetHub

--- 0110-balanced-binary-tree/0110-balanced-binary-tree.cpp
+++ 0110-balanced-binary-tree/0110-balanced-binary-tree.cpp
@@ -1,19 +1,16 @@
 class Solution {
 public:
     int findDepth(TreeNode* root) {
-        if (root == nullptr)
-            return 0;
+        if (root == nullptr) return 0;
         return 1 + max(findDepth(root->left), findDepth(root->right));
     }
 
     bool isBalanced(TreeNode* root) {
-        if (root == nullptr)
-            return true;
+        if (root == nullptr) return true;
         int lst = findDepth(root->left);
         int rst = findDepth(root->right);
 
-        if (abs(lst - rst) > 1)
-            return false;
+        if (abs(lst - rst) > 1) return false;
         return isBalanced(root->left) && isBalanced(root->right);
     }
 };
